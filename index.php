db802a99c272f1faedc41db1eb7c961402a86cdb
Peter Kraker <peter.kraker@mendeley.com>
2015-11-01 23:05:27 +0100

    Added a very basic frontend

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <script type="text/javascript" src="vis/lib/jquery-1.8.1.min.js">
+            </script>
+            <script type="text/javascript" src="vis/vis.js"></script>
+    </head>
+
+    <body style="margin:0px; padding:0px">
+        <div id="visualization" style="width:100%;"></div>
+        <script>
+                
+                var height = ($(document).height() < 700)?(700):($(document).height());
+                $("#visualization").css("height", height + "px");
+                
+                var myHeadstart = new Headstart(
+                        "localhost/" //host
+                        , "headstart2/" //path
+                        , "visualization" //append to tag
+                        , [{
+                                title: "dna"
+                                , file: "<?php echo $_GET["id"] ?>"
+                            }
+                            ] //data
+                        , {title: "Overview of dna"
+                            , max_diameter_size: 50
+                            , min_diameter_size: 30
+                            , max_area_size: 110
+                            , min_area_size: 50
+                            , use_area_uri: false
+                            , input_format: "json"
+                            , use_area_uri: true
+                            , base_unit: "views"
+                            } //options
+                )
+        </script>
+
+    </body>
+</html>
--- server/services/searchPLOS.php
+++ server/services/searchPLOS.php
@@ -1,6 +1,7 @@
 <?php
 
-require dirname(__FILE__) . '/../classes/headstart/preprocessing/RCalculation.php';
+require dirname(__FILE__) . '/../classes/headstart/preprocessing/calculation/RCalculation.php';
+require dirname(__FILE__) . '/../classes/headstart/persistence/SQLitePersistence.php';
 require_once dirname(__FILE__) . '/../classes/headstart/library/CommUtils.php';
 require_once dirname(__FILE__) . '/../classes/headstart/library/toolkit.php';
 
@@ -14,3 +15,21 @@ $ini_array = library\Toolkit::loadIni($INI_DIR);
 
 $query = library\CommUtils::getParameter($_GET, "q");
 
+$WORKING_DIR = $ini_array["general"]["preprocessing_dir"] . $ini_array["output"]["output_dir"];
+
+$calculation = new \headstart\preprocessing\calculation\RCalculation($ini_array);
+$output = $calculation->performCalculationAndReturnOutputAsJSON($WORKING_DIR, $query);
+
+$output_json = end($output);
+
+//echo $output_json;
+
+$persistence = new \headstart\persistence\SQLitePersistence($ini_array["connection"]["sqlite_db"]);
+$persistence->createVisualization($query, "My Test RPLOS", $output_json);
+
+?>
+<html>
+    <body>
+        Here is the link to your <a href="http://localhost/headstart2/index.php?id=" <?php echo $_GET["q"] ?> target="_blank">visualization</a>
+    </body>
+</html>
