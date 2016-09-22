064cc572bed44e6f2e3d6304e032d651d53b8188
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2016-09-22 12:56:10 +0200

    Temporary solution with global data-config loaded during runtime

--- examples/local_files/app-config.js
+++ examples/local_files/app-config.js
@@ -1,10 +1,10 @@
 const options = {
-    title: "Overview of Educational Technology",
+
     paper_max_scale: 1,
     paper_min_scale: 1,
     bubble_max_scale: 1,
     bubble_min_scale: 1,
-    input_format: "csv",
+
     use_area_uri: false,
     base_unit: "readers",
     is_force_areas: false,
@@ -14,25 +14,6 @@ const options = {
     show_intro: false
 };
 
-const data_path = "./data/";
-const data = [{
-    title: "edu1",
-    file: require(data_path + "educational-technology.csv")
-}, {
-    title: "edu2",
-    file: require(data_path + "edu2.csv")
-}, {
-    title: "edu3",
-    file: require(data_path + "edu3.csv")
-}, {
-    title: "edu4",
-    file: require(data_path + "edu4.csv")
-}, {
-    title: "edu5",
-    file: require(data_path + "edu5.csv")
-}];
-
 export const app_config = {
-    options: options,
-    data: data
-};
+    options: options
+}
