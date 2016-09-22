8b0a5b7c8ca480d7fd4665d29723ffac68e90df4
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2016-09-22 12:33:46 +0200

    Moved and created files for new example file structure

---
+++ examples/local_files/data-config.js
@@ -0,0 +1,21 @@
+window.data_config = {
+    title: "Overview of Educational Technology",
+    input_format: "csv",
+
+    files: [{
+        title: "edu1",
+        file: "./data/educational-technology.csv"
+    }, {
+        title: "edu2",
+        file: "./data/edu2.csv"
+    }, {
+        title: "edu3",
+        file: "./data/edu3.csv"
+    }, {
+        title: "edu4",
+        file: "./data/edu4.csv"
+    }, {
+        title: "edu5",
+        file: "./data/edu5.csv"
+    }]
+};
