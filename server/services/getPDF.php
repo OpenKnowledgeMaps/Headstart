f26f79aab067e458276ac7b946927f461f1820e1
Peter Kraker <opendiscovery@gmx.at>
2017-03-16 12:16:55 +0100

    Improved PDF retrieval

--- server/services/getPDF.php
+++ server/services/getPDF.php
@@ -9,6 +9,8 @@ require 'helper.php';
 
 use headstart\library;
 
+$USER_AGENT = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13';
+
 $INI_DIR = dirname(__FILE__) . "/../preprocessing/conf/";
 
 $ini_array = library\Toolkit::loadIni($INI_DIR);
@@ -35,7 +37,7 @@ library\CommUtils::echoOrCallback(json_encode(array("status" => "success", "file
 function getPDFLinkforBASE($url) {
   $link_list = preg_split("/;/", $url);
   //Remove all entries that are not URLs
-  $link_list = array_filter($link_list, function($item) { return filter_var($item, FILTER_VALIDATE_URL); });
+  //$link_list = array_filter($link_list, function($item) { return filter_var($item, FILTER_VALIDATE_URL); });
   
   $matches_pdf = array_filter($link_list, function($item) { return substr($item, -strlen(".pdf")) === ".pdf"; }); 
   if(count($matches_pdf) != 0) {
@@ -44,12 +46,12 @@ function getPDFLinkforBASE($url) {
   
   $matches_doi = array_filter($link_list, function($item) { return strpos($item, "dx.doi.org"); });
   if(count($matches_doi) != 0) {
-      return getRedirectURL($matches_doi[0]);
+      return getRedirectURL(array_values($matches_doi)[0]);
   }
   
   $matches_doaj = array_filter($link_list, function($item) { return strpos($item, "doaj.org"); });
   if(count($matches_doaj) != 0) {
-      $url = getRedirectDOAJ($matches_doaj[0]);
+      $url = getRedirectDOAJ(array_values($matches_doaj)[0]);
       if($url != false) {
         return getRedirectURL($url);
       }
@@ -77,33 +79,52 @@ function getRedirectDOAJ($doaj_url) {
     return ($fulltext_link === null)?(false):($fulltext_link);
 }
 
-function getRedirectURL($doi_link) {
+function getContentFromURL($link) {
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $doi_link);
+    curl_setopt($ch, CURLOPT_URL, $link);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_VERBOSE, true);
+    curl_setopt($ch, CURLOPT_USERAGENT, $GLOBALS["USER_AGENT"]);
     $response = curl_exec($ch);
     $redir = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
     curl_close($ch);
-
-    return parsePDFLink($response);
+    
+    return array($response, $redir);
 }
 
-function parsePDFLink($source) {
-    $has_match = preg_match_all('/["\']?([^"\'>]+(?:\.pdf))["\']?/', $source, $matches);
+function getRedirectURL($link) {
+    $response = getContentFromURL($link);
+    
+    return parsePDFLink($response[0], $response[1]);
+}
+
+function parsePDFLink($source, $url) {
+    $has_match = preg_match_all('/["\']?([^"\'\s>]+(?:\.pdf))["\']?/i', $source, $matches);
     
     if($has_match) {
-        return $matches[1][0];
+        $best_match = $matches[1][0];
+        if(!startsWith($best_match, "http://") || !startsWith($best_match, "https://") || !startsWith($best_match, "ftp://")) {
+            return substr($url, 0, strrpos( $url, '/')) . $best_match;
+        } else {
+            return $best_match;
+        }
     } else {
        return false; 
     }
+}
+
+function startsWith($haystack, $needle) {
+     $length = strlen($needle);
+     return (substr($haystack, 0, $length) === $needle);
 }
 
 function getPDFAndDownload($url, $images_path, $filename) {
     
     $output_path = $images_path . $filename;
 
-    $pdf = file_get_contents($url);
+    $pdf = getContentFromURL($url)[0];
 
     if ($pdf !== false) {
         file_put_contents($output_path, $pdf);
