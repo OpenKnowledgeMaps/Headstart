c7084ff417a468e5db87ef9018d23a04937f42c2
Peter Kraker <pkraker@openknowledgemaps.org>
2017-06-02 23:40:35 +0200

    Reverted webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ const common = {
     output: {
         path: path.resolve(__dirname, "dist"),
 		//dev: specify a full path including protocol, production: specify full path excluding protocol
-        publicPath: "//openknowledgemaps.org/educational-technology/dist/",
+        publicPath: "http://path/to/dist/",
         filename: 'headstart.js',
         libraryTarget: 'var',
         library: 'headstart'
