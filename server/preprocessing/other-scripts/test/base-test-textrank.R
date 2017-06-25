6f550577455613133023570f3110fd6fab99261c
Christopher Kittel <web@christopherkittel.eu>
2017-06-25 11:28:10 +0200

    added textrank tests

---
+++ server/preprocessing/other-scripts/test/base-test-textrank.R
@@ -0,0 +1,45 @@
+rm(list = ls())
+
+library(rstudioapi)
+
+options(warn=1)
+
+wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
+
+setwd(wd) #Don't forget to set your working directory
+
+query <- "editorial" #args[2]
+service <- "base"
+params <- NULL
+params_file <- "params_base.json"
+
+source("../vis_layout.R")
+source('../apis/base.R')
+
+debug = FALSE
+
+MAX_CLUSTERS = 15
+ADDITIONAL_STOP_WORDS = "english"
+
+if(!is.null(params_file)) {
+  params <- fromJSON(params_file)
+}
+
+#start.time <- Sys.time()
+
+input_data = get_papers(query, params, limit=125)
+
+#end.time <- Sys.time()
+#time.taken <- end.time - start.time
+#time.taken
+
+output_json = vis_layout(input_data$text, input_data$metadata,
+                         max_clusters=MAX_CLUSTERS,
+                         add_stop_words=ADDITIONAL_STOP_WORDS,
+                         testing=TRUE, list_size=100,
+                         labeling_method = "textrank")
+
+print(output_json)
+
+test = fromJSON(output_json)
+unique(test$cluster_labels)
---
+++ server/preprocessing/other-scripts/test/pubmed-test-textrank.R
@@ -0,0 +1,44 @@
+rm(list = ls())
+
+library(rstudioapi)
+
+options(warn=1)
+
+wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
+
+setwd(wd) #Don't forget to set your working directory
+
+query <- "deep learning" #args[2]
+service <- "pubmed"
+params <- NULL
+params_file <- "params_pubmed.json"
+
+source("../vis_layout.R")
+source("../apis/pubmed.R")
+
+debug = FALSE
+
+MAX_CLUSTERS = 15
+ADDITIONAL_STOP_WORDS = "english"
+
+if(!is.null(params_file)) {
+  params <- fromJSON(params_file)
+}
+
+#start.time <- Sys.time()
+
+input_data = get_papers(query, params)
+
+#end.time <- Sys.time()
+#time.taken <- end.time - start.time
+#time.taken
+
+output_json = vis_layout(input_data$text, input_data$metadata,
+                         max_clusters = MAX_CLUSTERS,
+                         add_stop_words = ADDITIONAL_STOP_WORDS, testing=TRUE,
+                         labeling_method = "textrank")
+
+print(output_json)
+
+test = fromJSON(output_json)
+unique(test$cluster_labels)
