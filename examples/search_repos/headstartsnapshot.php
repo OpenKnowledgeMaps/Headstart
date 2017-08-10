4d2a61be9ce20e7af523677d4989385d6ff8b10b
Rainer Bachleitner <r.bachleitner@gmail.com>
2017-08-10 17:07:38 +0200

    Add two snapshot parameters to config.ini, clean up of command in searchPubmed.php

---
+++ examples/search_repos/headstartsnapshot.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    </head>
+
+    <body style="margin:0px; padding:0px">
+        
+        <div id="visualization"></div>
+        <script type="text/javascript" src="data-config_<?php echo $_GET['service'] ?>.js"></script>
+        <script type="text/javascript" src="data-config_server.js"></script>
+        <script>
+        	data_config.title = <?php echo json_encode($_GET['title']) ?>;
+        	data_config.files = [{
+        		title: <?php echo json_encode($_GET['title']) ?>,
+        		file: <?php echo json_encode($_GET['file']) ?>
+        	}];
+        	data_config.nodot = 1;
+        </script>
+        <script type="text/javascript" src="../../dist/headstart.js"></script>
+        <link type="text/css" rel="stylesheet" href="../../dist/headstart.css"></link>
+        <script type="text/javascript">
+            headstart.start();
+        </script>
+         <div style="margin-top:20px">Built with <a href="http://github.com/pkraker/Headstart" target="_blank">Headstart</a> and <a href="http://github.com/ropensci/rplos" target="_blank">rplos</a>. All content retrieved from <a href="https://www.plos.org/publications/journals/" target="_blank">Public Library of Science Journals</a> under <a href="http://journals.plos.org/plosone/s/content-license" target="_blank">CC-BY</a>.
+        </div>
+    </body>
+</html>
--- server/services/searchPubmed.php
+++ server/services/searchPubmed.php
@@ -11,18 +11,29 @@ use headstart\library;
 $INI_DIR = dirname(__FILE__) . "/../preprocessing/conf/";
 
 $ini_array = library\Toolkit::loadIni($INI_DIR);
-$settings = $ini_array["general"];
+$settings = $ini_array["snapshot"];
+$general = $ini_array["general"];
 
 $dirty_query = library\CommUtils::getParameter($_POST, "q");
 
 $post_params = $_POST;
-
 $result = search("pubmed", $dirty_query, $post_params, array("article_types", "from", "to"), ";", null);
+$resultDecoded = json_decode($result);
 
-$id = json_decode($result)->id;
-$query = json_decode($result)->query;
+$id = $resultDecoded->id;
+$query = $resultDecoded->query;
 $title = 'Overview of PubMed articles for ' . $query . ':' . $id;
-$command = 'nohup '. $settings["phantomjs_path"] . ' ' . $settings["getsvg_path"] . ' "' . $settings["host"] . 'examples/search_repos/headstart.php?nodot=1&title='.$title.'&service=pubmed' . $post_params['service'] . '&file=' . $id . '" '. $settings["storage_path"] . $id .'.png "1200px" 1>/dev/null 2>/dev/null &';
+$command = 'nohup '.
+    $settings["phantomjs_path"] . ' ' .
+    $settings["getsvg_path"] .
+    ' "' .
+    $general["host"] .
+    $settings["snapshot_php"] . '?'.
+    'title='.$title.
+    '&service=pubmed'.
+    '&file=' . $id . '" '.
+    $settings["storage_path"] .
+    $id .'.png '. $settings["snapshot_width"] .' 1>/dev/null 2>/dev/null &';
 exec($command);
 
 echo $result
