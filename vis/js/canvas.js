9728db9554520a43ebedb4addd590635cdfe3b99
Peter Kraker <pkraker@openknowledgemaps.org>
2017-08-15 20:53:00 +0200

    Updated context tags to show articles types in popover

--- vis/js/canvas.js
+++ vis/js/canvas.js
@@ -285,21 +285,28 @@ class Canvas {
             $("#timespan").html(dateFormat(new Date(context.params.from), "d mmm yyyy") 
                     + " - " + dateFormat(new Date(context.params.to), "d mmm yyyy"));
             
-            let num_document_types = context.params.document_types.length;
+            let dtypes = (context.params.hasOwnProperty("document_types"))?("document_types"):("article_types");
+            let num_document_types = context.params[dtypes].length;
+            
             let document_types_string = "";
 
             let document_types = config.options.filter(function(obj) {
-                return obj.id == "article_types" || obj.id == "document_types";
+                return obj.id == dtypes;
             });
-            context.params.document_types.forEach(function (type) {
+            
+            context.params[dtypes].forEach(function (type) {
                 let type_obj = document_types[0].fields.filter(function(obj) {
                     return obj.id == type;
                 })
                 document_types_string += type_obj[0].text + ", ";
             })
+            document_types_string = document_types_string.substr(0, document_types_string.length - 2);
             
             $("#document_types").html(num_document_types + " " + config.localization[config.language].documenttypes_label);
-            $("#document_types_details").html(" (" + document_types_string.substr(0, document_types_string.length - 2) + ")");
+            $("#document_types").attr({
+                "data-content": document_types_string
+                , "title": document_types_string
+            });
         } else {
             $("#num_articles").html(context.num_documents);
         }
