5af85294a036d0d438f2866723c3e2449709cf5c
Christopher Kittel <web@christopherkittel.eu>
2018-03-05 22:14:17 +0100

    first implementation of openaire API plus test

---
+++ server/preprocessing/other-scripts/openaire.R
@@ -0,0 +1,64 @@
+library(ropenaire)
+
+# get_papers
+#
+# Params:
+#
+# * query: project acronym
+# * params: parameters for the search in JSON format: call_id and funding stream
+# * limit: number of search results to return
+#
+# It is expected that get_papers returns a list containing two data frames named "text" and "metadata"
+#
+# "text" contains the text for similarity analysis; it is expected to have two columns "id" and "content"
+#
+# "metadata" contains all metadata; its columns are expected to be named as follows:
+# * "id": a unique ID, preferably the DOI
+# * "title": the title
+# * "authors": authors, preferably in the format "LASTNAME1, FIRSTNAME1;LASTNAME2, FIRSTNAME2"
+# * "paper_abstract": the abstract
+# * "published_in": name of the journal or venue
+# * "year": publication date
+# * "url": URL to the landing page
+# * "readers": an indicator of the paper's popularity, e.g. number of readers, views, downloads etc.
+# * "subject": keywords or classification, split by ;
+
+
+get_papers <- function(query, params) {
+  # parse params
+  pubs <- roa_pubs(fp7 = query)
+  datasets = roa_datasets(fp7 = query)
+  all_artifacts <- rbind(pubs, datasets)
+
+
+
+  metadata = data.frame(matrix(nrow=length(all_artifacts$DOI)))
+  metadata$id = check_metadata(all_artifacts$DOI)
+  metadata$url = check_metadata(all_artifacts$`Download From`)
+
+  metadata$title = check_metadata(all_artifacts$Title)
+  metadata$subject = check_metadata(all_artifacts$subject)
+  metadata$paper_abstract = check_metadata(all_artifacts$abstract)
+  metadata$published_in = check_metadata(all_artifacts$Journal)
+  metadata$year = check_metadata(all_artifacts$`Publication Year`)
+
+
+
+
+  text = data.frame(matrix(nrow=length(all_artifacts$DOI)))
+  text$id = metadata$id
+  # paste whats available and makes sense
+  text$content = paste(metadata$title,
+                       sep = " ")
+  ret_val=list("metadata" = metadata, "text"=text)
+}
+
+
+
+check_metadata <- function (field) {
+  if(!is.null(field)) {
+    return (ifelse(is.na(field), '', field))
+  } else {
+    return ('')
+  }
+}
