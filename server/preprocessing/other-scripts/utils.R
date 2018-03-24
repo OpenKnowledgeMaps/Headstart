06dd2821feb2ecf84a7968ab710f6d7f151ef3cb
Christopher Kittel <web@christopherkittel.eu>
2018-03-24 01:41:23 +0100

    reflected package changes

---
+++ server/preprocessing/other-scripts/utils.R
@@ -0,0 +1,7 @@
+library(stringdist)
+
+levenshtein_ratio <- function(a, b) {
+  lv_dist = stringdist(a, b, method = "lv")
+  lv_ratio = lv_dist/(max(stri_length(a), stri_length(b)))
+  return(lv_ratio)
+}
