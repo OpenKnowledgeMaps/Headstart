97de9c0381c83ef6b3a1b36f68946c048dd79d76
Christopher Kittel <web@christopherkittel.eu>
2018-03-26 18:30:01 +0200

    better fix for #209

--- server/preprocessing/other-scripts/test/test-openaire.R
+++ server/preprocessing/other-scripts/test/test-openaire.R
@@ -35,4 +35,8 @@ input_data = get_papers(query, params)
 output_json = vis_layout(input_data$text, input_data$metadata, max_clusters=MAX_CLUSTERS,
                          add_stop_words=ADDITIONAL_STOP_WORDS, testing=TRUE, list_size=-1)
 
+if (service=='openaire'){
+  output_json = enrich_output_json(output_json)
+}
+
 print(output_json)
