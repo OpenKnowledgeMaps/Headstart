c61f659034503480bd85089949aabff818eb7d3e
Christopher Kittel <web@christopherkittel.eu>
2018-04-28 19:09:38 +0200

    separate querys for tasks

--- server/classes/headstart/persistence/ViperUpdater.php
+++ server/classes/headstart/persistence/ViperUpdater.php
@@ -36,4 +36,44 @@ class ViperUpdater extends SQLitePersistence {
     return $result;
   }
 
+  public function getUpdateMapsByID($vis_changed = false) {
+    $return_fields = "visualizations.vis_title,
+                      visualizations.vis_query,
+                      visualizations.vis_params";
+    if ($vis_changed == false) {
+        $stmt = "SELECT $return_fields FROM visualizations
+                  WHERE visualizations.vis_title == 'base'
+                  LIMIT 10";
+    } else {
+        $stmt = "SELECT $return_fields FROM visualizations
+                  WHERE visualizations.vis_title == 'base'
+                  WHERE visualizations.vis_changed = 1
+                  LIMIT 10";
+    }
+    $query = $this->db->query($stmt);
+    $result = $query->fetchAll();
+
+    return $result;
+  }
+
+  public function getUpdateMapsByFunderProject($vis_changed = false) {
+    $return_fields = "visualizations.vis_title,
+                      visualizations.vis_query,
+                      visualizations.vis_params";
+    if ($vis_changed == false) {
+        $stmt = "SELECT $return_fields FROM visualizations
+                  WHERE visualizations.vis_title == 'base'
+                  LIMIT 10";
+    } else {
+        $stmt = "SELECT $return_fields FROM visualizations
+                  WHERE visualizations.vis_title == 'base'
+                  WHERE visualizations.vis_changed = 1
+                  LIMIT 10";
+    }
+    $query = $this->db->query($stmt);
+    $result = $query->fetchAll();
+
+    return $result;
+  }
+
 }
