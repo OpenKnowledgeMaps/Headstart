e658d02cfd17c4d3bc687291dd82c5b433bff491
Christopher Kittel <web@christopherkittel.eu>
2018-05-02 11:47:29 +0200

    wip commit

--- server/classes/headstart/persistence/ViperUpdater.php
+++ server/classes/headstart/persistence/ViperUpdater.php
@@ -34,7 +34,7 @@ class ViperUpdater extends SQLitePersistence {
     return $result;
   }
 
-  public function getUpdateMapsByID($vis_changed) {
+  public function getUpdateMapsByID($vis_changed, $object_ids) {
     $return_fields = "visualizations.vis_title,
                       visualizations.vis_query,
                       visualizations.vis_params";
@@ -42,8 +42,11 @@ class ViperUpdater extends SQLitePersistence {
         $stmt = "SELECT $return_fields FROM visualizations
                  WHERE visualizations.vis_title == 'openaire'";
     } else {
-        $stmt = "SELECT $return_fields FROM visualizations
+        $stmt = "SELECT $return_fields,
+                        JSON_VALUE(visualizations.vis_params, '$.obj_id') AS ObjID
+                 FROM visualizations
                  WHERE visualizations.vis_title == 'openaire'
+                 AND ObjID IN $object_ids
                  AND visualizations.vis_changed = 1";
     }
     $query = $this->db->query($stmt);
