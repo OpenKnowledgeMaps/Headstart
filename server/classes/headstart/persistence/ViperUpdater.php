71fa66593b85d98b2484e10d6bdab91dddd81d96
Christopher Kittel <web@christopherkittel.eu>
2018-05-09 20:39:57 +0200

    removed defuncted because JSON1 dependent functions

--- server/classes/headstart/persistence/ViperUpdater.php
+++ server/classes/headstart/persistence/ViperUpdater.php
@@ -38,58 +38,6 @@ class ViperUpdater extends SQLitePersistence {
     return $result;
   }
 
-  public function getUpdateMapsByID($vis_changed, $object_ids) {
-    # currently defunct, only works with JSON1 extension
-    $return_fields = "visualizations.vis_title,
-                      visualizations.vis_query,
-                      visualizations.vis_params";
-    if ($vis_changed == false) {
-        $stmt = "SELECT $return_fields
-                 FROM visualizations
-                 WHERE visualizations.vis_title == 'openaire'
-                 AND JSON_VALUE(visualizations.vis_params, '$.obj_id') IN $object_ids
-                 ";
-    } else {
-        $stmt = "SELECT $return_fields
-                 FROM visualizations
-                 WHERE visualizations.vis_title == 'openaire'
-                 AND JSON_VALUE(visualizations.vis_params, '$.obj_id') IN $object_ids
-                 AND visualizations.vis_changed = 1
-                 ";
-    }
-    $query = $this->db->query($stmt);
-    $result = $query->fetchAll();
-
-    return $result;
-  }
-
-  public function getUpdateMapsByFunderProject($vis_changed, $funder, $project_id) {
-    # currently defunct, only works with JSON1 extension
-    $return_fields = "visualizations.vis_title,
-                      visualizations.vis_query,
-                      visualizations.vis_params";
-    if ($vis_changed == false) {
-        $stmt = "SELECT $return_fields
-                 FROM visualizations
-                 WHERE visualizations.vis_title == 'openaire'
-                   AND JSON_VALUE(visualizations.vis_params, '$.funder') = " . $funder .
-                 " AND JSON_VALUE(visualizations.vis_params, '$.project_id') = " . $project_id . "
-                 ";
-    } else {
-        $stmt = "SELECT $return_fields,
-                        JSON_VALUE(visualizations.vis_params, '$.project_id') AS pid
-                 FROM visualizations
-                 WHERE visualizations.vis_title == 'openaire'
-                 AND visualizations.vis_changed = 1
-                 ";
-    }
-    echo ($stmt);
-    $query = $this->db->query($stmt);
-    $result = $query->fetchAll();
-
-    return $result;
-  }
-
   public function resetFlag($vis_id) {
     $stmt = "UPDATE visualizations
              SET vis_changed=0
