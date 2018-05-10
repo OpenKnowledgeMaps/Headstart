e2dce9caaaafc6858c1e684a0d8685f8100b6997
tanteuschi <maxi@openknowledgemaps.org>
2018-05-10 20:02:27 +0200

    menu, pages, footer

--- examples/viper/index.php
+++ examples/viper/index.php
@@ -27,20 +27,26 @@ include 'config.php';
     </head>
 
     <body>
-        <div style="background-color: #3696e0; max-width:100%; height: 80px; padding: 20px 50px;">
-            <div style="max-width:1600px; width:100%; margin:0px auto;">
-                <div style="width:30%;display:inline-block;"><img style="display:inline-block; vertical-align: bottom;" src="viper-logo.png">
-                    <div style="display:inline-block; vertical-align: bottom; color:white;"><h2 style="margin-bottom: 0px; font-weight: 300;">VIPER</h2><span style="display:block; font-size: 18;">the visual project explorer</span>
-                    </div></div>
-                <div style="width:69%;display:inline-block; text-align:right;">
-                    <ul>
-                        <li style="display:inline; margin-right: 20px;"><a style="text-decoration:none; color:white;" href="https://openknowledgemaps.org" target="_blank">
-                                <span class="awesome"></span> back to Open Knowledge Maps</a></li>
-                        <li style="display:inline; margin-right: 20px;"><a style="text-decoration:none; color:white;" href="mailto:info@openknowledgemaps.org">
-                                <span class="awesome"></span> Contact</a></li>
-                        <li style="display:inline"><a style="text-decoration:none; color:white; border-bottom: 2px solid white;" href="about"><span class="awesome"></span> What is VIPER?</a></li>
+        <div class="vipercanvas">
+            <div class="viperheader">
+                <div class="viperlogo"><img src="viper-logo.png">
+                    <div class="viperdescr"><h2>VIPER</h2>
+                        <span>The Visual Project Explorer</span>
+                    </div>
+                    </div>
+                
+                
+                    <ul class="vipermenu">
+                        <li><a href="about">
+<!--<span class="awesome"></span>--> About</a></li>
+                        <li><a target="_blank" href="https://openknowledgemaps.org/getintouch">
+                                <!--<span class="awesome"></span>-->Get in touch</a>
+                        </li>
+                        <li><a href="https://openknowledgemaps.org" target="_blank">
+                                openknowledgemaps.org</a>
+                        </li>
                     </ul>
-                </div>
+                
             </div>
         </div>
 
@@ -66,13 +72,13 @@ include 'config.php';
 
                     </div>
                     
-                    <p class="try-out-maps">Try out: 
+                    <!--<p class="try-out-maps">Try out: 
                         
                             <a class="underline" target="_blank" href="https://openknowledgemaps.org/preview/viper/headstart.php?query=OPENAIRE%20-%20Open%20Access%20Infrastructure%20for%20Research%20in%20Europe&file=9b34f83d3940dc249c4ae9420c8fb2e2&service=openaire&service_name=OpenAire">Overview of OpenAIRE</a>
                         
-                    </p>
+                    </p>-->
                 </form>
-
+                <p class="error-building-map-button"><a class="newsletter2" href="" target="_blank">Try out the Overview of OpenAIRE</a></p>
             </div>
             <!--<p style="text-align:center; margin-top: 30px;"><a class="newsletter2" href="">What is VIPER?</a><p>-->
         </div>
@@ -106,21 +112,13 @@ include 'config.php';
         <div id="viper-search-pager">
         </div>
 
-        <div style="text-align:center; margin: 30px 0 20px; font-size:12px;">
-
-            <p>Built with <a href="https://openknowledgemaps.org/" target="_blank">Open Knowledge Maps</a>. 
-                This project received funding from <a href="https://openaire.eu" target="_blank">OpenAIRE</a>.
-                For more information please contact <a href="mailto:info@openknowledgemaps.org">info@openknowledgemaps.org</a>.</p>
-            <p style="margin-top: 20px;"><a href="https://openknowledgemaps.org/" target="_blank"><img style="margin-right:20px" width="70px" src="okmaps-logo.png"></a>
-                <a href="https://openaire.eu" target="_blank"><img width="70px" src="openaire-logo.png"></a>
-            </p>
-        </div>
-        <script type="text/javascript" src="search_openaire_projects.js"></script>
-        <script type="text/javascript" src="data-config_openaire.js"></script>
-        <script type="text/javascript" src="search_options.js"></script>
-        <script type="text/javascript" src="search.js"></script>
-        <script type ="text/javascript">
-            data_config.server_url = window.location.href.replace(/[^/]*$/, '') + "<?php echo $HEADSTART_PATH; ?>server/";
-        </script>
-    </body>
+        <?php include($COMPONENTS_PATH . 'footer.php'); ?>
+<script type="text/javascript" src="search_openaire_projects.js"></script>
+<script type="text/javascript" src="data-config_openaire.js"></script>
+<script type="text/javascript" src="search_options.js"></script>
+<script type="text/javascript" src="search.js"></script>
+<script type ="text/javascript">
+    data_config.server_url = window.location.href.replace(/[^/]*$/, '') + "<?php echo $HEADSTART_PATH; ?>server/";
+</script>
+</body>
 </html>
