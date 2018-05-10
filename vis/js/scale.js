b078403a60c1b5c90604c7738b3f8c6bb8f22b8c
Peter Kraker <pkraker@openknowledgemaps.org>
2018-05-10 12:02:10 +0200

    Uniform look and behaviour for all dropdowns

--- vis/js/scale.js
+++ vis/js/scale.js
@@ -10,18 +10,27 @@ class Scale {
   }
 
   drawScaleTypes () {
+    let self = this;
     if (this.requireStartUp){
       this.requireStartUp = false
       config.scale_types.forEach(type => {
-        d3.select('.scale-toolbar ul')
-        .append('li')
-        .attr('role', 'presentation')
-        .append('a')
-        .attr('role', 'menuitem')
-        .attr('tabindex', '-1')
-        .text(config.scale_label[type])
-        .on("click", this.doScale.bind(this, type))
+        var cur_item = d3.select('.scale-toolbar ul')
+            .append('li')
+            .attr('role', 'presentation')
+            .attr('class', 'scale_item')
+    
+        cur_item
+            .append('a')
+            .attr('role', 'menuitem')
+            .attr('tabindex', '-1')
+            .text(config.scale_label[type])
+            .on("click", function() {
+                $('.scale_item').removeClass('active')
+                cur_item.classed('active', true)
+                self.doScale(type)
+            })
       });
+      d3.select('.scale_item').classed('active', true);
       d3.select('#curr-scale-type').text(config.scale_label[config.scale_types[0]])
       d3.select('#curr-scale-explaination').text(config.scale_explaination[config.scale_types[0]])
     }
