a7bec9a18a80c6bd257f3f940376e16989caaa12
Peter Kraker <pkraker@openknowledgemaps.org>
2018-05-16 15:22:36 +0200

    Fix snapshot capability for OpenAIRE

--- server/services/snapshot/data-config_openaire.js
+++ server/services/snapshot/data-config_openaire.js
@@ -1,12 +1,16 @@
 var data_config = {
     tag: "visualization",
     mode: "search_repos",
-    
+
     bubble_min_scale: 1,
     bubble_max_scale: 1,
     
     paper_min_scale: 1,
     paper_max_scale: 1,
+    
+    dynamic_sizing: true,
+    dynamic_force_area: true,
+    dynamic_force_papers: true,
 
     service: "openaire",
     base_unit: "citations",
@@ -14,7 +18,9 @@ var data_config = {
     show_timeline: false,
     show_dropdown: false,
     preview_type: "pdf",
-    sort_options: ["title", "authors", "year"],
+    sort_options: ["title", "authors", "year", "citations", "tweets", "readers"],
+    filter_menu_dropdown: true,
+    sort_menu_dropdown: true,
     is_force_areas: true,
     language: "eng_openaire",
     area_force_alpha: 0.015,
@@ -30,11 +36,20 @@ var data_config = {
     
     viper_edit_modal: true,
     viper_embed_modal: true,
+    viper_share_modal: true,
+
+    viper_outlink: true,
 
     intro: 'intro_openaire',
+
+    viper_metric_list: true,
+    
+    viper_credit: false,
+
+    scale_toolbar: true,
     
     scale_types: ['content_based', 'citation_count', 'cited_by_tweeters_count', 'readers.mendeley'],
-    scale_explaination: {
+    scale_explanation: {
         content_based: 'The size of the bubbles is relative to the number of documents related to them.',
         citation_count: 'The size of the documents and bubbles is relative to the number of citations related to them. The citation data was retrieved from Crossref.',
         cited_by_tweeters_count: 'The size of the documents and bubbles is relative to the number of tweets related to them. The tweet data was retrieved from Altmetric.com.',
