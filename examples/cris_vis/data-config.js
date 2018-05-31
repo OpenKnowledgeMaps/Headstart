75988303db545b15749ecf6318bc8cf3fe9df2ad
Peter Kraker <pkraker@openknowledgemaps.org>
2018-05-31 16:55:22 +0200

    Add option to prevent outlink from repeated click on list title

--- examples/cris_vis/data-config.js
+++ examples/cris_vis/data-config.js
@@ -7,13 +7,13 @@ var data_config = {
     base_unit: "Nennungen",
     use_area_uri: false,
     is_force_areas: false,
-    url_prefix: "http://mendeley.com/catalog/",
     
     show_timeline: false,
     show_dropdown: false,
     show_intro: false,
     show_list:true,
     is_force_papers:true,
+    is_title_clickable: false,
 	
     show_context: false,
     create_title_from_context: false,
