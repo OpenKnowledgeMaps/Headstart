261a95250d2a94c32556334d213b2c36b76c936d
Peter Kraker <pkraker@openknowledgemaps.org>
2018-06-01 21:29:12 +0200

    Fixed issue where tags would not update in bubbles

--- vis/js/bubbles.js
+++ vis/js/bubbles.js
@@ -361,7 +361,7 @@ BubblesFSM.prototype = {
             let current_context = context.distributions_areas[d.area_uri][attribute];
             let overall_context = context.distributions_all[attribute];
             
-            let area_visual_distributions = d3.select(bubble_frames[0][i]).select(".area_visual_distributions");
+            let area_visual_distributions = d3.select(bubble_frames[0][i]).select("#area_visual_distributions");
             updateTags(current_context, overall_context, area_visual_distributions, attribute);
         }
     },
