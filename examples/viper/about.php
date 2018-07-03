420bcdd35b962aee74a5fe13a0d5379e9fda4488
Peter Kraker <pkraker@openknowledgemaps.org>
2018-07-03 22:52:46 +0200

    Update ID of OpenAIRE example project

--- examples/viper/about.php
+++ examples/viper/about.php
@@ -54,7 +54,7 @@ include 'config.php';
                     It enables funders, institutions and researchers to systematically explore a project’s output, and to understand its impact in different areas. 
                 </p>
 
-                <a href="https://openknowledgemaps.org/viper/project?id=6fd986ceb58e4e9b48fdd21f7f53abde" target="_blank">
+                <a href="https://openknowledgemaps.org/viper/project?id=3830ef0990652ae4adf20e5e545584c5" target="_blank">
                     <img style="max-width:900px; margin: 30px auto; background-color: #eff3f4; padding:5px; border-radius: 5px; moz-border-radius: 5px; width:100%;" src="./img/overview-openaire.png">
                 </a>
 
@@ -64,7 +64,7 @@ include 'config.php';
                     within the same interface.
                 </p>
 
-                <a href="https://openknowledgemaps.org/viper/project?id=6fd986ceb58e4e9b48fdd21f7f53abde" target="_blank">
+                <a href="https://openknowledgemaps.org/viper/project?id=3830ef0990652ae4adf20e5e545584c5" target="_blank">
                     <img style="max-width:900px; margin: 30px auto; background-color: #eff3f4; padding:5px; border-radius: 5px; moz-border-radius: 5px; width:100%;" src="./img/detail-openaire.png"></p>
                 </a>
 
