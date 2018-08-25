26c2b2ae85ea5ebc8550d39c5f3bb4fb5316e363
Christopher Kittel <web@christopherkittel.eu>
2018-08-26 01:22:08 +0200

    nasty )

--- server/preprocessing/other-scripts/cluster.R
+++ server/preprocessing/other-scripts/cluster.R
@@ -88,8 +88,8 @@ get_ndms <- function(distance_matrix, mindim=2, maxdim=2, maxit=500) {
   nm <- par.nmds(distance_matrix, mindim=mindim, maxdim=maxdim, maxit=maxit)
   nm.nmin = nmds.min(nm)
 
-  vclog$info(paste("NMDS-Stress:", min(nm$stress), sep=" ")))
-  vclog$info(paste("NMDS-R2:", min(nm$r2), sep=" ")))
+  vclog$info(paste("NMDS-Stress:", min(nm$stress), sep=" "))
+  vclog$info(paste("NMDS-R2:", min(nm$r2), sep=" "))
 
   if(exists("DEBUG") && DEBUG == TRUE) {
     # Plot results from multidimensional scaling, highlight clusters with symbols
