760e11276427d46d4be0f5b095beb8bc18e000ed
Christopher Kittel <web@christopherkittel.eu>
2018-08-25 22:26:36 +0200

    first steps of improved feature pipeline

--- server/preprocessing/other-scripts/vis_layout.R
+++ server/preprocessing/other-scripts/vis_layout.R
@@ -25,45 +25,51 @@ vlog <- getLogger('vis')
 # Expects the following metadata fields:
 # id, content, title, readers, published_in, year, authors, paper_abstract, subject
 
-vis_layout <- function(text, metadata, max_clusters=15, maxit=500, mindim=2, maxdim=2, lang="english",
-                       add_stop_words=NULL, testing=FALSE, taxonomy_separator=NULL, list_size=-1) {
+vis_layout <- function(text, metadata,
+                       max_clusters=15, maxit=500,
+                       mindim=2, maxdim=2,
+                       lang="english", add_stop_words=NULL,
+                       testing=FALSE, taxonomy_separator=NULL, list_size=-1) {
 
   start.time <- Sys.time()
 
   tryCatch({
    if(!isTRUE(testing)) {
      source('preprocess.R')
+     source('features.R')
      source('cluster.R')
      source('summarize.R')
      source('postprocess.R')
    } else {
      source('../preprocess.R')
+     source('../features.R')
      source('../cluster.R')
      source('../summarize.R')
      source('../postprocess.R')
    }
   }, error = function(err) print(err)
   )
+
+  vlog$debug("preprocess")
   filtered <- filter_duplicates(metadata, text, list_size)
   metadata <- filtered$metadata
   text <- filtered$text
-
   stops <- get_stopwords(lang, add_stop_words, testing)
+  corpus <- create_corpus(metadata, text, stops)
 
-  vlog$debug("calc matrix")
-  res <- create_corpus(metadata, text, stops)
-  corpus <- res$corpus
-  corpus_unstemmed <- res$corpus_unstemmed
-  metadata_full_subjects = replace_keywords_if_empty(corpus, metadata, stops)
-  tdm_matrix <- create_tdm_matrix(corpus)
-
-  vlog$debug("normalize matrix")
+  vlog$debug("get features")
+  tdm_matrix <- create_tdm_matrix(corpus$stemmed)
   normalized_matrix <- normalize_matrix(tdm_matrix);
 
-  vlog$debug("create clusters")
+  vlog$debug("get clusters")
   clusters <- create_clusters(normalized_matrix, max_clusters=max_clusters);
-  named_clusters <- create_cluster_labels(clusters, metadata_full_subjects, weightingspec="ntn", top_n=3, stops=stops, taxonomy_separator)
   layout <- create_ordination(normalized_matrix, maxit=500, mindim=2, maxdim=2)
+
+  vlog$debug("get cluster summaries")
+  metadata_full_subjects = replace_keywords_if_empty(corpus, metadata, stops)
+  named_clusters <- create_cluster_labels(clusters, metadata_full_subjects, weightingspec="ntn", top_n=3, stops=stops, taxonomy_separator)
+
+  vlog$debug("create output")
   output <- create_output(named_clusters, layout, metadata_full_subjects)
 
   end.time <- Sys.time()
