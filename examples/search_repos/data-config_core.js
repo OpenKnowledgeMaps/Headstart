80feb83ea55a0ce71c4a2ce7aa2fda3a43549cf8
Peter Kraker <pkraker@openknowledgemaps.org>
2018-08-29 14:40:47 -0700

    Added improved data-config for CORE

--- examples/search_repos/data-config_core.js
+++ examples/search_repos/data-config_core.js
@@ -5,18 +5,36 @@ var data_config = {
     service: "core",
 
     title: "",
-    base_unit: "views",
+    base_unit: "citations",
     use_area_uri: true,
     show_timeline: false,
     show_dropdown: false,
     preview_type: "pdf",
-    sort_options: ["readers", "title", "authors", "year"],
+    sort_options: ["relevance", "title", "authors", "year"],
     is_force_areas: true,
-    language: "eng",
+    language: "eng_pubmed",
     area_force_alpha: 0.015,
     show_list: true,
-    content_based: false,
+    content_based: true,
+    url_prefix: "https://core.ac.uk/display/",
 	
-	show_context: true,
-	create_title_from_context: true
+    show_context: true,
+    create_title_from_context: true,
+        
+    embed_modal: true,
+    share_modal: true,
+
+    doi_outlink: true,
+    filter_menu_dropdown: true,
+    sort_menu_dropdown: true,
+    filter_options: ["all", "open_access"],
+    
+    embed_modal: true,
+    share_modal: false,
+
+    doi_outlink: true,
+    filter_menu_dropdown: true,
+    sort_menu_dropdown: true,
+    filter_options: ["all", "open_access"],
+
 };
