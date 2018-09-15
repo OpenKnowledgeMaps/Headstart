fee4a75de052075695a9b5d1be36dc1c0caba50b
Christopher Kittel <web@christopherkittel.eu>
2018-09-15 20:01:10 +0200

    merged logging cleanup

--- server/preprocessing/other-scripts/test/linkedcat-test.R
+++ server/preprocessing/other-scripts/test/linkedcat-test.R
@@ -6,7 +6,6 @@ options(warn=1)
 
 wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(wd) #Don't forget to set your working directory
-Sys.setenv("HEADSTART_LOGFILE" = "test.log")
 
 query <- "Kommissionsbericht" #args[2]
 service <- "linkedcat"
@@ -14,10 +13,17 @@ params <- NULL
 params_file <- "params_linkedcat.json"
 
 source('../utils.R')
+DEBUG = FALSE
+
+if (DEBUG==TRUE){
+  setup_logging('DEBUG')
+} else {
+  setup_logging('INFO')
+}
+
 source("../vis_layout.R")
 source('../linkedcat.R')
 
-DEBUG = FALSE
 
 MAX_CLUSTERS = 15
 ADDITIONAL_STOP_WORDS = "english"
