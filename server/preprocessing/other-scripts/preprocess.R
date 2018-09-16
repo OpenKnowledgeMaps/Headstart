3d0505118be9d3ca7cb0bee45300a9d977d20793
Christopher Kittel <web@christopherkittel.eu>
2018-09-16 15:12:50 +0200

    fixed id warning OHE encoding

--- server/preprocessing/other-scripts/preprocess.R
+++ server/preprocessing/other-scripts/preprocess.R
@@ -88,8 +88,8 @@ replace_keywords_if_empty <- function(corpus, metadata, stops) {
 }
 
 get_OHE_feature <-function(metadata, feature_name) {
-  ohe_encoder <- onehot(metadata[c('id', feature_name)], stringsAsFactors = TRUE)
-  ohe_feat <- data.frame(predict(ohe_encoder, metadata[c('id', 'lang_detected')]))
+  ohe_encoder <- onehot(metadata[feature_name], stringsAsFactors = TRUE)
+  ohe_feat <- data.frame(predict(ohe_encoder, metadata[feature_name]))
   rownames(ohe_feat) <- metadata$id
   return(ohe_feat)
 }
