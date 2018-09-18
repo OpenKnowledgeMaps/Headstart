8f51585d306265622fa60d52c21bb3f2983d26b6
Christopher Kittel <web@christopherkittel.eu>
2018-09-18 23:41:26 +0200

    new logging framework

--- server/preprocessing/other-scripts/test/core-test.R
+++ server/preprocessing/other-scripts/test/core-test.R
@@ -13,13 +13,20 @@ service <- "core"
 params <- NULL
 params_file <- "params_core.json"
 
+source('../utils.R')
+DEBUG = FALSE
+
+if (DEBUG==TRUE){
+  setup_logging('DEBUG')
+} else {
+  setup_logging('INFO')
+}
+
 source("../vis_layout.R")
 source('../core.R')
-source('../utils.R')
-
-debug = FALSE
 
 MAX_CLUSTERS = 15
+LANGUAGE = "english"
 ADDITIONAL_STOP_WORDS = "english"
 
 if(!is.null(params_file)) {
@@ -29,6 +36,7 @@ if(!is.null(params_file)) {
 input_data = get_papers(query, params, limit=100)
 
 output_json = vis_layout(input_data$text, input_data$metadata, max_clusters=MAX_CLUSTERS,
+                         lang=LANGUAGE,
                          add_stop_words=ADDITIONAL_STOP_WORDS, testing=TRUE)
 
 print(output_json)
