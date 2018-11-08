87ee8e71cf293d7a85e0c2de16ebb74ed83fcbf3
tanteuschi <maxi@openknowledgemaps.org>
2018-11-08 13:34:07 +0100

    Added a skin for LinkedCat+ with a visual identity

--- examples/linkedcat/headstart.php
+++ examples/linkedcat/headstart.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+            <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet"> 
     </head>
 
     <body style="margin:0px; padding:0px">
