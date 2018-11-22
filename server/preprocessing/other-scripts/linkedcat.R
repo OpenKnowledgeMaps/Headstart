afad88c203b2638faba607ca423781097bd93340
Christopher Kittel <web@christopherkittel.eu>
2018-11-22 14:03:23 +0100

    includeable content

--- server/preprocessing/other-scripts/linkedcat.R
+++ server/preprocessing/other-scripts/linkedcat.R
@@ -97,9 +97,14 @@ build_query <- function(query, params, limit){
     temp <- paste0("-content_type_a:", ct)
     fq <- c(fq, temp)
   }
-
+  if (length(params$include_content_type) > 0) {
+    temp <- paste0("content_type_a:(",
+                   paste0(params$include_content_type, collapse=" OR "),
+                   ")")
+    fq <- c(fq, temp)
+  }
   q_params$fq <- fq
-
+  # end adding filter params
   return(q_params)
 }
 
