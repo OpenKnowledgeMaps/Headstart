5b5becd40e9ea5141c775a34d5245f3afacaff03
Christopher Kittel <web@christopherkittel.eu>
2018-11-22 15:41:42 +0100

    added content type params to search service

--- server/services/searchLinkedCat.php
+++ server/services/searchLinkedCat.php
@@ -11,7 +11,7 @@ $dirty_query = library\CommUtils::getParameter($_POST, "q");
 
 $post_params = $_POST;
 
-$result = search("linkedcat", $dirty_query, $post_params, array("from", "to"), ";", null);
+$result = search("linkedcat", $dirty_query, $post_params, array("from", "to", "exclude_content_type", "include_content_type"), ";", null);
 
 echo $result
 
