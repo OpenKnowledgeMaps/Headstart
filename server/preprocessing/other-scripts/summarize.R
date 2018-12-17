943f66fc5fc5e55c8c5a5b5393ef672266ea734a
Christopher Kittel <web@christopherkittel.eu>
2018-12-17 20:22:10 +0100

    fix stopword naming edge case

--- server/preprocessing/other-scripts/summarize.R
+++ server/preprocessing/other-scripts/summarize.R
@@ -51,7 +51,7 @@ create_cluster_labels <- function(clusters, metadata, lang,
   tfidf_top <- apply(nn_tfidf, 2, function(x) {x2 <- sort(x, TRUE);x2[x2>0]})
   empty_tfidf <- which(apply(nn_tfidf, 2, sum)==0)
   tfidf_top[c(empty_tfidf)] <- fill_empty_clusters(nn_tfidf, nn_corpus)[c(empty_tfidf)]
-  tfidf_top_names <- get_top_names(tfidf_top, top_n)
+  tfidf_top_names <- get_top_names(tfidf_top, top_n, stops)
   clusters$cluster_labels = ""
   for (k in seq(1, clusters$num_clusters)) {
     group = c(names(clusters$groups[clusters$groups == k]))
@@ -124,7 +124,7 @@ get_cluster_corpus <- function(clusters, metadata, stops, taxonomy_separator) {
 }
 
 
-get_top_names <- function(tfidf_top, top_n) {
+get_top_names <- function(tfidf_top, top_n, stops) {
   tfidf_top_names <- lapply(tfidf_top, names)
   tfidf_top_names <- lapply(tfidf_top_names, function(x) {another_prune_ngrams(x, stops)})
   tfidf_top_names <- lapply(tfidf_top_names, function(x) {x = gsub("_", " ", x); trim(x)})
