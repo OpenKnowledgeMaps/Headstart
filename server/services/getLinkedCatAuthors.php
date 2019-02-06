5951031be8c75123897febf02f2c4b19cf3a2337
Christopher Kittel <web@christopherkittel.eu>
2019-02-06 14:16:55 +0100

    wip commit

--- server/services/getLinkedCatAuthors.php
+++ server/services/getLinkedCatAuthors.php
@@ -10,8 +10,11 @@ use headstart\library;
 $INI_DIR = dirname(__FILE__) . "/../preprocessing/conf/";
 $ini_array = library\Toolkit::loadIni($INI_DIR);
 
-$url = $ini_array["connection"]["linkedcat_solr"] . 
-        'select?facet.field=author100_a_str&facet=on&fl=author100_a_str&facet.limit=-1&q=*:*';
+$url = "https://" .
+       $ini_array["connection"]["linkedcat_user"] . ":" .
+       $ini_array["connection"]["linkedcat_pwd"] . "@" .
+       $ini_array["connection"]["linkedcat_solr"] . "/solr/linkedcat/" .
+        "select?facet.field=author100_a_str&facet.query=author100_a_str&facet=on&fl=author100_a_str&q=*:*&rows=0";
 
 $ch = curl_init();
 
@@ -23,6 +26,10 @@ curl_setopt_array($ch, array(
 $jsonData = curl_exec($ch);
 
 $array = json_decode($jsonData, true);
-$retVal = array_values($array["facet_counts"]["facet_fields"]["author100_a_str"]);
+print_r($array);
+$authors = array_values($array["facet_counts"]["facet_fields"]["author100_a_str"]);
+echo json_encode($authors);
+
+$retVal = array_values();
 
 echo json_encode($retVal);
