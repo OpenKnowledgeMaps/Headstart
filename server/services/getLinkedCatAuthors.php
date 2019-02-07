18245f0bd3010832083d37d50e38f773a463f0c7
Christopher Kittel <web@christopherkittel.eu>
2019-02-07 14:14:03 +0100

    wip commit; query functions

--- server/services/getLinkedCatAuthors.php
+++ server/services/getLinkedCatAuthors.php
@@ -10,34 +10,39 @@ use headstart\library;
 $INI_DIR = dirname(__FILE__) . "/../preprocessing/conf/";
 $ini_array = library\Toolkit::loadIni($INI_DIR);
 
-$url = "https://" .
+$base_url = "https://" .
        $ini_array["connection"]["linkedcat_user"] . ":" .
        $ini_array["connection"]["linkedcat_pwd"] . "@" .
-       $ini_array["connection"]["linkedcat_solr"] . "/solr/linkedcat/" .
-        "select?facet.field=author100_a_str&facet.query=author100_a_str&facet=on&fl=author100_a_str&q=*:*&rows=0";
+       $ini_array["connection"]["linkedcat_solr"] . "/solr/linkedcat/";
 
-$ch = curl_init();
 
-curl_setopt_array($ch, array(
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_URL => $url
-));
+function execQuery($base_url, $query) {
+  $ch = curl_init();
+  curl_setopt_array($ch, array(
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_URL => $base_url . $query
+  ));
+  // make function out of initial author array getter
+  $jsonData = curl_exec($ch);
+  return $jsonData;
+}
+$author_facet_query = "select?facet.field=author100_a_str" .
+                      "&facet.query=author100_a_str" .
+                      "&facet=on&fl=author100_a_str" .
+                      "&q=*:*&rows=0";
+$author_data_query =
 
-$jsonData = curl_exec($ch);
-
-$resultarray = json_decode($jsonData, true);
+$author_facet = json_decode(execQuery($base_url, $author_facet_query), true);
 $authors = array();
 // [id, author100_a_str, doc_count, living_dates and possibly image_link]
-foreach ($resultarray["facet_counts"]["facet_fields"]["author100_a_str"] as $k => $v) {
+foreach ($author_facet["facet_counts"]["facet_fields"]["author100_a_str"] as $k => $v) {
   if ($k % 2 == 0) {
     $author = array();
     $author[] = $v;
-    $author[] = $resultarray["facet_counts"]["facet_fields"]["author100_a_str"][$k+1];
+    $author[] = $author_facet["facet_counts"]["facet_fields"]["author100_a_str"][$k+1];
     $authors[] = $author;
+    // write function to get additional author information
+    // user array_unshift($author, $author_id) to insert left
   }
 }
-echo json_encode($authors);
-
-$retVal = array_values();
-
 echo json_encode($authors);
