6d0dffe7865e2fac7b43f92bb6895839ab6c1700
Christopher Kittel <web@christopherkittel.eu>
2019-02-09 21:12:55 +0100

    first wip commit for authorview

---
+++ server/services/searchLinkedCatAuthorview.php
@@ -0,0 +1,23 @@
+<?php
+
+header('Content-type: application/json');
+
+require_once dirname(__FILE__) . '/../classes/headstart/library/CommUtils.php';
+require 'search.php';
+
+use headstart\library;
+
+$dirty_query = library\CommUtils::getParameter($_POST, "q");
+
+$post_params = $_POST;
+
+$result = search("linkedcat_authorview",
+                 $dirty_query,
+                 $post_params,
+                 array("id", "living_dates"),
+                 ";"
+               );
+
+echo $result
+
+?>
