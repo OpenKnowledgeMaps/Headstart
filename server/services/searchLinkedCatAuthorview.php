11a940267427c7540fc9e6cf74260d8232013de9
Christopher Kittel <web@christopherkittel.eu>
2019-02-14 19:56:18 +0100

    sixth param added to search

--- server/services/searchLinkedCatAuthorview.php
+++ server/services/searchLinkedCatAuthorview.php
@@ -14,9 +14,9 @@ $post_params = $_POST;
 $result = search("linkedcat_authorview",
                  $dirty_query,
                  $post_params,
-                 array("author_name", "doc_count", "living_dates", "image_link"),
-                 ";"
+                 array("author_id", "doc_count", "living_dates", "image_link"),
                  ";",
+                 null,
                );
 
 echo $result
