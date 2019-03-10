71015686f64c2c4dc62e8c068cab5a007c8a2494
Christopher Kittel <web@christopherkittel.eu>
2019-03-10 19:23:27 +0100

    wip new RCalculation for streamgraph

--- server/classes/headstart/preprocessing/calculation/RCalculation.php
+++ server/classes/headstart/preprocessing/calculation/RCalculation.php
@@ -13,39 +13,62 @@ use headstart\library;
 require_once 'Calculation.php';
 
 class RCalculation extends Calculation {
-      
+
     public function performCalculationAndWriteOutputToFile($working_dir) {
         $ini = $this->ini_array["calculation"];
         $output = $this->ini_array["output"];
-        
+
         $base_dir = $this->ini_array["general"]["preprocessing_dir"];
         $binary = $ini["binary"];
         $script = $base_dir . $ini["script"];
-        
+
         $path = '"' . $binary . '" ' .$script. ' "' . $working_dir . '" "'
                 . $output["cooc"] . '" "' . $output["metadata"] . '" "' . $output["output_scaling_clustering"] . '" "' . $ini["mode"] .'"';
-        
+
         library\Toolkit::info($path);
-        exec($path); 
+        exec($path);
     }
-    
+
     public function performCalculationAndReturnOutputAsJSON($working_dir, $query, $params, $service) {
         $ini = $this->ini_array["calculation"];
-        
+
         $base_dir = $this->ini_array["general"]["preprocessing_dir"];
         $binary = $ini["binary"];
         $script = $base_dir . $ini["script"];
-        
+
         $path = '"' . $binary . '" ' .$script. ' "' . $working_dir . '" "' . $query . '" "'
                 . $service . '"';
-        
+
         if($params != null) {
             $path .= ' "' . $params . '"';
         }
-        
+
         //library\Toolkit::info($path);
         exec($path, $output_r);
-        
+
         return $output_r;
+    }
+
+    public function performStreamgraphCalculation($output_json) {
+        $ini = $this->ini_array["calculation"];
+
+        $base_dir = $this->ini_array["general"]["preprocessing_dir"];
+        $working_dir = $this->$ini_array["general"]["preprocessing_dir"] . $this->$ini_array["output"]["output_dir"];
+        $binary = $ini["binary"];
+        $script = $base_dir . "other-scripts/streamgraph.R";
+
+        $tmp_json = tmpfile();
+        $tmp_meta = stream_get_meta_data($tmp_json);
+        $tmp_jsonname = $tmp_meta["uri"];
+        fwrite($tmp_json, $output_json);
+
+        $path = '"' . $binary . '" '
+                . $script
+                . ' "' . $working_dir . '" '
+                . '"' . $tmp_jsonname . '"';
+
+        exec($path, $streamgraph_json);
+
+        return $streamgraph_json;
     }
 }
