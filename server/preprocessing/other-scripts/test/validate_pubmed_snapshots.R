1be463032d733f6d59e2016fc1be2e0153371128
Christopher Kittel <web@christopherkittel.eu>
2019-07-01 23:26:30 +0200

    update commit id

--- server/preprocessing/other-scripts/test/validate_pubmed_snapshots.R
+++ server/preprocessing/other-scripts/test/validate_pubmed_snapshots.R
@@ -35,7 +35,7 @@ LANGUAGE = "english"
 ADDITIONAL_STOP_WORDS = "english"
 
 test_cases <- read.csv("queries.csv")
-commit_id <- "334154e" # latest master commit considered stable; switch to release tag later
+commit_id <- "60bb5eb" # latest master commit considered stable; switch to release tag later
 
 
 diffcols <- c("var.x", "var.y", "id", "values.x", "values.y", "row.x", "row.y", "query", "service")
