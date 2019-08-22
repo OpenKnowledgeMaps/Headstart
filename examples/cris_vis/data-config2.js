f6a3a1b879373e93c81b4ed6542068f937432c7c
pkraker <pkraker@openknowledgemaps.org>
2019-08-22 19:26:34 +0200

    Adaptation of Headstart for CRIS Vis 2

--- examples/cris_vis/data-config2.js
+++ examples/cris_vis/data-config2.js
@@ -2,12 +2,14 @@ var data_config = {
     tag: "visualization",
     mode: "local_files",
     
-    bubble_min_scale: 1.3,
+    bubble_min_scale: 1.2,
     bubble_max_scale: 1.1,
+    
+    paper_min_scale: 0.8,
 
-    title: "Übersicht für <b>Forschungsfragen zum Thema psychische Gesundheit</b>",
+    title: "Übersicht für <b>Forschungsfragen zum Thema Unfallverletzungen</b>",
     input_format: "json",
-    base_unit: "Nennungen",
+    base_unit: "Fragen",
     use_area_uri: true,
     preview_type: "pdf",
     
@@ -17,59 +19,39 @@ var data_config = {
     show_list:true,
     is_force_papers:true,
     is_title_clickable: false,
-    show_infolink_areas: true,
+    show_infolink_areas: false,
 	
     show_context: true,
     create_title_from_context: false,
     
-    scale_toolbar: true,
+    scale_toolbar: false,
     
     content_based: false,
-    list_sub_entries: false,
+    list_sub_entries: true,
+    list_sub_entries_readers: false,
+    list_sub_entries_statistics: false,
+    list_sub_entries_number: true,
     list_show_all_papers: false,
-    list_additional_images: true,
-    list_images: ["Altersgruppe", "Background", "Bundesland", "Geschlecht",
-                    "Höchste Ausbildung", "Land"],
-    list_images_path: "img/",
+    list_additional_images: false,
     
     is_force_areas: true,
     area_force_alpha: 0.02,
+    papers_force_alpha: 0.2,
     
-    language: "ger_cris",
+    language: "ger_cris_2",
     
     sort_options: ["title", "readers", "area"],
     
     sort_menu_dropdown: true,
-    visual_distributions: true,
-    cris_legend: true,
+    visual_distributions: false,
+    cris_legend: false,
     credit: true,
     
     is_evaluation: false,
     evaluation_service: "ga",
-    
-    rescale_map: false,
-    scale_types: ['none', 'Altersgruppe', 'Background', 'Bundesland', 'Geschlecht', 'Höchste Ausbildung', 'Land'],
-    scale_explanation: {
-        none: '',
-        Altersgruppe: 'Die Verteilung für <strong>Altersgruppe</strong> bezieht sich auf das Themenfeld/Thema. Anteile in absteigender Reihenfolge.',
-        Background: 'Die Verteilung für <strong>Background</strong> bezieht sich auf das Themenfeld/Thema. Anteile in absteigender Reihenfolge.',
-        Bundesland: 'Die Verteilung für <strong>Bundesländer AT</strong> bezieht sich auf das Themenfeld/Thema. Anteile in absteigender Reihenfolge.',
-        Geschlecht: 'Die Verteilung für <strong>Geschlecht</strong> bezieht sich auf das Themenfeld/Thema. Anteile in absteigender Reihenfolge.',
-        'Höchste Ausbildung': 'Die Verteilung für <strong>Höchste Ausbildung</strong> bezieht sich auf das Themenfeld/Thema. Anteile in absteigender Reihenfolge.',
-        Land: 'Die Verteilung für <strong>Land</strong> bezieht sich auf das Themenfeld/Thema. Anteile in absteigender Reihenfolge.',
-    },
-    scale_label: {
-        none: 'nichts ausgewählt',
-        Altersgruppe: 'Altersgruppe',
-        Background: 'Background',
-        Bundesland: 'Bundesländer AT',
-        Geschlecht: 'Geschlecht',
-        'Höchste Ausbildung': 'Höchste Ausbildung',
-        Land: 'Land',
-    },
 
     files: [{
         title: "edu1",
-        file: "./data/final_output_new.json"
+        file: "./data/final_output2_de.json"
     }],
 };
