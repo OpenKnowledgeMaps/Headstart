c63c1da3196938bb619539bc57223cdf087fd6ac
Christopher Kittel <web@christopherkittel.eu>
2019-12-13 13:14:28 +0100

    make dropdowns work again

--- examples/cris_vis/index2_en.php
+++ examples/cris_vis/index2_en.php
@@ -9,7 +9,6 @@ include 'config.php';
     <title>CRIS Vis II</title>
     <link rel="stylesheet" href="https://use.typekit.net/ift7wkn.css"></link>
     <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'></link>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
