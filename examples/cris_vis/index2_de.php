185936248e04d4c30b48dd041613a58e05d2c8e6
Christopher Kittel <web@christopherkittel.eu>
2020-01-11 13:51:32 +0100

    remove error-throwing bootstrap imports

--- examples/cris_vis/index2_de.php
+++ examples/cris_vis/index2_de.php
@@ -9,8 +9,6 @@ include 'config.php';
     <title>CRIS Vis II</title>
     <link rel="stylesheet" href="https://use.typekit.net/ift7wkn.css"></link>
     <link href='https://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'></link>
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 
 <body style="margin:0px; padding:0px; height:100%; position: relative;">
@@ -22,7 +20,7 @@ include 'config.php';
         data_config.server_url = window.location.href.replace(/[^/]*$/, '') + "../../server/";
         headstart.start();
     </script>
-    
+
     <!-- Piwik -->
     <script type="text/javascript">
       var _paq = _paq || [];
