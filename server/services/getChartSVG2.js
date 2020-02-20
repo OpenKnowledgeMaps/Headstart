0425b7b96268b06173ab9e946ae39345fee3ceaa
pkraker <pkraker@openknowledgemaps.org>
2020-02-20 01:45:25 +0100

    First version of snapshot feature based on puppeteer

---
+++ server/services/getChartSVG2.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+(async() => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({width: 1920, height: 1080})
+    await page.goto(process.argv[2], {waitUntil: 'networkidle2'});
+    await timeout(2000)
+    await page.screenshot({path: process.argv[3], clip: { x: 0, y: 0, width: 1150, height: 1080 }});
+    browser.close();
+
+})();
--- server/services/snapshot/headstart_snapshot.php
+++ server/services/snapshot/headstart_snapshot.php
@@ -9,17 +9,16 @@
         <script type="text/javascript" src="data-config_<?php echo $_GET['service'] ?>.js"></script>
 		<script src="../../../../js/search_options.js"></script>
         <script>
-            //data_config.title = '<?php echo 'Overview of <span id="search-term-unique">' . $_GET['query'] . '</span> based on <span id="num_articles"></span> ' . $_GET['service_name'] . ' articles'; ?>';
             data_config.files = [{
                     title: <?php echo json_encode($_GET['query']) ?>,
                     file: <?php echo json_encode($_GET['file']) ?>
             }];
-            data_config.nodot = 1;
+            //data_config.nodot = false;
             data_config.server_url = window.location.href.replace(/[^/]*$/, '') + "../../";
             data_config.show_context = true;
             data_config.create_title_from_context= true;
-			data_config.is_phantomjs = true;
-			data_config.options = options_<?php echo $_GET['service']; ?>.dropdowns;
+            //data_config.is_phantomjs = true;
+            data_config.options = options_<?php echo $_GET['service']; ?>.dropdowns;
         </script>
         <script type="text/javascript" src="../../../dist/headstart.js"></script>
         <link type="text/css" rel="stylesheet" href="../../../dist/headstart.css"></link>
