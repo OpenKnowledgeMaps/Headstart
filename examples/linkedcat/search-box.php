0153cb9db0b4b8c3c573b1d69c20ace091be0eee
tanteuschi <maxi@openknowledgemaps.org>
2020-02-21 17:09:39 +0100

    Faq page, rework of search box, text improvement and other bits and bobs

--- examples/linkedcat/search-box.php
+++ examples/linkedcat/search-box.php
@@ -1,24 +1,49 @@
 <div class="search">
-
     <form id="searchform" action="#" method="POST" target="_blank">
         <p class="library">
             <label class="radio-inline author-btn"><input type="radio" name="optradio" value="authors" class="radio-inv">
-                <span class="bold"><i class="fas fa-search"></i> Autoren</span></label>
+                <span class="bold"><i class="fas fa-search"></i> Autoren</span>
+            </label>
+
             <label class="radio-inline keyword-btn"><input type="radio" name="optradio" value="keywords" class="radio-inv">
-                <span class="bold"><i class="fas fa-search"></i> Stichwörter</span></label>
+                <span class="bold"><i class="fas fa-search"></i> Stichwörter</span>
+            </label>
         </p>
+
         <div style="background-color: white; border-radius: 0px 0px 5px 5px;">
             <div id="searchfield">
+
                 <div id="additional-information" class="additional-information"></div>
-                <label class="radio-button"><input type="radio" name="vis_type" value="overview" checked="checked"> 
-                    <span data-toggle="popover" data-trigger="hover" data-content='Eine Knowledge Map (zu deutsch "Wissenslandkarte") 
-                          gibt einen thematischen Überblick über ein Stichwort/einen Autor. 
-                             Mehr Infos dazu finden Sie in den FAQs.'>Knowledge Map</span></label>
-                <label class="radio-button"><input type="radio" name="vis_type" value="timeline"> 
-                    <span data-toggle="popover" data-trigger="hover" data-content="Ein Streamgraph gibt die zeitliche Entwicklung der 
-                          häufigsten Schlagworte zu einem Stichwort/Autor wider. Mehr Infos dazu finden Sie in den FAQs.">Streamgraph</span></label>
-                <div id="filter-container"></div>
-                 
+                <div style="margin-left: 30px;">
+                    <div>
+                        <label class="radio-button">
+                            <input type="radio" name="vis_type" value="overview" checked="checked">
+                            <span class="checkmark"></span>
+                            <span class="popover-selection">Knowledge Map
+                            </span>
+                        </label>
+                        <span class="info-btn" data-toggle="popover" data-trigger="hover" data-content='Eine Knowledge Map (zu deutsch "Wissenslandkarte") 
+                              gibt einen thematischen Überblick über ein Stichwort/einen Autor. 
+                              Mehr Infos dazu finden Sie in den FAQs.'>Was ist eine Knowledge Map?</span>
+                    </div>
+                    
+                    <div>
+                    <label class="radio-button">
+                        <input type="radio" name="vis_type" value="timeline">
+                        <span class="checkmark"></span>
+                        <span class=popover-selection>Streamgraph
+                        </span>
+                    </label>
+                    <span class="info-btn" data-toggle="popover" data-trigger="hover" data-content="Ein Streamgraph gibt die zeitliche Entwicklung der 
+                          häufigsten Schlagworte zu einem Stichwort/Autor wieder. Mehr Infos dazu finden Sie in den FAQs.">Was ist ein Streamgraph?<!--<i class="fas fa-info-circle"></i>--></span>
+                    </div>
+                </div>
+                
+                
+                <div id="filter-container">
+                    <p>Weitere Optionen:</p>
+                </div>
+
                 <!--<label for="q">Suchbegriff:</label>-->
                 <div class="searchfield" style="max-width:600px; padding: 0px 30px 50px; margin: 0px auto;">
                     <label id="q-error" class="q-error label-hide" for="q"></label>
@@ -29,7 +54,7 @@
                     </button>
                 </div>
             </div>
-            
+
             <div id="authors-loading" class="loading-indicator">
                 <img class="loading" src="img/ajax-loader.gif">
             </div>
@@ -42,6 +67,6 @@
                 </span>
             </p>-->
         </div>
-         
+
     </form>
 </div>
--- examples/linkedcat/search.js
+++ examples/linkedcat/search.js
@@ -17,7 +17,7 @@ var chooseOptions = function () {
         case "keywords":
             options = options_linkedcat;
             service_url = data_config.server_url + "services/searchLinkedCat.php";
-            placeholder = "Stichwort eingeben...";
+            placeholder = "Bitte geben Sie ein Stichwort ein...";
             $('.keyword-btn').addClass('btn-enabled');
             $('#additional-information').html(additional_information.keywords);
             break;
@@ -25,7 +25,7 @@ var chooseOptions = function () {
         case "authors":
             options = options_linkedcat_authors;
             service_url = data_config.server_url + "services/searchLinkedCatAuthorview.php";
-            placeholder = "Autor auswählen...";
+            placeholder = "Bitte wählen Sie einen Autor aus der Liste...";
             $('.author-btn').addClass('btn-enabled');
             $('#additional-information').html(additional_information.authors);
             break;
