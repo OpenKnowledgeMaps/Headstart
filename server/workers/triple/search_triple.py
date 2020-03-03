c092a48e350e1517c46ba728b0a5c1e196945800
chreman <web@christopherkittel.eu>
2020-03-03 16:30:31 +0100

    wip

--- server/workers/triple/search_triple.py
+++ server/workers/triple/search_triple.py
@@ -8,5 +8,10 @@ with open("config.json") as infile:
     config = json.load(infile)
 
 es = Elasticsearch(
-    ['{protocol}://{user}:{pass}@{url}:{port}'.format(**config)]
+    [config.get('host')],
+    http_auth=(config.get('user', config.get('pass'))),
+    scheme = "https",
+    port = config.get('port'),
+    send_get_body_as='POST',
+    http_compress=True
 )
