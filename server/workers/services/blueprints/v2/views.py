2839d53e776107adf0577948bf3862a2e0c58696
chreman <web@christopherkittel.eu>
2020-03-04 14:29:48 +0100

    wip

--- server/workers/services/blueprints/v2/views.py
+++ server/workers/services/blueprints/v2/views.py
@@ -6,3 +6,19 @@ from flask import Blueprint, make_response
 
 redis_store = redis.StrictRedis(host="localhost", port=6379, db=0)
 app = Blueprint('v2', __name__)
+
+
+@app.route('/api/v2/search_triple', methods=['GET', 'POST'])
+def search_triple():
+    """
+    """
+
+
+
+@app.route('/api/v2/example_data', methods=['GET', 'POST'])
+def example_data():
+    headers = {"Content-Type": "application/json"}
+    data = {"test": "document string"}
+    return make_response(data,
+                         200,
+                         headers=headers)
