7eaebc4a43b564ce47678c99ea07fe888693a8e9
Christopher Kittel <web@christopherkittel.eu>
2020-03-09 19:39:54 +0100

    swagger+restx added

--- server/workers/services/src/app.py
+++ server/workers/services/src/app.py
@@ -1,16 +1,15 @@
 from flask import Flask
+from apis import api
 
 
 def new_services_app(settings_override=None):
-    from blueprints.v2.views import app as v2
-    app = Flask('v2', instance_relative_config=True)
+    flask_app = Flask('v2', instance_relative_config=True)
 
-    app.config.from_object('config.settings')
-    app.config.from_pyfile('settings.py', silent=True)
+    flask_app.config.from_object('config.settings')
+    flask_app.config.from_pyfile('settings.py', silent=True)
 
-    app.register_blueprint(v2)
-
-    return app
+    api.init_app(flask_app)
+    return flask_app
 
 
 if __name__ == '__main__':
