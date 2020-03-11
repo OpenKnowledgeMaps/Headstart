10adf046a74acb759ce7c3cd12a98cfc01773498
Christopher Kittel <web@christopherkittel.eu>
2020-03-11 10:47:07 +0100

    introduce rate limiting

--- server/services/getLinkedCatAuthors.php
+++ server/services/getLinkedCatAuthors.php
@@ -63,6 +63,7 @@ function getAuthorData($base_url, $author_data_query, $author_ids) {
   $window = 100;
   $res = array();
   $mh = curl_multi_init();
+  curl_multi_setopt($mh, CURLMOPT_MAX_TOTAL_CONNECTIONS, 10);
   $urls = array();
   foreach ($author_ids as $i => $id) {
     if (strlen($id)>0) {
@@ -194,7 +195,7 @@ function loadOrRefresh($lc_cache) {
   # checks if file exists AND if its fresher than 24h
   # if true && true, load the cached file
   if (($GLOBALS['force_refresh'] == FALSE) &&
-       (file_exists($lc_cache) && (time() - filemtime($lc_cache) < 86400))
+       (file_exists($lc_cache) && (time() - filemtime($lc_cache) < 172800))
      ) {
     $authors = loadCache($lc_cache);
   }
