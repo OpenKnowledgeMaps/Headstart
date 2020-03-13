5ec5eae235831fc46de1701ab6da1742590b42bf
pkraker <pkraker@openknowledgemaps.org>
2020-03-13 21:03:32 +0100

    Enable exclusion of terms

--- server/preprocessing/other-scripts/test/base-test.R
+++ server/preprocessing/other-scripts/test/base-test.R
@@ -7,7 +7,7 @@ options(warn=1)
 wd <- dirname(rstudioapi::getActiveDocumentContext()$path)
 setwd(wd) #Don't forget to set your working directory
 
-query <- '"knowledge domain visualization" or ("knowledge map" and science)' #args[2]
+query <- '"knowledge discovery" -literature' #args[2]
 service <- "base"
 params <- NULL
 params_file <- "params_base.json"
