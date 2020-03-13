9a0cb140cd28c469976f9d8ae3e3e1ac8f734ef0
pkraker <pkraker@openknowledgemaps.org>
2020-03-13 20:38:32 +0100

    Further improved regex to avoid matching opening parentheses

--- server/preprocessing/other-scripts/base.R
+++ server/preprocessing/other-scripts/base.R
@@ -41,8 +41,9 @@ get_papers <- function(query, params, limit=100,
 
   exact_query <- ""
 
-  # add textus: to each word/phrase to enable verbatim search
-  exact_query = gsub("(\"(.*?)\")|(?!or|and\\b)(?<!\\S)(?=\\S)", "textus:\\1", query, perl=T)
+  # add "textus:" to each word/phrase to enable verbatim search
+  # make sure it is added after any opening parentheses to enable queries such as "(a and b) or (a and c)"
+  exact_query = gsub("(\"(.*?)\")|(?<=\\()|(?!or|and\\b)(?<!\\S)(?=\\S)(?!\\()", "textus:\\1", query, perl=T)
   
   blog$info(paste("BASE query:", exact_query))
 
