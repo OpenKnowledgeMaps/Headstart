8e1e80d8a1080e2d2d40fff9f4662e830ed49e28
tanteuschi <maxi@openknowledgemaps.org>
2020-03-13 18:38:48 +0100

    popup messages aaded additional facts and fixed other small issues

--- examples/linkedcat/faqs.php
+++ examples/linkedcat/faqs.php
@@ -40,7 +40,7 @@ $date = new DateTime();
                         Blasen, die sich thematisch weniger ähnlich sind. 
                     </p>
                     <p class="paragraph-style">
-                        Knowledge Maps haben ihren Ursprung bereits in den 1970er-Jahren. Weitere Informationen zu dem hier verwendeten Knowledge Map-Typ finden Sie in <a href="https://doi.org/10.31263/voebm.v72i2.3202" target="_blank">diesem Artikel</a>; einen allgemeinen Überblick über bei Knowledge Maps verwendeten Verfahren finden Sie bspw. in <a href="https://cns.iu.edu/docs/publications/2003-borner-arist.pdf" target="_blank">diesem Artikel</a>.
+                        Knowledge Maps haben ihren Ursprung bereits in den 1970er-Jahren. Weitere Informationen zu dem hier verwendeten Knowledge Map-Typ <a href="https://doi.org/10.31263/voebm.v72i2.3202" target="_blank">finden Sie in diesem Artikel</a>; einen allgemeinen Überblick über bei Knowledge Maps verwendeten Verfahren <a href="https://cns.iu.edu/docs/publications/2003-borner-arist.pdf" target="_blank">finden Sie bspw. in diesem Artikel</a>.
                     </p>
 
                     <h4 class="question">Was ist ein Streamgraph?</h4>
@@ -53,7 +53,7 @@ $date = new DateTime();
                     </p>
                     <p class="paragraph-style">
                         Ein Streamgraph zeigt die zeitliche Entwicklung der häufigsten Schlagworte zu einem Stichwort/Autor. 
-                        Die Schlagworte werden als farbige Ströme (Englisch "streams") dargestellt. Jedem Strom sind relevante Dokumente zugeordnet, die mit einem Klick auf einen Stream angezeigt werden können. Ein Dokument wird in einem Streamgraph mehreren Strömen zugeordnet.
+                        Die Schlagworte werden als farbige Ströme (Englisch "streams") dargestellt. Jedem Strom sind relevante Dokumente zugeordnet, die mit einem Klick auf einen Stream angezeigt werden können. Ein Dokument kann in einem Streamgraph mehreren Strömen zugeordnet.
                     </p>
                     <p class="paragraph-style">
                         Streamgraphen eignen sich besonders dazu, die Entwicklung von Schlagwörtern über die Zeit zu analysieren und Trends zu erkennen. Die Höhe eines Stroms entspricht der Anzahl der zugeordneten Dokumente zu einem bestimmten Zeitpunkt. 
@@ -64,7 +64,7 @@ $date = new DateTime();
                         Da das für die Interpolation verwendete Verfahren noch nicht perfekt ist, kann es dadurch vereinzelt auch zu grafischen Artefakten kommen; das bedeutet, dass Ströme an Stellen kleine Ausschläge aufweisen, obwohl zu diesem Zeitpunkt keine Dokumente für das Stichwort vorhanden sind. Im Zweifelsfall können Sie dies überprüfen, indem Sie mit der Maus über den Strom fahren und sich die Anzahl der Dokumente anzeigen lassen.
                     </p>
                     <p class="paragraph-style">
-                        Streamgraphen wurden im Jahr 2008 von Byron und Wattenberg ursprünglich für die New York Times entwickelt. Weitere Informationen finden Sie in <a href="https://leebyron.com/streamgraph/" target="_blank">diesem Artikel</a>.
+                        Streamgraphen wurden im Jahr 2008 von Byron und Wattenberg ursprünglich für die New York Times entwickelt. Weitere Informationen <a href="https://leebyron.com/streamgraph/" target="_blank">finden Sie in diesem Artikel</a>.
                     </p>
 
                     <h4 class="question">Was ist unter relevanteste Dokumente zu verstehen?</h4>
@@ -92,7 +92,7 @@ $date = new DateTime();
                     </ul>
                     <p class="paragraph-style">
                         Dabei gilt zu beachten, dass die Lage der Bereiche (Blasen) innerhalb einer Knowledge Map lediglich ein Hinweis auf thematische Ähnlichkeit sind. Die Anordnung wird beim Laden entzerrt um die Knowledge Map besser lesbar zu gestalten. 
-                        Die Lage eines Dokuments innerhalb eines Bereichs (Blase) hat keine Bedeutung. Um Überlappungen zu vermeiden wurden diese neu angeordnet. Mehr Informationen <a target="_blank" href="https://arxiv.org/abs/1412.6462">finden sie in diesem Artikel</a>.
+                        Die Lage eines Dokuments innerhalb eines Bereichs (Blase) hat keine Bedeutung. Um Überlappungen zu vermeiden wurden diese neu angeordnet. Mehr Informationen <a target="_blank" href="https://arxiv.org/abs/1412.6462">finden Sie in diesem Artikel</a>.
                     </p>
 
                     <h4 class="question">Warum sind manche Visualisierungen besser als andere?</h4>
