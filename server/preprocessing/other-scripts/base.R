8a7e8899de193e1a0d70ef76e2199aa3a2f2e329
pkraker <pkraker@openknowledgemaps.org>
2020-03-17 11:16:26 +0100

    Handle single inverted commas

--- server/preprocessing/other-scripts/base.R
+++ server/preprocessing/other-scripts/base.R
@@ -45,7 +45,7 @@ get_papers <- function(query, params, limit=100,
   
   # add "textus:" to each word/phrase to enable verbatim search
   # make sure it is added after any opening parentheses to enable queries such as "(a and b) or (a and c)"
-  exact_query = gsub('([\"]+(.*?)[\"]+)|(?<=\\(\\b|\\+|-\\"\\b)|(?!or\\b|and\\b|-[\\"\\(]*\\b)(?<!\\S)(?=\\S)(?!\\(|\\+)'
+  exact_query = gsub('([\"\']+(.*?)[\"\']+)|(?<=\\(\\b|\\+|-\\"\\b)|(?!or\\b|and\\b|-[\\"\\(]*\\b)(?<!\\S)(?=\\S)(?!\\(|\\+)'
                      , "textus:\\1", query_cleaned, perl=T)
   
   blog$info(paste("BASE query:", exact_query))
