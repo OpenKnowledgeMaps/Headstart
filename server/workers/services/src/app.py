6b7e22f7ab46224ea2824087711f1a3e8a6c9717
Christopher Kittel <web@christopherkittel.eu>
2020-03-17 23:55:00 +0100

    wip

--- server/workers/services/src/app.py
+++ server/workers/services/src/app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_restx import Api
+from flask_cors import CORS
 from apis.triple import triple_ns
 
 app = Flask('v1', instance_relative_config=True)
 app.config.from_object('config.settings')
 app.config.from_pyfile('settings.py', silent=True)
+CORS(app, expose_headers=["Content-Disposition"])
 
 api = Api(app=app, title="Head Start API", version="0.1",
           description="Head Start API demo",
-          endpoint='/api',
+          prefix='/api',
           doc='/api/docs')
 api.add_namespace(triple_ns, path='/triple')
 
