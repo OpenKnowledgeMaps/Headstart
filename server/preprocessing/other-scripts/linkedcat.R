d967e16b904582074965a5e99428b16d8c7edccd
Christopher Kittel <web@christopherkittel.eu>
2020-03-18 20:36:31 +0100

    negative phrases enabled

--- server/preprocessing/other-scripts/linkedcat.R
+++ server/preprocessing/other-scripts/linkedcat.R
@@ -203,9 +203,10 @@ build_queryfield_query <- function(field, query) {
   l_q <- length(unlist(strsplit(query, " ")))
   if (l_q > 1) {
     query <- gsub(" ", " AND ", query, perl=TRUE)
-    for (i in 1:l_q) {
-      query <- gsub('("[\\w+ äöü]+) AND ([äöü\\w \\.]+")', "\\1 \\2", query, perl=TRUE)
+    for (i in 1:l_q*l_q) {
+      query <- gsub('(\\-?"[\\w+ äöü]+) AND ([äöü\\w \\.]+")', "\\1 \\2", query, perl=TRUE)
     }
+    query <- gsub(' AND -', ' -', query)
     query <- paste0(field, ':', '(', query, ')', add_boost_factor(field))
   } else {
     query <- paste0(field, ':', '"', query, '"', add_boost_factor(field))
