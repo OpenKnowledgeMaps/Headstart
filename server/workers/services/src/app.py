78f81bf29e8c5e3587eec24695562e952521ce1d
Christopher Kittel <web@christopherkittel.eu>
2020-03-21 00:11:15 +0100

    revert proxy fix

--- server/workers/services/src/app.py
+++ server/workers/services/src/app.py
@@ -2,14 +2,12 @@ from flask import Flask
 from flask_restx import Api
 from flask_cors import CORS
 from apis.triple import triple_ns
-from werkzeug.contrib.fixers import ProxyFix
 
 
 app = Flask('v1', instance_relative_config=True)
 app.config.from_object('config.settings')
 app.config.from_pyfile('settings.py', silent=True)
 CORS(app, expose_headers=["Content-Disposition"])
-app = ProxyFix(app)
 
 api = Api(app=app, title="Head Start API", version="0.1",
           description="Head Start API demo",
