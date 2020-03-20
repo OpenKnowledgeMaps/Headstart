ca3ae2a07d4822120f402a05797b0c142b747f89
pkraker <pkraker@openknowledgemaps.org>
2020-03-21 00:33:26 +0100

    Adapt search services to deal with precomputed IDs

--- server/services/searchDOAJ.php
+++ server/services/searchDOAJ.php
@@ -8,10 +8,14 @@ require 'search.php';
 use headstart\library;
 
 $dirty_query = library\CommUtils::getParameter($_POST, "q");
+$precomputed_id = (isset($_POST["unique_id"]))?($_POST["unique_id"]):(null);
 
 $post_params = $_POST;
 
-$result = search("doaj", $dirty_query, $post_params, array("from", "to", "today", "sorting"), ";", null);
+$result = search("doaj", $dirty_query, $post_params
+                    , array("from", "to", "today", "sorting")
+                    , ";", null, true, true, null, 3
+                    , "area_uri", "subject", $precomputed_id);
 
 echo $result
 
