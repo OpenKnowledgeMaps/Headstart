7dcb1aac6083c89fbe5fe60b0dd6ebc29d13156f
Christopher Kittel <web@christopherkittel.eu>
2020-03-28 20:30:21 +0100

    more WIP

--- server/workers/services/src/app.py
+++ server/workers/services/src/app.py
@@ -2,9 +2,13 @@ from flask import Flask
 from flask_restx import Api
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from apis.triple import triple_ns
-from apis.triple import gsheets_ns
 from werkzeug.middleware.proxy_fix import ProxyFix
+
+from apis.triple import triple_ns
+from apis.gsheets import gsheets_ns
+from apis.gsheets import app as gsheets_bp
+
+from database import db
 from config import settings
 from utils.monkeypatches import ReverseProxied, __schema__, specs_url, _register_apidoc, inject_flasgger
 
@@ -26,19 +30,24 @@ def api_patches(app, settings):
     return api_fixed
 
 
-app = Flask('v1', instance_relative_config=True)
-app = inject_flasgger(app)
-db = SQLAlchemy(app)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_port=1, x_for=1, x_host=1, x_prefix=1)
-app.wsgi_app = ReverseProxied(app.wsgi_app)
-app.config.from_object('config.settings')
-app.config.from_pyfile('settings.py', silent=True)
-CORS(app, expose_headers=["Content-Disposition"])
-
-api = api_patches(app, settings)
-api.add_namespace(triple_ns, path='/triple')
-api.add_namespace(gsheets_ns, path='/gsheets')
+def create_app():
+    app = Flask('v1', instance_relative_config=True)
+    app = inject_flasgger(app)
+    app.config.from_object('config.settings')
+    app.config.from_pyfile('settings.py', silent=True)
+    db.init_app(app)
+    app.register_blueprint(gsheets_bp)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_port=1, x_for=1, x_host=1, x_prefix=1)
+    app.wsgi_app = ReverseProxied(app.wsgi_app)
+    CORS(app, expose_headers=["Content-Disposition"])
+    return app
 
 
 if __name__ == '__main__':
+    app = create_app()
+
+    api = api_patches(app, settings)
+    api.add_namespace(triple_ns, path='/triple')
+    api.add_namespace(gsheets_ns, path='/gsheets')
+
     app.run(host="localhost", port=5001, debug=True)
