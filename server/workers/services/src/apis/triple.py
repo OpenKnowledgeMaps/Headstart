6990b57416fffb28329935003673658e19eacdd0
Christopher Kittel <web@christopherkittel.eu>
2020-04-06 17:52:52 +0200

    fix serialization

--- server/workers/services/src/apis/triple.py
+++ server/workers/services/src/apis/triple.py
@@ -91,7 +91,7 @@ class Search(Resource):
                                    axis=1)
                     result = df.to_csv()
                 else:
-                    result = pd.read_json(result).to_csv()
+                    result = pd.DataFrame.from_records(result).to_csv()
                 headers["Content-Type"] = "text/csv"
                 headers["Content-Disposition"] = "attachment; filename={0}.csv".format(k)
             if data.get("raw") is True:
