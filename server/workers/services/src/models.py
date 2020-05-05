c3c817081606d2147cacdef86c44106a1435bf26
Christopher Kittel <web@christopherkittel.eu>
2020-05-05 15:36:35 +0200

    wip

--- server/workers/services/src/models.py
+++ server/workers/services/src/models.py
@@ -3,13 +3,11 @@ from database import db
 
 class Revisions(db.Model):
     rev_id = db.Column(db.Integer,
-                       db.ForeignKey('visualizations.vis_latest'),
                        nullable=False,
                        primary_key=True)
     rev_vis = db.Column(db.Text, nullable=False,
                         primary_key=True)
-    vis_query = db.Column(db.Text,
-                          db.ForeignKey('visualizations.vis_clean_query'))
+    vis_query = db.Column(db.Text)
     rev_user = db.Column(db.Text)
     rev_timestamp = db.Column(db.DateTime)
     rev_comment = db.Column(db.Text)
@@ -17,7 +15,7 @@ class Revisions(db.Model):
 
 
 class Visualizations(db.Model):
-    vis_id = db.Column(db.Text, nullable=False, unique=True,
+    vis_id = db.Column(db.Text, nullable=False, unique=False,
                        primary_key=True)
     vis_query = db.Column(db.Text)
     vis_clean_query = db.Column(db.Text)
