8115002e7f033b5c8c50c3ce15c396d3322e5d49
Christopher Kittel <web@christopherkittel.eu>
2020-05-29 13:19:30 +0200

    reduce loglevel for failing altmetrics from error to warn

--- server/preprocessing/other-scripts/altmetrics.R
+++ server/preprocessing/other-scripts/altmetrics.R
@@ -52,7 +52,7 @@ get_altmetrics <- function(dois){
       metrics <- altmetric_data(altmetrics(doi=doi))
       results <- rbind.fill(results, metrics)
     }, error = function(err){
-      alog$error(gsub("[\r\n]", "", paste(err, doi, sep=" ")))
+      alog$warn(gsub("[\r\n]", "", paste(err, doi, sep=" ")))
     })
   }
   return (results)
@@ -69,7 +69,7 @@ add_citations <- function(output){
     cc <- tryCatch({
       cr_citation_count(doi=doi, async=TRUE)
       }, error = function(err){
-          alog$error(gsub("[\r\n]", "", paste(err, doi, sep=" ")))
+          alog$warn(gsub("[\r\n]", "", paste(err, doi, sep=" ")))
           return(list(doi=doi, count=NA))
       })
     cit_count <- rbind(cit_count, cc)
