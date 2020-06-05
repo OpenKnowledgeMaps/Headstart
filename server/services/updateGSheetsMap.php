e8ec3a3dbce89e103aa445e9eefa3a15dc671ba7
Christopher Kittel <web@christopherkittel.eu>
2020-06-05 14:38:57 +0200

    CLI + server call

--- server/services/updateGSheetsMap.php
+++ server/services/updateGSheetsMap.php
@@ -7,16 +7,24 @@ require 'search.php';
 
 use headstart\library;
 
-$shortopts = "";
-$longopts = array(
-  "q:",
-  "sheet_id:"
-);
 
-# parse options
-$options = getopt($shortopts, $longopts);
-$dirty_query = $options["q"];
-$sheet_id = $options["sheet_id"];
+if(php_sapi_name() == 'cli') {
+    // Called from command-line, maybe cron
+    # parse options
+    $shortopts = "";
+    $longopts = array(
+      "q:",
+      "sheet_id:"
+    );
+    $options = getopt($shortopts, $longopts);
+    $dirty_query = $options["q"];
+    $sheet_id = $options["sheet_id"];
+} elseif(php_sapi_name() == 'apache2handler') {
+    // Called from the apache2 webserver upon web request
+    $dirty_query = library\CommUtils::getParameter($_GET, "q");
+    $sheet_id = library\CommUtils::getParameter($_GET, "sheet_id");
+}
+
 
 $result = search("gsheets", $dirty_query, array("q" => $dirty_query, "sheet_id" => $sheet_id, "sheet_range" => "Resources!A1:O200")
                     , array("sheet_id")
