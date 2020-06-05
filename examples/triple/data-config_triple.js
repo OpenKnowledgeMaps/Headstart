bb4f10e8ec15c06b752505a3e1cecca744f7f5de
tanteuschi <maxi@openknowledgemaps.org>
2020-06-05 19:16:21 +0200

    added triple streamgraph colors

--- examples/triple/data-config_triple.js
+++ examples/triple/data-config_triple.js
@@ -31,5 +31,8 @@ var data_config = {
     embed_modal: true,
     share_modal: false,
 
-    backend: "api"
+    backend: "api",
+    
+    streamgraph_colors: ["#215A66", "#66214A", "#5D40FB", "#CB40FB", "#40C0FB", "#FB4068"
+        , "#FBB240", "#40FBC8", "#fee4bc", "#bcfeec", "#c6bcfe", "#febcca"],
 };
