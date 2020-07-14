7535f767b6971a12e91624e34d1ce3cb7b7c5a33
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-14 17:52:53 +0200

    backlink refactored

--- vis/test/example.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Adapter from 'enzyme-adapter-react-16';
-import { configure, shallow } from 'enzyme';
-import ExampleModernComponent from '../js/components/ExampleModernComponent';
-
-configure({adapter: new Adapter()});
-it('renders without crashing', () => {
-  shallow(<ExampleModernComponent />);
-});
