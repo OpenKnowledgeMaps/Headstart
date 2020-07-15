31b972ca6df98fbf91d5dd66c6f6c347fc81bb0a
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-15 15:51:12 +0200

    config alias added to jest + example test changed

--- vis/test/backlink.test.js
+++ vis/test/backlink.test.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Adapter from "enzyme-adapter-react-16";
 import { configure, shallow } from "enzyme";
-import Backlink from "../js/templates/Backlink";
+import { Backlink } from "../js/components/Backlink";
 
 configure({ adapter: new Adapter() });
 it("renders without crashing", () => {
-  shallow(
-    <Backlink label={"Go back!"} onClick={() => alert("...going back!")} />
-  );
+  shallow(<Backlink onClick={() => alert("...going back!")} />);
 });
