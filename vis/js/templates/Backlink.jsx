8dae8e8eb37ad085c5fc1e584a93313ad6c6744b
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-15 13:52:46 +0200

    comments added to the template

--- vis/js/templates/Backlink.jsx
+++ vis/js/templates/Backlink.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Backlink = ({ label, onClick }) => {
   return (
+    // html template starts here
     <p id="backlink" className="backlink">
       <a className="underline" onClick={onClick}>
         {label}
       </a>
     </p>
+    // html template ends here
   );
 };
 
