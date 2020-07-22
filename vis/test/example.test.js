79844f0f27351f65f51aeba32b015cd37f5420b1
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-22 11:50:28 +0200

    files with config import now testable

--- vis/test/example.test.js
+++ vis/test/example.test.js
@@ -3,7 +3,9 @@ import Adapter from 'enzyme-adapter-react-16';
 import { configure, shallow } from 'enzyme';
 import ExampleModernComponent from '../js/components/ExampleModernComponent';
 
+import config from "config";
+
 configure({adapter: new Adapter()});
 it('renders without crashing', () => {
-  shallow(<ExampleModernComponent />);
+  shallow(<ExampleModernComponent text={config.language} />);
 });
