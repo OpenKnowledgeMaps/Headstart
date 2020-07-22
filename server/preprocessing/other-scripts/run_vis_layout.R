c3bef32fc37b413e22db6b84171e22c195d7b7cc
chreman <web@christopherkittel.eu>
2020-07-22 11:09:09 +0200

    integration fix

--- server/preprocessing/other-scripts/run_vis_layout.R
+++ server/preprocessing/other-scripts/run_vis_layout.R
@@ -8,7 +8,7 @@ service <- args[3]
 setwd(wd) #Don't forget to set your working directory
 
 renv::activate()
-renv::restore(lockfile = './renv.lock')
+renv::restore(lockfile = '../renv.lock')
 Sys.setlocale(category="LC_ALL", locale = "en_US.UTF-8")
 
 library(tibble)
