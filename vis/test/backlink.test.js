cef781bfeed231fb8c3b85e3a6fc9c3f75fc18e8
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-22 16:54:29 +0200

    tests global setup added

--- vis/test/backlink.test.js
+++ vis/test/backlink.test.js
@@ -1,9 +1,7 @@
 import React from "react";
-import Adapter from "enzyme-adapter-react-16";
-import { configure, shallow } from "enzyme";
+import { shallow } from "enzyme";
 import { Backlink } from "../js/components/Backlink";
 
-configure({ adapter: new Adapter() });
 it("renders without crashing", () => {
   shallow(<Backlink onClick={() => alert("...going back!")} />);
 });
