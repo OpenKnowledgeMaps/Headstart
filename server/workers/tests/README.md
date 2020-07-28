44431f75e7415777d4cf4896763d3ed29d2aa60f
chreman <web@christopherkittel.eu>
2020-07-28 14:57:48 +0200

    stopword title exclusion test

--- server/workers/tests/README.md
+++ server/workers/tests/README.md
@@ -4,3 +4,5 @@
 Currently, some tests require the dockerized backend to run when testing e.g. data clients or the dataprocessing. This is because the R-parts of the backend are not easily integrated into the current testing framework.
 
 Tests are run from the server/workers folder by running pytest in an environment (e.g. anaconda) that has the requirements (requirements.txt) installed.
+
+Additionally, language data (e.g. stopwords) need to be downloaded for NLTK `python -m nltk.downloader all`
--- server/workers/tests/test_summarization.py
+++ server/workers/tests/test_summarization.py
@@ -1,7 +1,20 @@
 import pytest
-from .test_helpers import CASES, RESULTS
+from .test_helpers import CASES, RESULTS, get_stopwords
 
+LANGS = ["english", "german"]
 
 @pytest.mark.parametrize("testcase_", RESULTS)
 def test_empty_area_titles(testcase_):
     assert testcase_.area.map(lambda x: len(x)==0).sum() == 0
+
+@pytest.mark.parametrize("testcase_", RESULTS)
+@pytest.mark.parametrize("lang_", LANGS)
+def test_stopwords_not_start_end_keywords_areatitles(testcase_, lang_):
+    stops = get_stopwords(lang_)
+    areatitles = testcase_.area.unique()
+    for at in areatitles:
+        keywords = at.split(", ")
+        for kw in keywords:
+            tokens = kw.split(" ")
+            assert tokens[0] not in stops
+            assert tokens[-1] not in stops
