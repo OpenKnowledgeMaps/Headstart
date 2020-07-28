021c57003f1f8458a1e304b7a97cb4f0a2f59021
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-28 14:18:38 +0200

    add backlink-streamgraph class to the backlink

--- vis/js/components/Backlink.js
+++ vis/js/components/Backlink.js
@@ -1,11 +1,11 @@
 import React from "react";
 import config from "config";
 import { connect } from "react-redux";
-import { zoomOut } from "../actions"
+import { zoomOut } from "../actions";
 
 import BacklinkTemplate from "../templates/Backlink";
 
-export const Backlink = ({ hidden, dispatch }) => {
+export const Backlink = ({ hidden, streamgraph, dispatch }) => {
   if (hidden) {
     return null;
   }
@@ -13,14 +13,15 @@ export const Backlink = ({ hidden, dispatch }) => {
   return (
     <BacklinkTemplate
       label={config.localization[config.language].backlink}
-      onClick={() => dispatch( zoomOut() )}
+      onClick={() => dispatch(zoomOut())}
+      className={streamgraph ? "backlink backlink-streamgraph" : "backlink"}
     />
   );
 };
 
 const mapStateToProps = (state) => ({
   hidden: !state.zoom,
-  streamgraph: state.chartType === 'streamgraph'
+  streamgraph: state.chartType === "streamgraph",
 });
 
 export default connect(mapStateToProps)(Backlink);
