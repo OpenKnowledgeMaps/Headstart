a8390b056aff73bd5097b5b090dd1de1d755c555
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-28 12:59:27 +0200

    bugfix: streamgraph + modern frontend

--- vis/js/reducers/zoom.js
+++ vis/js/reducers/zoom.js
@@ -1,13 +1,12 @@
 const zoom = (state = null, action) => {
-    switch (action.type) {
-      case "ZOOM_IN":
-        return true;
-      case "ZOOM_OUT":
-          return false
-      default:
-        return state;
-    }
-  };
-  
-  export default zoom;
-  
+  switch (action.type) {
+    case "ZOOM_IN":
+      return true;
+    case "ZOOM_OUT":
+      return false;
+    default:
+      return state;
+  }
+};
+
+export default zoom;
