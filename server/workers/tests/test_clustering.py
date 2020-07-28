c14af272f2eea9504aebae55ff13af71afb3455b
chreman <web@christopherkittel.eu>
2020-07-28 12:43:49 +0200

    test n=2 clustering

--- server/workers/tests/test_clustering.py
+++ server/workers/tests/test_clustering.py
@@ -1,10 +1,19 @@
+import json
 import pytest
-from .test_helpers import CASES, RESULTS
+import pandas as pd
+from .test_helpers import CASES, RESULTS, get_dataprocessing_result
 
 
 @pytest.mark.parametrize("testcase_", CASES)
 def test_clustering_2_items(testcase_):
-    pass
+    metadata = pd.DataFrame.from_records(json.loads(testcase_["input_data"]["metadata"]))
+    text = pd.DataFrame.from_records(json.loads(testcase_["input_data"]["text"]))
+    metadata_sample = metadata.sample(n=2, random_state=42)
+    text_sample = text.sample(n=2, random_state=42)
+    testcase_["input_data"]["metadata"] = metadata_sample.to_json(orient='records')
+    testcase_["input_data"]["text"] = text_sample.to_json(orient='records')
+    test_result = get_dataprocessing_result(testcase_)
+    assert len(test_result) == 2
 
 
 @pytest.mark.parametrize("testcase_", RESULTS)
