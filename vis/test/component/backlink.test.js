5f0d317814e9d69be2a990a22271eb41a988e1b0
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-28 17:03:10 +0200

    added more tests (full coverage achieved)  - integration redux-react  - missing template test

--- vis/test/component/backlink.test.js
+++ vis/test/component/backlink.test.js
@@ -3,7 +3,13 @@ import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import { shallow } from "enzyme";
 
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+
+import BacklinkContainer from "../../js/components/Backlink";
+import BacklinkTemplate from "../../js/templates/Backlink";
 import { Backlink } from "../../js/components/Backlink";
+import { zoomOut } from "../../js/actions";
 
 const setup = (overrideProps) => {
   const props = Object.assign(
@@ -18,11 +24,6 @@ const setup = (overrideProps) => {
 
   return { wrapper, props };
 };
-
-/**
- * Redux state tests are missing.
- * They probably belong in a separate file...
- */
 
 /**
  * All the tests for the Backlink component, including its template, can be found here.
@@ -53,13 +54,7 @@ describe("Backlink component", () => {
     });
   });
 
-  /**
-   * Component event tests
-   *   using react-dom, because we need full DOM for that
-   *
-   * More component integration tests can be added here too.
-   */
-  describe("onclick", () => {
+  describe("dom tests", () => {
     // suppressing the info warns in the backlink onClick handler
     global.console = {
       log: jest.fn(),
@@ -81,34 +76,84 @@ describe("Backlink component", () => {
       container = null;
     });
 
-    it("triggers the onClick function when clicked", () => {
-      const onClick = jest.fn();
-      act(() => {
-        render(<Backlink onClick={onClick} />, container);
+    /**
+     * Component event tests
+     *   using react-dom, because we need full DOM for that
+     *
+     * More component integration tests can be added here too.
+     */
+    describe("onclick", () => {
+      it("triggers the onClick function when clicked", () => {
+        const onClick = jest.fn();
+        act(() => {
+          render(<Backlink onClick={onClick} />, container);
+        });
+
+        const link = document.querySelector("a.underline");
+        act(() => {
+          link.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+        });
+
+        expect(onClick).toHaveBeenCalledTimes(1);
       });
 
-      const link = document.querySelector("a.underline");
-      act(() => {
-        link.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-      });
+      it("doesn't crash when onClick is not a function", () => {
+        const onClick = null;
+        act(() => {
+          render(<Backlink onClick={onClick} />, container);
+        });
 
-      expect(onClick).toHaveBeenCalledTimes(1);
+        const link = document.querySelector("a.underline");
+        act(() => {
+          link.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+        });
+      });
     });
 
-    it("doesn't crash when onClick is not a function", () => {
-      const onClick = null;
-      act(() => {
-        render(<Backlink onClick={onClick} />, container);
-      });
+    /**
+     * Redux/React integration tests
+     */
+    describe("onclick (redux integration)", () => {
+      const mockStore = configureStore([]);
 
-      const link = document.querySelector("a.underline");
-      act(() => {
-        link.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+      it("should zoom out when clicked", () => {
+        const store = mockStore({
+          zoom: true,
+          chartType: "normal",
+        });
+
+        act(() => {
+          render(
+            <Provider store={store}>
+              <BacklinkContainer />
+            </Provider>,
+            container
+          );
+        });
+
+        const link = document.querySelector("a.underline");
+        act(() => {
+          link.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+        });
+
+        const actions = store.getActions();
+        const expectedPayload = zoomOut();
+
+        expect(actions).toEqual([expectedPayload]);
       });
     });
   });
+});
 
+describe("Backlink template", () => {
   /**
-   * Redux/React integration tests probably belong here...
+   * Basic render test
+   *   using Enzyme for shallow rendering (no children rendered)
    */
+  it("renders with the default classname", () => {
+    const wrapper = shallow(
+      <BacklinkTemplate label="sample text" onClick={jest.fn()} />
+    );
+    expect(wrapper.exists()).toBe(true);
+  });
 });
