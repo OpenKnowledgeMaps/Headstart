b7728b568d6e9f8cf0220e036fa66d20690e364f
chreman <web@christopherkittel.eu>
2020-07-28 12:31:24 +0200

    add kmap schema

--- server/workers/tests/test_metadata_schema.py
+++ server/workers/tests/test_metadata_schema.py
@@ -58,6 +58,46 @@ pubmed_schema = Schema([
 
 
 knowledgemap_schema = Schema([
+    Column('id', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('relation', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('identifier', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('title', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('paper_abstract', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('published_in', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('year', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('subject_orig', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('subject', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('authors', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('link', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('oa_state', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('url', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('relevance', [CustomElementValidation(
+                    lambda x: isinstance(x, int), "Not an integer.")]),
+    Column('lang_detected', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('cluster_labels', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('x', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('y', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
+    Column('area_uri', [CustomElementValidation(
+                    lambda x: isinstance(x, int), "Not an integer.")]),
+    Column('area', [CustomElementValidation(
+                    lambda x: isinstance(x, str), "Not a string.")]),
 ])
 
 
@@ -83,6 +123,9 @@ def test_metadata_schema(testcase_):
                                                 for e in service_errors])
 
 
-@pytest.mark.parametrize("testcase_", CASES)
+@pytest.mark.parametrize("testcase_", RESULTS)
 def test_knowledgemap_schema(testcase_):
-    pass
+    errors = knowledgemap_schema.validate(
+                        testcase_, columns=knowledgemap_schema.get_column_names())
+    assert len(errors) == 0, "\n".join([report_validation_error(e)
+                                             for e in errors])
