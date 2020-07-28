3f34b90227e60e4de7518395bc0ddac9f0cd1279
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-28 13:56:28 +0200

    updated Backlink test - fixed test import - added test for streamgraph

--- vis/test/snapshot/backlink.test.js
+++ vis/test/snapshot/backlink.test.js
@@ -1,22 +1,40 @@
 import React from "react";
-import { Provider } from 'react-redux';
+import { Provider } from "react-redux";
 import renderer from "react-test-renderer";
-import configureStore from 'redux-mock-store';
+import configureStore from "redux-mock-store";
 
-import { Backlink } from "../../js/components/Backlink";
+import Backlink from "../../js/components/Backlink";
 
 const mockStore = configureStore([]);
 
-describe( 'Backlink component snapshot', () => {
-    it("matches as snapshot", () => {
-        const store = mockStore({
-            zoom: true,
-          })
-        const tree = renderer.create(
-          <Provider store={store}>
-            <Backlink />
-          </Provider>
-        ).toJSON();
-        expect(tree).toMatchSnapshot();
-      });
-} )
+describe("Backlink component snapshot", () => {
+  it("normal matches as snapshot", () => {
+    const store = mockStore({
+      zoom: true,
+      chartType: "normal",
+    });
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Backlink />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it("streamgraph matches as snapshot", () => {
+    const store = mockStore({
+      zoom: true,
+      chartType: "streamgraph",
+    });
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Backlink />
+        </Provider>
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
