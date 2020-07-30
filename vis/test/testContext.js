5033fd52b9033251970889a74a5ee71df538b9af
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-30 16:25:48 +0200

    WIP: heading tests

---
+++ vis/test/testContext.js
@@ -0,0 +1,24 @@
+const context = {
+  id: "6d42b6d93313afaea1e709a49a9c4658",
+  query: "Hammer-Purgstall, Joseph",
+  service: "linkedcat_authorview",
+  timestamp: "2020-06-24 14:02:29",
+  params: {
+    title: "Sample Title",
+    acronym: "Sample acronym",
+    project_id: 123,
+    today: "2020-06-24",
+    author_id: "(DE-588)118545426",
+    doc_count: "91",
+    living_dates: "1774-1856",
+    image_link:
+      "http://commons.wikimedia.org/wiki/Special:FilePath/Hammer%20Purgstall.jpg",
+    vis_type: "timeline",
+  },
+  num_documents: 91,
+  share_oa: 91,
+  num_datasets: 0,
+  num_papers: 0,
+};
+
+export default context;
