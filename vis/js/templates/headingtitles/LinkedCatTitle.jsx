8f56ccd9a38b0bb957b5169e56ac59888c4cc40b
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-07-30 16:25:25 +0200

    WIP: heading components

---
+++ vis/js/templates/headingtitles/LinkedCatTitle.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LinkedCatTitle = ({ label, fullTitle, shortTitle }) => {
+  return (
+    // html template starts here
+    <>
+      {label}{" "}
+      <span id="search-term-unique" title={fullTitle}>
+        {shortTitle}
+      </span>
+    </>
+    // html template ends here
+  );
+};
+
+export default LinkedCatTitle;
