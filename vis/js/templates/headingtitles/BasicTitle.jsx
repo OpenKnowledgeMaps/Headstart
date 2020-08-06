b5336f974bb9e931dd9007d9a11d71bbb244f4fa
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-08-06 12:58:37 +0200

    bugfix: covis title with html tags

--- vis/js/templates/headingtitles/BasicTitle.jsx
+++ vis/js/templates/headingtitles/BasicTitle.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 const BasicTitle = ({ title }) => {
-  // no HTML yet
-  return <>{title}</>;
+  // dangerously Setting the Inner HTML because of e.g. Covis, where the basic title is HTML
+  // the span must be here so the dangerouslySetInnerHTML works
+  return <span dangerouslySetInnerHTML={{ __html: title }}></span>;
 };
 
 export default BasicTitle;
