182ee7073135ed699a8ddf2291d5a0d6d2fefe6b
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-08-06 13:41:45 +0200

    code format with prettier

--- vis/js/reducers/localization.js
+++ vis/js/reducers/localization.js
@@ -1,7 +1,10 @@
 const localization = (state = {}, action) => {
   switch (action.type) {
     case "INITIALIZE":
-      return Object.assign({}, action.configObject.localization[action.configObject.language]);
+      return Object.assign(
+        {},
+        action.configObject.localization[action.configObject.language]
+      );
     default:
       return state;
   }
--- vis/js/reducers/query.js
+++ vis/js/reducers/query.js
@@ -1,11 +1,12 @@
 const query = (state = null, action) => {
-    switch (action.type) {
-      case "INITIALIZE":
-        return typeof action.contextObject.query !== "undefined" ? action.contextObject.query : null;
-      default:
-        return state;
-    }
-  };
-  
-  export default query;
-  
+  switch (action.type) {
+    case "INITIALIZE":
+      return typeof action.contextObject.query !== "undefined"
+        ? action.contextObject.query
+        : null;
+    default:
+      return state;
+  }
+};
+
+export default query;
