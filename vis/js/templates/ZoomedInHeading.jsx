ce83e1f7ce8c9fb2969b137612eb87eafb6f47d8
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-08-06 12:04:58 +0200

    escaping issue solved

--- vis/js/templates/ZoomedInHeading.jsx
+++ vis/js/templates/ZoomedInHeading.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
+// TODO remove the dangerouslySetInnerHTML when we know where the title comes from
 const ZoomedInHeading = ({ label, title }) => {
   return (
     // html template starts here
     <h4>
       <span id="area-bold">{label}:</span>{" "}
-      <span id="area-not-bold">{title}</span>
+      <span id="area-not-bold" dangerouslySetInnerHTML={{ __html: title }}></span>
     </h4>
     // html template ends here
   );
