41db6719d72ce51bf8fbb1f211ffa56ecf655446
Jan Konstant <jkonstant@openknowledgemaps.org>
2020-08-25 16:41:53 +0200

    spaces between context line items changed

--- vis/js/templates/contextfeatures/PaperCount.jsx
+++ vis/js/templates/contextfeatures/PaperCount.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PaperCount = ({ value, label }) => {
   return (
     // html template starts here
-    <span id="context-paper_count">
+    <span id="context-paper_count" className="context_item">
       {value} {label}
     </span>
     // html template ends here
