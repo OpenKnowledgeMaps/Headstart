7a45b1efd9a5b1827ef945295768c687b4fe4b1a
Jan Konstant <456545@mail.muni.cz>
2020-09-16 15:17:48 +0200

    popovers warning fixed and moved them back to the right

--- vis/js/components/SubdisciplineTitle.js
+++ vis/js/components/SubdisciplineTitle.js
@@ -4,14 +4,16 @@ import Heading from "./Heading";
 import Backlink from "./Backlink";
 import ContextLine from "./ContextLine";
 
-export const SubdisciplineTitle = () => {
-  return (
-    <div id="subdiscipline_title">
-      <Heading />
-      <Backlink />
-      <ContextLine />
-    </div>
-  );
-};
+class SubdisciplineTitle extends React.Component {
+  render() {
+    return (
+      <div id="subdiscipline_title" style={{position: "relative"}}>
+        <Heading />
+        <Backlink />
+        <ContextLine popoverContainer={this} />
+      </div>
+    );
+  }
+}
 
 export default SubdisciplineTitle;
