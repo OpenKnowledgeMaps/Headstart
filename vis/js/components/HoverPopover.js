07ee46e7a2398a59e990f66540ffa015f8dedf46
Christopher Kittel <web@christopherkittel.eu>
2020-10-01 15:45:27 +0200

    HoverPopover Hotfix

--- vis/js/components/HoverPopover.js
+++ vis/js/components/HoverPopover.js
@@ -7,7 +7,7 @@ const HoverPopover = ({
   content,
   children,
   container, // = null
-  placement = "right",
+  placement = "bottom",
 }) => {
   const popover = <Popover id={id}>{content}</Popover>;
 
