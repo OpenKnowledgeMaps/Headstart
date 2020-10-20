5667e90043475954eb0f96a17e89f72582639825
Christopher Kittel <web@christopherkittel.eu>
2020-10-20 12:29:43 +0200

    Update text_similarity.R

--- server/preprocessing/other-scripts/text_similarity.R
+++ server/preprocessing/other-scripts/text_similarity.R
@@ -46,7 +46,7 @@ if (!is.null(params$vis_type)) {
     vis_type <- 'overview'
 }
 
-.GlobalEnv$MAP_ID <- params$vis_id
+.GlobalEnv$MAP_ID <- params$map_id
 taxonomy_separator = NULL
 limit = 100
 list_size = -1
