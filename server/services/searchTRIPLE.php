f62965dc2ace4eb805562dec37976b5fe08fa079
Christopher Kittel <web@christopherkittel.eu>
2020-10-22 15:10:11 +0200

    attempt correct escaping in query strings

--- server/services/searchTRIPLE.php
+++ server/services/searchTRIPLE.php
@@ -15,7 +15,7 @@ $post_params = $_POST;
 $result = search("triple", $dirty_query
                 , $post_params, array("from", "to", "sorting", "vis_type", "language", "limit", "sg_method")
                 , ";", null, true, true, null, 3
-                , "area_uri", "subject", $precomputed_id, false
+                , "area_uri", "subject", $precomputed_id, true
                 , "api");
 
 echo $result
