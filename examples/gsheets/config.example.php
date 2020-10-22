2667f7278da912b24b8722913854df2dc8000421
pkraker <pkraker@openknowledgemaps.org>
2020-10-22 23:09:30 +0200

    First version of the GSheets example

---
+++ examples/gsheets/config.example.php
@@ -0,0 +1,13 @@
+<?php
+$SITE_URL = "//localhost/covis/";
+$HEADSTART_PATH = "../../";
+$TOPIC = "Research Topic";
+$CURATOR_NAME = "Curator Name";
+$CURATOR_EMAIL = "curator@email.net";
+$SHEET_ID = "";
+$DEBUG = false;
+$PIWIK_ENABLED = false;
+$GA_ENABLED = false;
+$GA_CODE = "XX-00000000-0";
+$COOKIE_DOMAIN = "";
+?>
