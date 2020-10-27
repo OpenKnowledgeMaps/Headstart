74e247c2ec535bfe897e3c074d5c79f0937da3dd
Jan Konstant <456545@mail.muni.cz>
2020-10-27 15:54:49 +0100

    capitalized first letters of the dropdown items

--- vis/js/components/FilterDropdown.js
+++ vis/js/components/FilterDropdown.js
@@ -1,14 +1,16 @@
 import { connect } from "react-redux";
 
+import { capitalize } from "../utils/string";
+
 import FilterDropdownTemplate from "../templates/filtersort/FilterDropdown";
 import { filter } from "../actions";
 
 const mapStateToProps = (state) => ({
   value: state.list.filterValue,
-  valueLabel: state.localization[state.list.filterValue],
+  valueLabel: capitalize(state.localization[state.list.filterValue]),
   options: state.list.filterOptions.map((id) => ({
     id,
-    label: state.localization[id],
+    label: capitalize(state.localization[id]),
   })),
   label: state.localization.filter_by_label,
 });
