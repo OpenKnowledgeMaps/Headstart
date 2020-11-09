7c9366c11055b88c25c95bd833091a7e53348ea6
pkraker <pkraker@openknowledgemaps.org>
2020-11-09 20:35:28 +0100

    WIP: Add context from map json to index.php

--- examples/gsheets/config.example.php
+++ examples/gsheets/config.example.php
@@ -1,9 +1,6 @@
 <?php
 $SITE_URL = "//localhost/covis/";
 $HEADSTART_PATH = "../../";
-$TOPIC = "Research Topic";
-$CURATOR_NAME = "Curator Name";
-$CURATOR_EMAIL = "curator@email.net";
 $SHEET_ID = "";
 $DEBUG = false;
 $PIWIK_ENABLED = false;
