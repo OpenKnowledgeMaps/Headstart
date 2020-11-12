ec7029a0713b1df5bb8384d6ec4f90a0e7b65f2d
Christopher Kittel <web@christopherkittel.eu>
2020-11-12 18:47:38 +0100

    multiple db wip

--- server/workers/services/src/manage.py
+++ server/workers/services/src/manage.py
@@ -1,10 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import app
-from models import Visualizations, Revisions
+from models import Visualizations, Revisions, TripleRevisions, TripleVisualizations
 from database import db
 
 
 if __name__ == '__main__':
     db.init_app(app)
     with app.app_context():
-        db.create_all()
+        db.create_all(bind=None)
+        db.create_all(bind=['triple', 'gsheets', 'openaire'])
