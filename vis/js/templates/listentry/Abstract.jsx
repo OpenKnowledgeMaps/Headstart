d8be8b9c419aac12fa5bbe5f56dbe7d13a6845e2
Jan Konstant <456545@mail.muni.cz>
2020-11-17 10:06:09 +0100

    fixed the abstract escaping bug

--- vis/js/templates/listentry/Abstract.jsx
+++ vis/js/templates/listentry/Abstract.jsx
@@ -3,10 +3,12 @@ import React from "react";
 import Highlight from "../../components/Highlight";
 
 const Abstract = ({ text }) => {
+  let sanitizedText = $("<textarea />").html(text).text();
+
   return (
     // html template starts here
     <p id="list_abstract">
-      <Highlight queryHighlight>{text}</Highlight>
+      <Highlight queryHighlight>{sanitizedText}</Highlight>
     </p>
     // html template ends here
   );
