b9db01bb7604b9655b9fb4855ec1958f9a9e63fa
Jan Konstant <456545@mail.muni.cz>
2020-11-19 11:55:04 +0100

    moved the linkedcat color change to react code

--- vis/js/components/FilterSort.js
+++ vis/js/components/FilterSort.js
@@ -5,22 +5,23 @@ import StandardFilterSort from "../templates/filtersort/StandardFilterSort";
 import BasicFilterSort from "../templates/filtersort/BasicFilterSort";
 import ButtonFilterSort from "../templates/filtersort/ButtonFilterSort";
 
-const FilterSort = ({ showList, showFilter, showDropdownSort }) => {
+const FilterSort = ({ showList, showFilter, showDropdownSort, color }) => {
   if (showFilter) {
-    return <StandardFilterSort displaySort={showList} />;
+    return <StandardFilterSort displaySort={showList} color={color} />;
   }
 
   if (showDropdownSort) {
-    return <BasicFilterSort displaySort={showList} />;
+    return <BasicFilterSort displaySort={showList} color={color} />;
   }
 
-  return <ButtonFilterSort displaySort={showList} />;
+  return <ButtonFilterSort displaySort={showList} color={color} />;
 };
 
 const mapStateToProps = (state) => ({
   showList: state.list.show,
   showFilter: state.list.showFilter,
   showDropdownSort: state.list.showDropdownSort,
+  color: state.selectedBubble ? state.selectedBubble.color : null,
 });
 
 export default connect(mapStateToProps)(FilterSort);
--- vis/js/templates/filtersort/StandardFilterSort.jsx
+++ vis/js/templates/filtersort/StandardFilterSort.jsx
@@ -3,10 +3,14 @@ import FilterDropdown from "../../components/filtersort/FilterDropdown";
 import SearchBox from "../../components/filtersort/SearchBox";
 import SortDropdown from "../../components/filtersort/SortDropdown";
 
-const StandardFilterSort = ({ displaySort }) => {
+const StandardFilterSort = ({ displaySort, color }) => {
   return (
     // html template starts here
-    <div id="explorer_options" className="row">
+    <div
+      id="explorer_options"
+      className="row"
+      style={{ borderBottom: color ? "5px solid " + color : "" }}
+    >
       <SearchBox />
       <FilterDropdown />
       {!!displaySort && <SortDropdown />}
