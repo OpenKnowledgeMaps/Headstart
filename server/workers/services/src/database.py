4543ad0b9ddd9f74d53d81c61b1d62439f9d60be
Christopher Kittel <web@christopherkittel.eu>
2020-12-11 00:24:47 +0100

    db robustness under load

--- server/workers/services/src/database.py
+++ server/workers/services/src/database.py
@@ -8,4 +8,4 @@ db = SQLAlchemy()
 sessions = {}
 sessions[settings.POSTGRES["db"]] = sessionmaker(bind=create_engine(settings.SQLALCHEMY_DATABASE_URI))
 for data_integration, database in settings.SQLALCHEMY_BINDS.items():
-    sessions[data_integration] = sessionmaker(bind=create_engine(database))
+    sessions[data_integration] = sessionmaker(bind=create_engine(database, pool_recycle=3600))
