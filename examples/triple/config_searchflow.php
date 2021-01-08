be0e6fbfbcde2950029b012177e8237950e9a34b
pkraker <pkraker@openknowledgemaps.org>
2021-01-08 15:59:02 +0100

    Fix config bugs

---
+++ examples/triple/config_searchflow.php
@@ -0,0 +1,110 @@
+<?php
+
+$search_flow_config_local = array(
+    "params_arrays" => array(
+        "triple_km" => array("from", "to", "sorting", "language", "limit")
+            , "triple_sg" => array("from", "to", "sorting", "language", "limit", "sg_method")
+    )
+);
+
+?>
+<script>
+var search_flow_config_local = {
+    search_options: {
+            options: [
+                { id: "triple_sg", name: "Streamgraph TRIPLE", disabled: false, default: false 
+                    , text: "Streamgraph", description: ""
+                    , script: "searchTRIPLE.php", milliseconds_progressbar: 800
+                    , max_length_search_term_short: 115, timeout: 120000
+
+                }
+                , { id: "triple_km", name: "Knowledge Map TRIPLE", disabled: false, default: true 
+                    , text: "Knowledge Map", description: ""
+                    , script: "searchTRIPLE.php", milliseconds_progressbar: 800
+                    , max_length_search_term_short: 115, timeout: 120000
+                }
+            ] 
+            , examples: {
+                examples_integration1: {
+                    example_text: "Try out:",
+                    examples: [
+                        {text: "covid-19", link: "url"}
+                    ]
+                }
+            }
+            , filter_options: {
+
+                options_triple_km: {
+                    dropdowns: [
+                        {id: "year_range", multiple: false, name: "Time Range", type: "dropdown"
+                            , fields: [
+                                {id: "any-time-years", text: "Any time"}
+                                , {id: "this-year", text: "This year"}
+                                , {id: "last-year-years", text: "Last year"}
+                                , {id: "user-defined", text: "Custom range", class: "user-defined",
+                                    inputs: [
+                                        {id: "from", label: "From: ", class: "time_input"}
+                                        , {id: "to", label: "To: ", class: "time_input"}
+                                    ]}
+                            ]},
+                        {id: "sorting", multiple: false, name: "Sorting", type: "dropdown"
+                            , fields: [
+                                {id: "most-relevant", text: "Most relevant"}
+                                , {id: "most-recent", text: "Most recent"}
+                            ]},
+                        {id: "language", multiple: false, name: "Language", type: "dropdown"
+                            , fields: [
+                                {id: "all", text: "All languages"},
+                                {id: "en", text: "English"},
+                                {id: "fr", text: "Français"},
+                                {id: "es", text: "Español"}
+                            ]},
+                        {id: "limit", multiple: false, name: "Limit", type: "dropdown"
+                            , fields: [
+                                {id: "100", text: "100 documents"},
+                                {id: "1000", text: "1000 documents"}
+                            ]}
+                    ]}
+                , options_triple_sg: {
+                    dropdowns: [
+                        {id: "year_range", multiple: false, name: "Time Range", type: "dropdown"
+                            , fields: [
+                                {id: "any-time-years", text: "Any time"}
+                                , {id: "this-year", text: "This year"}
+                                , {id: "last-year-years", text: "Last year"}
+                                , {id: "user-defined", text: "Custom range", class: "user-defined",
+                                    inputs: [
+                                        {id: "from", label: "From: ", class: "time_input"}
+                                        , {id: "to", label: "To: ", class: "time_input"}
+                                    ]}
+                            ]},
+                        {id: "sorting", multiple: false, name: "Sorting", type: "dropdown"
+                            , fields: [
+                                {id: "most-relevant", text: "Most relevant"}
+                                , {id: "most-recent", text: "Most recent"}
+                            ]},
+                        {id: "language", multiple: false, name: "Language", type: "dropdown"
+                            , fields: [
+                                {id: "all", text: "All languages"},
+                                {id: "en", text: "English"},
+                                {id: "fr", text: "Français"},
+                                {id: "es", text: "Español"}
+                            ]},
+                        {id: "limit", multiple: false, name: "Limit", type: "dropdown"
+                            , fields: [
+                                {id: "100", text: "100 documents"},
+                                {id: "1000", text: "1000 documents"}
+                            ]}
+                        , {id: "sg_method", multiple: false, name: "Streamgraph method", type: "dropdown"
+                            , fields: [
+                                {id: "count", text: "Count"},
+                                {id: "tfidf", text: "TF-IDF"},
+                                {id: "nmf", text: "NMF"},
+                                {id: "lda", text: "LDA"}
+                            ]}
+                    ]}
+            }
+            
+        }
+    }
+ </script>
--- examples/triple/search.php
+++ examples/triple/search.php
@@ -1,9 +1,3 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <?php
 include 'config.php';
 include 'config_searchflow.php';
