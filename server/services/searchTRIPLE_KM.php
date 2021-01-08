dc4e56f669d09e3cce1d2b0149ee005d7cc8f157
Christopher Kittel <web@christopherkittel.eu>
2021-01-08 20:10:19 +0100

    backend-layer work in progress

---
+++ server/services/searchTRIPLE_KM.php
@@ -0,0 +1,25 @@
+<?php
+
+header('Content-type: application/json');
+
+require_once dirname(__FILE__) . '/../classes/headstart/library/CommUtils.php';
+require 'search.php';
+
+use headstart\library;
+
+$dirty_query = library\CommUtils::getParameter($_POST, "q");
+$precomputed_id = (isset($_POST["unique_id"]))?($_POST["unique_id"]):(null);
+
+$post_params = $_POST;
+
+$result = search("triple", $dirty_query
+                , $post_params, array("from", "to", "sorting", "language", "limit")
+                , ";", null, true
+                , true, null, 3
+                , "area_uri", "subject"
+                , $precomputed_id, true
+                , "api");
+
+echo $result
+
+?>
