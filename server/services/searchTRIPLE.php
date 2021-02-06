346dc5800cc2eafe5ea2f59b862b8d66116a2848
Christopher Kittel <web@christopherkittel.eu>
2021-02-06 18:15:42 +0100

    triple param cleanup

--- server/services/searchTRIPLE.php
+++ server/services/searchTRIPLE.php
@@ -14,13 +14,24 @@ $persistence_backend = (isset($_POST["persistence_backend"]))?($_POST["persisten
 $post_params = $_POST;
 unset($post_params["persistence_backend"]);
 
+if (isset($post_params["optradio"]) && $post_params["optradio"] === "triple_km") {
+  $post_params["vis_type"] = "overview";
+  $param_types = array("from", "to", "sorting", "language", "limit");
+  $repo = $post_params["optradio"];
+}
+if (isset($post_params["optradio"]) && $post_params["optradio"] === "triple_sg") {
+  $post_params["vis_type"] = "timeline";
+  $param_types = array("from", "to", "sorting", "language", "limit", "sg_method");
+  $repo = $post_params["optradio"];
+}
+unset($post_params["optradio"]);
+
 $result = search("triple", $dirty_query
-                , $post_params, array("from", "to", "sorting", "vis_type", "language", "limit", "sg_method")
+                , $post_params, $param_types
                 , ";", null, true
                 , true, null, 3
                 , "area_uri", "subject"
-                , $precomputed_id, true
-                , "api", $persistence_backend);
+                , $precomputed_id, true);
 
 echo $result
 
