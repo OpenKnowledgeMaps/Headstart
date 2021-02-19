4557c6fa4998c9ae6082a0d17693ad22e6b3ca0a
pkraker <pkraker@openknowledgemaps.org>
2021-02-19 13:51:30 +0100

    Adapted to new inc structure

--- examples/triple/index.php
+++ examples/triple/index.php
@@ -9,16 +9,14 @@ include 'config_searchflow.php';
     <meta http-equiv="Content-Type">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <?php include('search-flow/inc/head-search-form.php') ?>
+    <?php include('search-flow/inc/shared/head-search-form.php') ?>
 </head>
 
 <body style="padding-left:10px; padding-right:10px;">
-    <?php include('search-flow/inc/banner-browser-unsupported.php') ?>
+    <?php include('search-flow/inc/shared/banner-browser-unsupported.php') ?>
     <div>
         <h2>Search TRIPLE and turn it into a visualization</h2>
-        <?php include('search-flow/inc/search-form.php') ?>
-    <div style="margin-top:20px ">Built with <a href="https://github.com/OpenKnowledgeMaps/Headstart" target="_blank ">Head Start</a>. All content retrieved from <a href="https://www.gotriple.eu/" target="_blank ">TRIPLE</a>.
-    </div>
+        <?php include('search-flow/inc/search-form/search-form.php') ?>
     <script type="text/javascript " src="search-flow/js/search_form.js "></script>
 </body>
 
--- examples/triple/search.php
+++ examples/triple/search.php
@@ -1,4 +1,5 @@
 <?php
+include 'search-flow/inc/waiting-page/waiting-page-header.php';
 include 'config.php';
 ?>
 <html>
@@ -9,18 +10,16 @@ include 'config.php';
     <meta http-equiv="Content-Type">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
-    <?php include('search-flow/inc/head-search-form.php') ?>
+    <?php include('search-flow/inc/shared/head-search-form.php') ?>
 </head>
 
 <body style="padding-left:10px; padding-right:10px;">
-        <?php include('search-flow/inc/banner-browser-unsupported.php') ?>
+        <?php include('search-flow/inc/shared/banner-browser-unsupported.php') ?>
         <?php             
-            include('search-flow/inc/waiting-page.php') 
+            include('search-flow/inc/waiting-page/waiting-page.php') 
         ?>
     
     <script type="text/javascript " src="search-flow/js/search.js "></script>
-    
-    <div style="margin-top:20px ">Built with <a href="https://github.com/OpenKnowledgeMaps/Headstart" target="_blank ">Head Start</a>. All content retrieved from <a href="https://www.gotriple.eu/" target="_blank ">TRIPLE</a>.
     </div>
 </body>
 
