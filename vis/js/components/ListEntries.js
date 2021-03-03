074b1834152de3ae09eeb86deec7f3024e31452d
Jan Konstant <456545@mail.muni.cz>
2021-03-03 15:49:43 +0100

    empty list message corner case implemented

--- vis/js/components/ListEntries.js
+++ vis/js/components/ListEntries.js
@@ -8,14 +8,36 @@ import EntriesWrapper from "./listentries/EntriesWrapper";
 
 import { useLocalizationContext } from "./LocalizationProvider";
 
-const ListEntries = ({ show, service, displayedData }) => {
+import { filterData } from "../utils/data";
+import { STREAMGRAPH_MODE } from "../reducers/chartType";
+
+const ListEntries = ({
+  show,
+  service,
+  displayedData,
+  rawData,
+  searchSettings,
+  filterSettings,
+  isStreamgraph,
+}) => {
   const localization = useLocalizationContext();
 
   if (!show) {
     return null;
   }
 
-  if (displayedData.length === 0) {
+  let showEmptyMessage = displayedData.length === 0;
+  if (!isStreamgraph && showEmptyMessage) {
+    // we have to perform knowledge map filtering here (it's different
+    // to the filtering in the List.js component - paper selection is 
+    // not taken into account)
+    // if the list is empty, but there are visible papers in the zoomed
+    // bubble, the message is not displayed
+    const papers = filterData(rawData, searchSettings, filterSettings);
+    showEmptyMessage = papers.length === 0;
+  }
+
+  if (showEmptyMessage) {
     return (
       <EntriesWrapper>
         <div className="empty-area-warning">
@@ -39,6 +61,19 @@ const ListEntries = ({ show, service, displayedData }) => {
 const mapStateToProps = (state) => ({
   show: state.list.show,
   service: state.service,
+  rawData: state.data.list,
+  searchSettings: {
+    value: state.list.searchValue,
+  },
+  filterSettings: {
+    value: state.list.filterValue,
+    field: state.list.filterField,
+    zoomed: state.zoom,
+    area: state.selectedBubble ? state.selectedBubble.uri : null,
+    isStreamgraph: state.chartType === STREAMGRAPH_MODE,
+    title: state.selectedBubble ? state.selectedBubble.title : null,
+  },
+  isStreamgraph: state.chartType === STREAMGRAPH_MODE,
 });
 
 export default connect(mapStateToProps)(ListEntries);
