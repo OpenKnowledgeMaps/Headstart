a0b4946608c1794d3702570b7cb6a7128ba2177d
chreman <web@christopherkittel.eu>
2021-05-06 13:41:42 +0200

    remove outer reload button

--- examples/ideabox/index.php
+++ examples/ideabox/index.php
@@ -52,7 +52,6 @@ $project_website = ($project_website_raw !== null)?(addScheme($project_website_r
 <body class="knowledge-map" style="margin:0px; padding:0px; height:100%;">
     <div id="visualization" class="headstart"></div>
     <div id="errors" class="errors-container"></div>
-    <div id="reload" class="reload-button"><i class="fas fa-redo"></i><span id="reload-text"> An update is available <br><a id="reload" class="dismiss-reload">reload now</a> or <a id="dismiss-reload" class="dismiss-reload">do it later</a></span></div>
     <script type="text/javascript">
         
         function displayErrors(errors) {
@@ -112,19 +111,6 @@ $project_website = ($project_website_raw !== null)?(addScheme($project_website_r
             });
         }
         
-        function updateCheck(context) {
-            let last_update = (typeof context !== "undefined" && context.hasOwnProperty("last_update"))?(context.last_update):("");
-            
-            $.getJSON("<?php echo $headstart_url ?>server/services/GSheetUpdateAvailable.php?vis_id=<?php echo $SHEET_ID ?>&gsheet_last_updated=" + encodeURIComponent(last_update),
-                        function(output) {
-                            if (output.update_available) {
-                                $("#reload").addClass("show-reload-button");
-                                $("#reload-text").removeClass("hide-reload-text");
-                                window.clearInterval(check_update);
-                            }
-                        });
-        }
-        
         <?php if(isset($DEBUG) && $DEBUG === true): ?>
             function updateMap() {
                 $.getJSON("<?php echo $headstart_url ?>server/services/updateGSheetsMap.php?q=covis&sheet_id=<?php echo $SHEET_ID ?>",
@@ -135,15 +121,6 @@ $project_website = ($project_website_raw !== null)?(addScheme($project_website_r
             var update_map = window.setInterval(updateMap, 45000);
         <?php endif; ?>
         
-        let elem = document.getElementById('visualization');
-        var check_update = null;
-        
-        elem.addEventListener('headstart.data.loaded', function(e) {
-            let errors = e.detail.data.errors;
-            displayErrors(errors);
-            check_update = window.setInterval(updateCheck, 6000, e.detail.data.context);
-            
-        });
                    
     </script>
     <script type="text/javascript" src="data-config.js"></script>
@@ -214,15 +191,6 @@ $project_website = ($project_website_raw !== null)?(addScheme($project_website_r
                 $("#visualization").css("height", div_height + "px")
             });
             $(window).trigger('resize');
-
-            $("#reload").on("click", function () {
-                location.reload();
-            });
-
-            $("#dismiss-reload").on("click", function (event) {
-                $("#reload-text").addClass("hide-reload-text");
-                event.stopPropagation() 
-            });
         });
     </script>
 
