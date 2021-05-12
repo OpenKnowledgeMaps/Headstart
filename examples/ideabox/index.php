506da24121cd95178694c140ac147bed06910a64
Jan Konstant <456545@mail.muni.cz>
2021-05-12 11:55:34 +0200

    removed error box completely

--- examples/ideabox/index.php
+++ examples/ideabox/index.php
@@ -50,66 +50,8 @@ $project_website = ($project_website_raw !== null)?(addScheme($project_website_r
 </head>
 
 <body class="knowledge-map" style="margin:0px; padding:0px; height:100%;">
-    <div id="visualization" class="headstart"></div>
-    <div id="errors" class="errors-container"></div>
+    <div id="visualization" class="headstart" style="height:100vh;"></div>
     <script type="text/javascript">
-        
-        function displayErrors(errors) {
-            if(errors.length > 0) {
-                $("#errors").addClass("show-errors")
-                
-                let errors_info =
-                    $("<p/>", {
-                        id: "errors-info"
-                        , class: "errors-info"
-                        , html: '<i id="expand-icon" class="fa fa-plus-circle expand-icon" aria-hidden="true"/> The following errors were detected in the data sheet:</i>'
-                    });
-                $("#errors").append(errors_info);
-                
-                let errors_table =
-                     $("<table/>", {
-                         id: "errors-table"
-                         , class: "errors-table errors-table-hidden"
-                     });   
-                
-                $("#errors").append(errors_table);
-                
-                $("<tr/>", {
-                    id: "top-row"
-                    , class: "top-row"
-                }).appendTo("#errors-table");
-
-                for (let field of ["Row", "Column", "Details"]) {
-                    $("<th/>", {
-                        class: "error-row-top"
-                        , text: field
-                    }).appendTo("#top-row");
-
-                }
-                
-                for (let error_num in errors) {
-                    let current_id = "error-row-" + error_num;
-                    $("<tr/>", {
-                        id: current_id
-                        , class: "error-row-entry"
-                    }).appendTo("#errors-table")
-                    
-                    for (let field of ["row", "column", "reason"]) {
-                        $("<td/>", {
-                            id: "error-row-row"
-                            , class: "error-row-entry"
-                            , text: errors[error_num][field]
-                        }).appendTo("#" + current_id)
-                    }
-                }
-                                          
-            }
-            
-            $("#errors-info").on("click", function () {
-                $("#errors-table").toggleClass("errors-table-hidden");
-                $("#expand-icon").toggleClass("fa-minus-circle");
-            });
-        }
         
         <?php if(isset($DEBUG) && $DEBUG === true): ?>
             function updateMap() {
@@ -120,7 +62,6 @@ $project_website = ($project_website_raw !== null)?(addScheme($project_website_r
 
             var update_map = window.setInterval(updateMap, 45000);
         <?php endif; ?>
-        
                    
     </script>
     <script type="text/javascript" src="data-config.js"></script>
@@ -145,55 +86,6 @@ $project_website = ($project_website_raw !== null)?(addScheme($project_website_r
                 headstart.start();
             })
         
-    </script>
-        
-    <script>
-        var calcDivHeight = function () {
-
-            let height = $(window).height();
-            let width = $(window).width();
-            let calculated_height = 0;
-            let calculation_method = "";
-
-            if(height <= 730 || width < 904 || (width >= 985 && width  < 1070)) {
-                calculation_method = "Height calculation min_height";
-                calculated_height = 689;              
-             } else if (width >= 904 && width <= 984) {
-                 calculation_method = "Height calculation no. 1";
-                calculated_height = 670 + (width - 904);
-            } else if (height >= 890 && width >= 1070 && width < 1400) {
-                calculation_method = "Height calculation no. 2";
-                calculated_height = 670 + (width - 1070)/2;
-            } else if (width >= 1441 && height >= 1053) {
-                calculation_method = "Height calculation large";
-                calculated_height = 1000; 
-            } else if (height >= 988 && height < 1053 && width >= 1404 && width < 1435) {
-                calculation_method = "Height calculation no. 3";
-                calculated_height = 670 + (width - 1170);
-            }  else {
-                calculation_method = "Height calculation default";
-                calculated_height = $(window).height() - $("header").outerHeight();
-            }
-            
-            <?php if(isset($DEBUG) && $DEBUG === true): ?>
-                console.log("Height: " +height);
-                console.log("Width: " +width);
-                console.log("Calculation method: " +calculation_method);
-                console.log("Calculated height: " +calculated_height);
-            <?php endif; ?>
-
-            return calculated_height;
-        }
-
-        $(document).ready( function () {
-            $(window).on("resize", function () {
-                let div_height = calcDivHeight();
-                // TODO do we even need to calculate height precisely?
-                // $("#visualization").css("min-height", div_height + "px")
-                $("#visualization").css("height", "100vh")
-            });
-            $(window).trigger('resize');
-        });
     </script>
 
 </body>
