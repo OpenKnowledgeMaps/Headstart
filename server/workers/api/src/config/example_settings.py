5baf0a934385fec48371bcbdf7bb630d7c26a872
chreman <web@christopherkittel.eu>
2021-05-13 12:36:01 +0200

    move persistence config to env

--- server/workers/api/src/config/example_settings.py
+++ server/workers/api/src/config/example_settings.py
@@ -1,22 +1,6 @@
 BEHIND_PROXY = True
-DEFAULT = {
-    'user': 'user',
-    'pw': 'pw',
-    'db': 'dev',
-    'host': '127.0.0.1',
-    'port': '5432',
-}
-TEST = {
-    'user': 'testuser',
-    'pw': 'testpassword',
-    'db': 'test',
-    'host': '127.0.0.1',
-    'port': '5432',
-}
-SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % DEFAULT
-SQLALCHEMY_BINDS = {
-    'test': 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % TEST
-}
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SWAGGER_BASEPATH = ""
+DEFAULT_DATABASE = "dev"
+DATABASES = ["test"]
 ENV = "development"
 DEBUG = True
--- server/workers/api/src/database.py
+++ server/workers/api/src/database.py
@@ -1,13 +1,27 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from config import settings
 
 
+bind_params = {
+    "user": os.getenv("POSTGRES_USER"),
+    "pw": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("POSTGRES_HOST"),
+    "port": os.getenv("POSTGRES_PORT"),
+    "db": settings.DEFAULT_DATABASE
+}
+
 sessions = {}
-sessions[settings.DEFAULT["db"]] = sessionmaker(bind=create_engine(settings.SQLALCHEMY_DATABASE_URI))
-for data_integration, database in settings.SQLALCHEMY_BINDS.items():
-    sessions[data_integration] = sessionmaker(bind=create_engine(database,
+sessions[settings.DEFAULT_DATABASE] = sessionmaker(bind=create_engine('postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % bind_params,
+                                                                 max_overflow=15,
+                                                                 pool_pre_ping=True,
+                                                                 pool_recycle=3600,
+                                                                 pool_size=30))
+for database in settings.DATABASES:
+    bind_params["db"] = database
+    sessions[database] = sessionmaker(bind=create_engine('postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % bind_params,
                                                                  max_overflow=15,
                                                                  pool_pre_ping=True,
                                                                  pool_recycle=3600,
