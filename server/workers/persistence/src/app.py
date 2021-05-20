8b5a832d7534a02115f5f762357467788489b02e
chreman <web@christopherkittel.eu>
2021-05-20 14:23:25 +0200

    finish spinning off persistence API

--- server/workers/persistence/src/app.py
+++ server/workers/persistence/src/app.py
@@ -8,8 +8,39 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from apis.persistence import persistence_ns
 
 from config import settings
-from utils.monkeypatches import ReverseProxied, __schema__, specs_url, _register_apidoc, inject_flasgger
 import logging
+
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5001;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
 
 
 def api_patches(app, settings):
@@ -33,7 +64,6 @@ app = Flask('v1', instance_relative_config=True)
 app.config.from_object('config.settings')
 handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(app.logger.level)
-app = inject_flasgger(app)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_port=1, x_for=1, x_host=1, x_prefix=1)
 app.wsgi_app = ReverseProxied(app.wsgi_app)
 CORS(app, expose_headers=["Content-Disposition", "Access-Control-Allow-Origin"])
