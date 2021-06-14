baba2b1ac13c88b5e3626b395bc7de3bced51822
chreman <web@christopherkittel.eu>
2021-06-14 13:48:28 +0200

    fix error handling

--- server/workers/dataprocessing/src/headstart.py
+++ server/workers/dataprocessing/src/headstart.py
@@ -90,5 +90,5 @@ class Dataprocessing(object):
                 res["id"] = k
                 res["params"] = params
                 res["status"] = "error"
-                res["error"] = e
+                res["error"] = "unexpected data processing error"
                 self.redis_store.set(k+"_output", json.dumps(res))
