1a2f2e45ba8260253985537843f8012008e71d63
chreman <web@christopherkittel.eu>
2021-07-06 12:44:04 +0200

    update to new date format

--- server/workers/dataprocessing/src/streamgraph.py
+++ server/workers/dataprocessing/src/streamgraph.py
@@ -25,7 +25,7 @@ class Streamgraph(object):
 
     def get_streamgraph_data(self, metadata, query, n=12, method="tfidf"):
         df = pd.DataFrame.from_records(metadata)
-        df.year = pd.to_datetime(df.year, format="%Y")
+        df.year = pd.to_datetime(df.year).map(lambda x: x.year)
         df = df[df.subject.map(lambda x: x is not None)]
         df.subject = df.subject.map(lambda x: [s for s in x.split("; ")] if isinstance(x, str) else "")
         df = df[df.subject.map(lambda x: x != [])]
