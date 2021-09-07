05efd1ad2739e38866697a5e8daf03a76b0fbf9b
Jan Konstant <456545@mail.muni.cz>
2021-09-07 13:00:23 +0200

    returned the yyyy format in triple

--- vis/js/reducers/timespan.js
+++ vis/js/reducers/timespan.js
@@ -18,6 +18,19 @@ const SERVICE_START = {
   doaj: "1809",
   pubmed: "1809-01-01",
   base: "1665-01-01",
+  default: "1970-01-01",
+};
+
+const SERVICE_DISPLAY_FORMAT = {
+  doaj: "yyyy",
+  triple: "yyyy",
+  default: "d mmm yyyy",
+};
+
+const SERVICE_HYPHEN_FORMAT = {
+  doaj: "yyyy",
+  triple: "yyyy",
+  default: "yyyy-mm-dd",
 };
 
 const getTimespan = (config, context) => {
@@ -25,33 +38,22 @@ const getTimespan = (config, context) => {
     return null;
   }
 
-  const displayFormat = config.service === "doaj" ? "yyyy" : "d mmm yyyy";
-  const hyphenFormat = config.service === "doaj" ? "yyyy" : "yyyy-mm-dd";
+  const displayFormat =
+    SERVICE_DISPLAY_FORMAT[config.service] || SERVICE_DISPLAY_FORMAT.default;
+  const hyphenFormat =
+    SERVICE_HYPHEN_FORMAT[config.service] || SERVICE_HYPHEN_FORMAT.default;
 
-  const today = new Date();
-  const from = new Date(context.params.from);
-  const to = new Date(context.params.to);
-  if (
-    typeof context.params.to === "string" &&
-    context.params.to.match(/^\d{4}$/)
-  ) {
-    to.setMonth(11);
-    to.setDate(31);
-  }
-  if (to.getTime() > today.getTime()) {
-    to.setTime(today.getTime());
-  }
-
-  today.setTime(today.getTime() + today.getTimezoneOffset() * 60 * 1000);
-  from.setTime(from.getTime() + from.getTimezoneOffset() * 60 * 1000);
-  to.setTime(to.getTime() + to.getTimezoneOffset() * 60 * 1000);
+  const today = getTodayDate();
+  const from = getFromDate(context);
+  const to = getToDate(context, today);
 
   const toFormatted = dateFormat(to, displayFormat);
   const modifier = getModifier(config, context);
   // most recent streamgraphs straight away display "Until xyz",
   // other maps have a more complicated logic
   if (!config.is_streamgraph || modifier !== "most-recent") {
-    const defaultFromHyphenated = SERVICE_START[config.service] || "1970-01-01";
+    const defaultFromHyphenated =
+      SERVICE_START[config.service] || SERVICE_START.default;
 
     const fromHyphenated = dateFormat(from, hyphenFormat);
     const fromFormatted = dateFormat(from, displayFormat);
@@ -69,6 +71,38 @@ const getTimespan = (config, context) => {
   }
 
   return "Until " + toFormatted;
+};
+
+const getTodayDate = () => {
+  const today = new Date();
+  today.setTime(today.getTime() + today.getTimezoneOffset() * 60 * 1000);
+
+  return today;
+};
+
+const getFromDate = (context) => {
+  const from = new Date(context.params.from);
+  from.setTime(from.getTime() + from.getTimezoneOffset() * 60 * 1000);
+
+  return from;
+};
+
+const getToDate = (context, today) => {
+  const to = new Date(context.params.to);
+  if (
+    typeof context.params.to === "string" &&
+    context.params.to.match(/^\d{4}$/)
+  ) {
+    to.setMonth(11);
+    to.setDate(31);
+  }
+  if (to.getTime() > today.getTime()) {
+    to.setTime(today.getTime());
+  }
+
+  to.setTime(to.getTime() + to.getTimezoneOffset() * 60 * 1000);
+
+  return to;
 };
 
 export default timespan;
