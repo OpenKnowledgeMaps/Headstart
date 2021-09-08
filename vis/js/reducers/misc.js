c80c4f06315311c5ee6b7979640cdf5be2680d55
Jan Konstant <456545@mail.muni.cz>
2021-09-08 17:01:20 +0200

    replaced CreatedBy with BuiltWith

--- vis/js/reducers/misc.js
+++ vis/js/reducers/misc.js
@@ -12,13 +12,13 @@ const misc = (state = { isLoading: true, showLoading: false }, action) => {
     case "INITIALIZE":
       return {
         ...state,
-        showCreatedBy: action.configObject.credit_embed,
+        isEmbedded: action.configObject.credit_embed,
         showCreatedByViper: action.configObject.credit,
-        createdByUrl: action.configObject.canonical_url,
         renderMap: action.configObject.render_map,
         renderList: action.configObject.render_list,
         isLoading: false,
         visTag: action.configObject.tag,
+        timestamp: action.contextObject.timestamp,
       };
     default:
       return state;
