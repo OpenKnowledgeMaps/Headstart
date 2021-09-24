d733cad3b7820ae2b1524fdc55d1ac666ea17aee
tanteuschi <maxi@openknowledgemaps.org>
2021-09-24 15:04:00 +0200

    added mobile css

--- examples/triple/search.php
+++ examples/triple/search.php
@@ -14,7 +14,7 @@ include 'search-flow/inc/waiting-page/waiting-page-header.php';
     <?php include('search-flow/inc/shared/head-search-form.php') ?>
 </head>
 
-<body style="padding-left:10px; padding-right:10px;" class="searchpage">
+<body class="searchpage">
     <div>
         <?php include('search-flow/inc/shared/banner-browser-unsupported.php') ?>
         <?php include('search-flow/inc/shared/banner-mobile.php') ?>
