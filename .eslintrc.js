c7deff4b8556998ab2c1107ab862ae5041927801
Jan Konstant <konstantjan@seznam.cz>
2021-10-20 17:06:51 +0200

    updated eslint & jest (and node to v14)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,31 @@
 module.exports = {
-    "extends": [
-        "react-app",
-        "eslint:recommended"
-    ]
-}
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+    },
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  plugins: ["react", "react-hooks"],
+  rules: {
+    // we don't use prop-types
+    "react/prop-types": 0,
+    // this would throw an error e.g. for quotes ('')
+    "react/no-unescaped-entities": 0,
+    // in callbacks it's handy to leave unused function parameters
+    "no-unused-vars": ["error", { args: "none" }],
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
