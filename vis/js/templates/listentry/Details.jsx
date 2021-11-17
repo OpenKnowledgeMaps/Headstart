f127c03792c8c1886508541748596c533ee36356
Jan Konstant <konstantjan@seznam.cz>
2021-11-17 14:34:05 +0100

    abbreviating paper source

--- vis/js/templates/listentry/Details.jsx
+++ vis/js/templates/listentry/Details.jsx
@@ -1,27 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 
 import Highlight from "../../components/Highlight";
 
-const Details = ({ authors, source }) => {
+const Details = ({ authors, source, isSelected }) => {
   return (
     // html template starts here
     <div className="list_details">
-      <span className="list_authors">
+      <div className="list_authors">
         <Highlight queryHighlight>{authors}</Highlight>
-      </span>
+      </div>
       {!!source && (
-        <>
+        <div className={"list_source" + (isSelected ? "" : " short")}>
           <span className="list_in">
             <Highlight> in </Highlight>
           </span>
           <span className="list_published_in">
             <Highlight queryHighlight>{source}</Highlight>
           </span>
-        </>
+        </div>
       )}
     </div>
     // html template ends here
   );
 };
 
-export default Details;
+const mapStateToProps = (state) => ({
+  isSelected: !!state.selectedPaper,
+});
+
+export default connect(mapStateToProps)(React.memo(Details));
