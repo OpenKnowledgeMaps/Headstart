0e23c49c046522e7c1b642110326f4e108744c15
chreman <web@christopherkittel.eu>
2021-11-24 18:13:05 +0100

    summarization cleanup

--- server/preprocessing/other-scripts/summarize.R
+++ server/preprocessing/other-scripts/summarize.R
@@ -10,29 +10,33 @@ trim <- function (x) gsub("^\\s+|\\s+$", "", x)
 
 
 prune_ngrams <- function(ngrams, stops){
-  tokens = mapply(strsplit, ngrams, split=" |;")
-  tokens = mapply(strsplit, tokens, split="_")
-  tokens = lapply(tokens, function(y){
-                          Filter(function(x){
-                                  if (!purrr::is_empty(x))
-                                  if (x[1] != "") !any(stri_detect_fixed(stops, x[1]))
-                                            }, y)})
-  tokens = lapply(tokens, function(y){
-                          Filter(function(x){
-                                  if (!purrr::is_empty(x))
-                                  if (tail(x,1) != "") !any(stri_detect_fixed(stops, tail(x,1)))
-                                            }, y)})
-  tokens = lapply(tokens, function(y){
-                          Filter(function(x){
-                                  if (!purrr::is_empty(x))
-                                      !(x[1]==tail(x,1))
-                                        }, y)})
-  tokens = lapply(tokens, function(y){Filter(function(x){length(x)>1},y)})
-  empties = which(lapply(tokens, length)==0)
-  tokens[c(empties)] = list("")
-  tokens = lapply(tokens, function(x){mapply(paste, x, collapse="_")})
-  tokens = lapply(tokens, paste, collapse=";")
-  return (tokens)
+  ngrams = mapply(strsplit, ngrams, split=" |;")
+  tokenized_ngrams = mapply(function(x) {
+                            strsplit(x, split="_")
+                          }, ngrams)
+  # filter out empty tokens
+  tokenized_ngrams = lapply(tokenized_ngrams, function(ngrams){ngrams[lapply(ngrams, length)>0]})
+  # remove ngrams starting with a stopword
+  tokenized_ngrams = lapply(tokenized_ngrams, function(x) {
+                            Filter(function(tokens){
+                              !any(stri_detect_fixed(stops, tolower(tokens[[1]])))
+                            }, x)})
+  # remove ngrams ending with a stopword
+  tokenized_ngrams = lapply(tokenized_ngrams, function(x) {
+                            Filter(function(tokens){
+                              !any(stri_detect_fixed(stops, tolower(tail(tokens,1))))
+                            }, x)})
+  # remove ngrams starting and ending with the same word
+  tokenized_ngrams = lapply(tokenized_ngrams, function(x) {
+                            Filter(function(tokens){
+                              !(tokens[[1]]==tail(tokens,1))
+                            }, x)})
+  # keep ngrams with min length 2
+  tokenized_ngrams = lapply(tokenized_ngrams, function(x){x[lapply(x, length)>1]})
+  tokenized_ngrams = tokenized_ngrams[lapply(tokenized_ngrams, length)>1]
+  tokenized_ngrams = lapply(tokenized_ngrams, function(x){mapply(paste, x, collapse="_")})
+  pruned_ngrams = lapply(tokenized_ngrams, paste, collapse=";")
+  return (pruned_ngrams)
 }
 
 create_cluster_labels <- function(clusters, metadata,
@@ -108,7 +112,7 @@ get_cluster_corpus <- function(clusters, metadata, service, stops, taxonomy_sepa
     subjects = metadata$subject[matches]
     titles = lapply(titles, function(x) {gsub("[^[:alnum:]-]", " ", x)})
     titles = lapply(titles, gsub, pattern="\\s+", replacement=" ")
-    title_ngrams <- get_title_ngrams(titles, stops)
+    title_ngrams <- get_title_ngrams(titles, stops, c(2, 3))
     titles = lapply(titles, function(x) {removeWords(x, stops)})
 
     subjects = mapply(gsub, subjects, pattern = "; ", replacement=";")
@@ -125,13 +129,14 @@ get_cluster_corpus <- function(clusters, metadata, service, stops, taxonomy_sepa
       subjects = mapply(paste, subjects, taxons, collapse=";")
     }
     if (add_title_ngrams == T) {
-      all_subjects = paste(subjects, title_ngrams$bigrams, title_ngrams$trigrams, collapse=" ")
+      all_subjects = paste(subjects, title_ngrams, collapse=" ")
     } else {
       all_subjects = paste(subjects, collapse=" ")
     }
     all_subjects <- str_replace_all(all_subjects, "\\?+_\\?+|\\?+|\\?+ ", "")
     all_subjects <- str_replace_all(all_subjects, ";+", ";")
     all_subjects <- str_replace_all(all_subjects, " ?; ?", ";")
+    all_subjects <- str_replace_all(all_subjects, " +", ";")
     subjectlist = c(subjectlist, all_subjects)
   }
   nn_corpus <- VCorpus(VectorSource(subjectlist))
@@ -154,16 +159,15 @@ another_prune_ngrams <- function(ngrams, stops){
   tokens <- unname(unlist(ngrams))
   # split ngrams
   tokens = lapply(tokens, strsplit, split="_")
+  tokens = tokens[lapply(tokens, length)>0]
   # check if first token of ngrams in stopword list
   tokens = lapply(tokens, function(y){
                           Filter(function(x){
-                                  if (!purrr::is_empty(x))
                                   if (x[1] != "") !any(stri_detect_fixed(stops, x[1]))
                                             }, y)})
   # check if last token of ngrams in stopword list
   tokens = lapply(tokens, function(y){
                           Filter(function(x){
-                                  if (!purrr::is_empty(x))
                                   if (tail(x,1) != "") !any(stri_detect_fixed(stops, tail(x,1)))
                                             }, y)})
   # check that first token is not the same as the last token
@@ -189,15 +193,11 @@ fill_empty_clusters <- function(nn_tfidf, nn_corpus){
   return(replacement_tfidf_top)
 }
 
-get_title_ngrams <- function(titles, stops) {
+get_title_ngrams <- function(titles, stops, ngram_lengths) {
   # for ngrams: we have to collapse with "_" or else tokenizers will split ngrams again at that point and we'll be left with unigrams
   titles_bigrams = prune_ngrams(lapply(lapply(titles, function(x)unlist(lapply(ngrams(unlist(strsplit(x, split = " ")), 2), paste, collapse  = "_"))), paste, collapse = " "), stops)
   titles_trigrams = prune_ngrams(lapply(lapply(titles, function(x)unlist(lapply(ngrams(unlist(strsplit(x, split = " ")), 3), paste, collapse = "_"))), paste, collapse = " "), stops)
-  #titles_quadgrams = lapply(lapply(titles, function(x)unlist(lapply(ngrams(unlist(strsplit(x, split=" ")), 4), paste, collapse="_"))), paste, collapse=" ")
-  #titles_quadgrams = prune_ngrams(titles_quadgrams, stops)
-  #titles_fivegrams = lapply(lapply(titles, function(x)unlist(lapply(ngrams(unlist(strsplit(x, split=" ")), 5), paste, collapse="_"))), paste, collapse=" ")
-  #titles_fivegrams = prune_ngrams(titles_fivegrams, stops)
-  return(list("bigrams" = titles_bigrams, "trigrams" = titles_trigrams))
+  return(c(titles_bigrams, titles_trigrams))
 }
 
 
