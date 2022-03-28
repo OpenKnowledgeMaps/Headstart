092579ad197e5f977bb26e8acc0001ac33c239e0
Jan Konstant <konstantjan@seznam.cz>
2022-03-28 15:20:36 +0200

    paper tag changes: new icons, layout, case

--- vis/js/templates/listentry/Tags.jsx
+++ vis/js/templates/listentry/Tags.jsx
@@ -7,9 +7,7 @@ const Tags = ({ values }) => {
     // html template starts here
     <div className="tags">
       {values.map((tag) => (
-        <div className="paper-tag" key={tag}>
-          <Highlight>{tag}</Highlight>
-        </div>
+        <Tag key={tag}>{tag}</Tag>
       ))}
     </div>
     // html template ends here
@@ -17,3 +15,30 @@ const Tags = ({ values }) => {
 };
 
 export default Tags;
+
+export const getIcon = (tag) => {
+  if (tag.toLowerCase() === "audio") {
+    return "fas fa-music";
+  }
+
+  if (tag.toLowerCase().includes("video")) {
+    return "fas fa-photo-video";
+  }
+
+  if (tag.toLowerCase() === "dataset") {
+    return "fas fa-database";
+  }
+
+  return null;
+};
+
+const Tag = ({ children: tag }) => {
+  const faIcon = getIcon(tag);
+
+  return (
+    <div className="paper-tag">
+      {!!faIcon && <span className={`access_icon ${faIcon}`}></span>}
+      <Highlight>{tag}</Highlight>
+    </div>
+  );
+};
---
+++ vis/js/templates/paper/Icons.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { getIcon } from "../listentry/Tags";
+
+const Icons = ({ paper, iconClasses }) => {
+  const { oa: isOpenAccess, free_access: isFreeAccess } = paper;
+
+  const doctypeIcons = paper.resulttype
+    .map((type) => getIcon(type))
+    .filter((icon) => icon !== null);
+
+  return (
+    <div className="icons">
+      {isOpenAccess && (
+        <p className={"open-access-logo " + iconClasses}>
+          <i className="fas fa-lock-open"></i>
+        </p>
+      )}
+      {isFreeAccess && (
+        <p className={"free-access-logo " + iconClasses}>
+          <i className="fas fa-lock-open"></i>
+        </p>
+      )}
+      {doctypeIcons.map((icon) => (
+        <p className={"dataset-icon " + iconClasses}>
+          <i className={icon}></i>
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default React.memo(Icons);
