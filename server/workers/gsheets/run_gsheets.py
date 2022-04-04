30af489c14c4b3544d873c013068847dd3e1136d
chreman <web@christopherkittel.eu>
2022-04-04 12:51:15 +0200

    redis ssl cleanup

--- server/workers/gsheets/run_gsheets.py
+++ server/workers/gsheets/run_gsheets.py
@@ -10,8 +10,7 @@ if __name__ == '__main__':
         "port": os.getenv("REDIS_PORT"),
         "db": os.getenv("REDIS_DB"),
         "password": os.getenv("REDIS_PASSWORD"),
-        "client_name": "gsheets_retrieval",
-        "ssl": True if os.getenv("REDIS_SSL") == "true" else False
+        "client_name": "gsheets_retrieval"
     }
     redis_store = redis.StrictRedis(**redis_config)
     gc = GSheetsClient(redis_store, os.environ.get("LOGLEVEL", "INFO"))
--- server/workers/pubmed/run_pubmed.py
+++ server/workers/pubmed/run_pubmed.py
@@ -10,8 +10,7 @@ if __name__ == '__main__':
         "port": os.getenv("REDIS_PORT"),
         "db": os.getenv("REDIS_DB"),
         "password": os.getenv("REDIS_PASSWORD"),
-        "client_name": "pubmed_retrieval",
-        "ssl": True if os.getenv("REDIS_SSL") == "true" else False
+        "client_name": "pubmed_retrieval"
     }
     redis_store = redis.StrictRedis(**redis_config)
     wrapper = PubMedClient("./other-scripts", "run_pubmed.R", redis_store,
