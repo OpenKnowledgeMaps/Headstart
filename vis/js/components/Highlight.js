3cd3722e3774d60f5cfa94f250817e7c638ba8e2
Jan Konstant <konstantjan@seznam.cz>
2022-05-03 09:16:25 +0200

    fixed highlight in quotes

--- vis/js/components/Highlight.js
+++ vis/js/components/Highlight.js
@@ -98,9 +98,8 @@ const hyphenateStringSafely = (string) => {
  * @param {boolean} useLookBehind use ?<= if true, else use \b
  */
 export const getQueryTermMatcher = (term, useLookBehind) => {
-  // this should theoretically work but it does not: [^\w\u00AD] (??? is this still true?)
-  // this is used instead:
-  const wordBreakers = "[-\\s.,:;?!/()[\\]{}]";
+  // former regex: [-\\s.,:;?!/()[\\]{}]
+  const wordBreakers = "[^\\w\u00AD]";
   let lookBehind = `(?<=${wordBreakers}|^)`;
   if (!useLookBehind) {
     lookBehind = "\\b";
