941912bbd81d78103dddbb4b4890975f3a759783
chreman <web@christopherkittel.eu>
2023-04-14 17:35:34 +0200

    resolve merge conflict

--- server/preprocessing/other-scripts/vis_layout.R
+++ server/preprocessing/other-scripts/vis_layout.R
@@ -34,10 +34,7 @@ vis_layout <- function(text, metadata, service,
   start.time <- Sys.time()
   vlog$debug("preprocess")
   metadata <- sanitize_abstract(metadata)
-  filtered <- filter_duplicates(metadata, text, list_size)
-  metadata <- filtered$metadata
   languages <- names(head(sort(table(unlist(lapply(metadata$dclang, strsplit, "; "))), decreasing = TRUE), 2))
-  text <- filtered$text
   vlog$info(paste("vis_id:", .GlobalEnv$VIS_ID, "doc count:", nrow(metadata), sep=" "))
 
   if(vis_type=='overview'){
