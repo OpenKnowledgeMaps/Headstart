dfcdfde4ae7cdfd5ea2b0da1f14276fa587426ac
Darren Sapalo <darren.sapalo@gmail.com>
2023-05-24 18:38:28 +0800

    Added generated assets because they were missing.

--- config.example.js
+++ config.example.js
@@ -3,5 +3,5 @@ module.exports = {
     publicPath : "http://localhost:8080/dist/", 
     skin : "",
     // This is where the generated React JS code will be generated, for use in your index.html.
-    ouputDirectory: "examples/tome.gg/dist"
+    ouputDirectory: "examples/tome.gg/assets"
 };
