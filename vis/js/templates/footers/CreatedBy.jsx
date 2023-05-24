6672a67c013c9df50e17ad7de530e1623702884a
Alexandra Shubenko <as@belichberg.com>
2023-05-24 18:50:09 +0200

    try to remove ?embed=true

--- vis/js/templates/footers/CreatedBy.jsx
+++ vis/js/templates/footers/CreatedBy.jsx
@@ -6,44 +6,51 @@ import { getDateTimeFromTimestamp } from "../../utils/dates";
 import useMatomo from "../../utils/useMatomo";
 
 const CreatedBy = ({ timestamp, faqsUrl }) => {
-  const dateTime = getDateTimeFromTimestamp(timestamp);
-  const { trackEvent } = useMatomo();
+    const dateTime = getDateTimeFromTimestamp(timestamp);
+    const {trackEvent} = useMatomo();
 
-  const trackLink = (action) =>
-    trackEvent("Added components", action, "Footer");
+    const trackLink = (action) =>
+        trackEvent("Added components", action, "Footer");
 
-  return (
-    <div className="builtwith" id="builtwith">
-      <img src={okmapsRoundLogo} alt="OKMaps round logo" />{" "}
-      <a
-        href={window.location.href}
-        target="_blank"
-        rel="noreferrer"
-        onClick={() => trackLink("Open this visualization")}
-      >
-        This visualization
-      </a>{" "}
-      was created by{" "}
-      <a
-        href="https://openknowledgemaps.org"
-        target="_blank"
-        rel="noreferrer"
-        onClick={() => trackLink("Open main page")}
-      >
-        Open Knowledge Maps
-      </a>
-      {dateTime ? " " + dateTime : ""}. For more information{" "}
-      <a
-        href={faqsUrl}
-        target="_blank"
-        rel="noreferrer"
-        onClick={() => trackLink("Open FAQs")}
-      >
-        read our FAQs
-      </a>
-      .
-    </div>
-  );
+
+    // function urlWithoutEmbed() {
+    //     return window.location.href.replace("?embed=true", "");
+    // }
+
+    return (
+        <div className="builtwith" id="builtwith">
+            <img src={okmapsRoundLogo} alt="OKMaps round logo"/>{" "}
+            <a
+                // href={window.location.href}
+                // href={urlWithoutEmbed()}
+                href={window.location.href.replace("?embed=true", "")}
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => trackLink("Open this visualization")}
+            >
+                This visualization
+            </a>{" "}
+            was created by{" "}
+            <a
+                href="https://openknowledgemaps.org"
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => trackLink("Open main page")}
+            >
+                Open Knowledge Maps
+            </a>
+            {dateTime ? " " + dateTime : ""}. For more information{" "}
+            <a
+                href={faqsUrl}
+                target="_blank"
+                rel="noreferrer"
+                onClick={() => trackLink("Open FAQs")}
+            >
+                read our FAQs
+            </a>
+            .
+        </div>
+    );
 };
 
 export default CreatedBy;
