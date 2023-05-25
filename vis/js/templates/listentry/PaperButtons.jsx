28ccdebaeeb98f3c3163ab06a031b432e352820e
Alexandra Shubenko <as@belichberg.com>
2023-05-25 11:18:20 +0200

    hide file button if no access

--- vis/js/templates/listentry/PaperButtons.jsx
+++ vis/js/templates/listentry/PaperButtons.jsx
@@ -17,6 +17,7 @@ const PaperButtons = ({
   handleExportClick,
 }) => {
   const { trackEvent } = useMatomo();
+  console.log("PaperButtons.jsx: paper = ", paper);
 
   const onPDFClick = getPaperPDFClickHandler(paper, handlePDFClick);
 
@@ -43,46 +44,47 @@ const PaperButtons = ({
     !paper.resulttype.some((t) => noCitationDoctypes.includes(t.toLowerCase()));
 
   return (
-    // html template starts here
-    <div className="paper_buttons_row">
-      {isText && !!onPDFClick && (
-        <button
-          className="paper_button main"
-          title="Open the PDF"
-          onClick={handlePDFButtonClick}
-        >
-          <i className="far fa-file-pdf"></i>&nbsp;&nbsp;
-          <Highlight>PDF</Highlight>
-        </button>
-      )}
-      {!isText && (
-        <a href={paper.list_link.address} title="Open the file" target="_blank">
-          <div className="paper_button main">
-            <i className="far fa-file"></i>&nbsp;&nbsp;
-            <Highlight>File</Highlight>
-          </div>
-        </a>
-      )}
-      {hasCiteButton && (
-        <button
-          className="paper_button"
-          title="Cite this document"
-          onClick={handleCiteButtonClick}
-        >
-          <i className="fa fa-quote-right"></i>&nbsp;&nbsp;Cite as
-        </button>
-      )}
-      {showExportButton && (
-        <button
-          className="paper_button"
-          title="Export this document"
-          onClick={handleExportButtonClick}
-        >
-          <i className="fa fa-arrow-down"></i>&nbsp;&nbsp;Export
-        </button>
-      )}
-    </div>
-    // html template ends here
+      // html template starts here
+      <div className="paper_buttons_row">
+        {isText && !!onPDFClick && (
+            <button
+                className="paper_button main"
+                title="Open the PDF"
+                onClick={handlePDFButtonClick}
+            >
+              <i className="far fa-file-pdf"></i>&nbsp;&nbsp;
+              <Highlight>PDF</Highlight>
+            </button>
+        )}
+        {/*add paper.oa condition to avoid show file button if no access */}
+        {(paper.oa && !isText) && (
+            <a href={paper.list_link.address} title="Open the file" target="_blank" rel="noreferrer">
+              <div className="paper_button main">
+                <i className="far fa-file"></i>&nbsp;&nbsp;
+                <Highlight>File</Highlight>
+              </div>
+            </a>
+        )}
+        {hasCiteButton && (
+            <button
+                className="paper_button"
+                title="Cite this document"
+                onClick={handleCiteButtonClick}
+            >
+              <i className="fa fa-quote-right"></i>&nbsp;&nbsp;Cite as
+            </button>
+        )}
+        {showExportButton && (
+            <button
+                className="paper_button"
+                title="Export this document"
+                onClick={handleExportButtonClick}
+            >
+              <i className="fa fa-arrow-down"></i>&nbsp;&nbsp;Export
+            </button>
+        )}
+      </div>
+      // html template ends here
   );
 };
 
