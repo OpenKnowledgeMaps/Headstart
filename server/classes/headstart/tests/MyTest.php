6a8967a7f4b4bc781d66c29fe8f64045e98930aa
chreman <web@christopherkittel.eu>
2023-06-29 13:20:21 +0200

    basic test demo and config

---
+++ server/classes/headstart/tests/MyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class MyTest extends TestCase
+{
+  public function testMyTest(): void
+  {
+    self::assertTrue(false);
+  }
+}
