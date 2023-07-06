66f3bfa86cc56559d40e9a6c96253ebde9ec4b62
Alexandra Shubenko <as@belichberg.com>
2023-07-06 09:56:22 +0200

    tests in process, optimize db connection

--- server/classes/headstart/tests/dbConnectionTest.php
+++ server/classes/headstart/tests/dbConnectionTest.php
@@ -4,18 +4,42 @@ use PHPUnit\Framework\TestCase;
 
 class DbConnectionTest extends TestCase
 {
-  public function testDbConnectionTest(): void
-  {
-    require dirname(__FILE__) . '/../persistence/SQLitePersistence.php';
+    protected static $persistence;
 
-    $db_path = '/app/storage/test.sqlite';
-    $persistence = new headstart\persistence\SQLitePersistence($db_path);
+    public static function setUpBeforeClass(): void
+    {
+        require dirname(__FILE__) . '/../persistence/SQLitePersistence.php';
 
-    $vis_id = 'd161dba364a1e8c0468b9c74407e3575';
+        $db_path = '/app/storage/test.sqlite';
+        self::$persistence = new headstart\persistence\SQLitePersistence($db_path);
+    }
 
-    $data = $persistence->getLastVersion($vis_id, $details = false, $context = true)[0];
-    var_dump($data);
-//    echo($data);
-  }
+    public static function tearDownAfterClass(): void
+    {
+        self::$persistence = null;
+    }
+
+    public function testDbConnection(): void
+    {
+        $vis_id = 'd161dba364a1e8c0468b9c74407e3575';
+
+        $data = self::$persistence->getLastVersion($vis_id, $details = false, $context = true)[0];
+//        var_dump($data);
+    }
+
+//    old version of connection to db test
+//  public function testDbConnectionTest(): void
+//  {
+//    require dirname(__FILE__) . '/../persistence/SQLitePersistence.php';
+//
+//    $db_path = '/app/storage/test.sqlite';
+//    $persistence = new headstart\persistence\SQLitePersistence($db_path);
+//
+//    $vis_id = 'd161dba364a1e8c0468b9c74407e3575';
+//
+//    $data = $persistence->getLastVersion($vis_id, $details = false, $context = true)[0];
+//    var_dump($data);
+////    echo($data);
+//  }
 }
 
---
+++ server/classes/headstart/tests/writeToDbTest.php
@@ -0,0 +1,95 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+function cleanQuery($dirty_query, $transform_query_tolowercase, $add_slashes) {
+    $query = strip_tags($dirty_query);
+    $query = trim($query);
+
+    if ($transform_query_tolowercase) {
+        $query = strtolower($query);
+    }
+
+    if ($add_slashes) {
+        $query = addslashes($query);
+    }
+
+    return $query;
+}
+
+function packParamsJSON($params_array, $post_params) {
+
+    if($params_array === null) {
+        return null;
+    }
+
+    $output_array = array();
+
+    foreach ($params_array as $entry) {
+        $current_params = library\CommUtils::getParameter($post_params, $entry);
+        $output_array[$entry] = $current_params;
+    }
+
+    return json_encode($output_array);
+}
+
+class WriteToDbTest extends TestCase
+{
+    protected static $persistence;
+
+    public static function setUpBeforeClass(): void
+    {
+        require dirname(__FILE__) . '/../persistence/SQLitePersistence.php';
+
+        $db_path = '/app/storage/test.sqlite';
+        self::$persistence = new headstart\persistence\SQLitePersistence($db_path);
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$persistence = null;
+    }
+    public function testWriteToDbTest(): void
+    {
+    require dirname(__FILE__) . '/../persistence/SQLitePersistence.php';
+
+
+    //    load json
+    $jsonFilePath = '/app/examples/project_website/data/digital-education-lang[].json';
+    $json = file_get_contents($jsonFilePath);
+    $jsonData = json_decode($json, true);
+    //    var_dump($jsonData);
+    //    var_dump($jsonData['context']);
+
+    $jsonObject = json_decode($json);
+    var_dump($jsonObject);
+    //    var_dump($jsonObject->context->query);
+
+
+
+    $unique_id = $jsonData['context']['id'];
+    $dirty_query = $jsonObject->context->query;
+    var_dump($dirty_query);
+    $query = cleanQuery($dirty_query, $transform_query_tolowercase = true, $add_slashes = true);
+    $unique_id = $jsonObject->context->id;
+    $service = $jsonObject->context->service;
+    $repo2snapshot = array("plos" => "PLOS"
+                        , "pubmed" => "PubMed"
+                        , "doaj" => "DOAJ"
+                        , "base" => "BASE"
+                        , "openaire" => "OpenAire"
+                        , "gsheets" => "GSheets");
+    $vis_title = $service;
+    $params_json = $jsonObject->context->params;
+    //    $params_json = packParamsJSON($param_types, $post_params);
+
+    //    $persistence->createVisualization($unique_id, $vis_title, $jsonObject, $query, $dirty_query, $params_json);
+    self::$persistence->createVisualization($unique_id, $vis_title, $jsonObject, $query, $dirty_query, $params_json);
+
+    $res = getLatestRevisions();
+
+    var_dump($res);
+
+    }
+}
+
