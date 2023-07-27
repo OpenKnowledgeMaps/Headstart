363c571cfca8a8b87fc3f511e636e6ac2a09cbaf
Alexandra Shubenko <as@belichberg.com>
2023-07-27 14:57:13 +0200

    integration test updates

--- server/classes/headstart/tests/MyTest.php
+++ server/classes/headstart/tests/MyTest.php
@@ -1,13 +1,13 @@
 <?php
-
-declare(strict_types=1);
-
-use PHPUnit\Framework\TestCase;
-
-class MyTest extends TestCase
-{
-  public function testMyTest(): void
-  {
-    self::assertTrue(false);
-  }
-}
+//
+//declare(strict_types=1);
+//
+//use PHPUnit\Framework\TestCase;
+//
+//class MyTest extends TestCase
+//{
+//  public function testMyTest(): void
+//  {
+//    self::assertTrue(false);
+//  }
+//}
--- server/classes/headstart/tests/dbConnectionTest.php
+++ server/classes/headstart/tests/dbConnectionTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 use headstart\persistence\SQLitePersistence as SQLitePersistence;
 
+
 class DbConnectionTest extends TestCase
 {
 
@@ -15,10 +16,16 @@ class DbConnectionTest extends TestCase
     {
         $tempFile = tempnam("/tmp", "test-db");
         $persistence = new SQLitePersistence($tempFile);
-        $vis_id = 'd161dba364a1e8c0468b9c7s4407e3575'; // this is just some random hash
-        $vis_title = 'base'; // Is this right? Looks to Tom like vis_title parameter is actually the service name?
-        $data = '{"some": "pretty-empty-json"}'; // here is a very small malformed map json. We could load a proper from disk in the future
-        
+
+        $jsonFilePath = __DIR__ . "/test_data/digital-education.json";
+
+        $data = file_get_contents($jsonFilePath); // returns string
+
+        $jsonObject = json_decode($data); // returns object
+
+        $vis_id = $jsonObject->context->id;
+        $vis_title = $jsonObject->context->service;
+
         // Since this is a totally new SQLite file that we will have each time we need to initialise the tables
         $persistence->createTables();
 
@@ -28,12 +35,50 @@ class DbConnectionTest extends TestCase
         // Now let's try to retrieve the visualisation that we stored
         $resultFromDataBase = $persistence->getLastVersion($vis_id);
 
-        // Finally let's assert that we get back the response that we expected to get
-        $this->assertEquals($resultFromDataBase, [
-            "rev_data" => $data,
-            0 => $data
-        ]); // quite an interesting response from the Persistence layer, not really what I would have expected
+        $jsonRes = $resultFromDataBase["rev_data"];
+        $jsonDecoded = json_decode($jsonRes);
+
+
+        $resId = $jsonDecoded->context->id;
+        $resTitle = $jsonDecoded->context->service;
+
+//        var_dump($jsonRes);
+        var_dump($resId);
+        var_dump($resTitle);
+        $this->assertEquals($resId, $vis_id, "actual value of ID is not equals to expected");
+        $this->assertEquals($resTitle, $vis_title, "actual value of Title is not equals to expected");
+        
     }
 
 }
+
+// // structure from Tom
+//class DbConnectionTest extends TestCase
+//{
+//
+//    public function testCreateVisualizationAndRetrieve(): void
+//    {
+//        $tempFile = tempnam("/tmp", "test-db");
+//        $persistence = new SQLitePersistence($tempFile);
+//        $vis_id = 'd161dba364a1e8c0468b9c7s4407e3575'; // this is just some random hash
+//        $vis_title = 'base'; // Is this right? Looks to Tom like vis_title parameter is actually the service name?
+//        $data = '{"some": "pretty-empty-json"}'; // here is a very small malformed map json. We could load a proper from disk in the future
+//
+//        // Since this is a totally new SQLite file that we will have each time we need to initialise the tables
+//        $persistence->createTables();
+//
+//        // Let's store something in it that looks like a visualisation
+//        $persistence->createVisualization($vis_id, $vis_title, $data);
+//
+//        // Now let's try to retrieve the visualisation that we stored
+//        $resultFromDataBase = $persistence->getLastVersion($vis_id);
+//
+//        // Finally let's assert that we get back the response that we expected to get
+//        $this->assertEquals($resultFromDataBase, [
+//            "rev_data" => $data,
+//            0 => $data
+//        ]); // quite an interesting response from the Persistence layer, not really what I would have expected
+//    }
+//
+//}
 
