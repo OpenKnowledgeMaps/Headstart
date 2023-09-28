7a0e79912a4bc6108677626fc8c099767b0534e0
Alexandra Shubenko <as@belichberg.com>
2023-09-28 12:05:54 +0200

    testing customTitle in cite

--- vis/js/templates/modals/CitationModal.jsx
+++ vis/js/templates/modals/CitationModal.jsx
@@ -92,8 +92,8 @@ const mapStateToProps = (state) => ({
   isStreamgraph: state.chartType === STREAMGRAPH_MODE,
   query: state.query.text,
   customTitle:
-  // state.heading.titleStyle === "custom" ? state.heading.customTitle : null,
-  state.heading.customTitle,
+      state.heading.titleStyle === "custom" ? state.heading.customTitle : null,
+  // state.heading.customTitle,
   timestamp: state.misc.timestamp,
   q_advanced: state.q_advanced.text,
   titleStyle: state.heading.titleStyle
