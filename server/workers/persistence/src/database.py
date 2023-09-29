75c9dc1dfac9c4e1398c7f866b23781a3f9da3b5
chreman <web@christopherkittel.eu>
2023-09-29 13:21:26 +0200

    config updates

--- server/workers/persistence/src/database.py
+++ server/workers/persistence/src/database.py
@@ -3,9 +3,11 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
-
-host_string = "&".join(zip(os.getenv("POSTGRES_HOSTS").split(","),
-                           os.getenv("POSTGRES_PORTS").split(",")))
+hosts = []
+for host, port in zip(os.getenv("POSTGRES_HOSTS").split(","),
+                      os.getenv("POSTGRES_PORTS").split(",")):
+    hosts.append(f"{host}:{port}")
+host_string = "&host=".join(hosts)
 
 bind_params = {
     "user": os.getenv("POSTGRES_USER"),
@@ -14,10 +16,18 @@ bind_params = {
     "db": os.getenv("DEFAULT_DATABASE")
 }
 
+if len(hosts) == 1:
+    engine = create_engine('postgresql+psycopg2://%(user)s:%(pw)s@%(host)s/%(db)s' % bind_params,
+                                                    max_overflow=5,
+                                                    pool_pre_ping=True,
+                                                    pool_recycle=600,
+                                                    pool_size=5)
+else:
+    engine = create_engine('postgresql+psycopg2://%(user)s:%(pw)s@/%(db)s?host=%(host)s&target_session_attrs=read-write' % bind_params,
+                                                    max_overflow=5,
+                                                    pool_pre_ping=True,
+                                                    pool_recycle=600,
+                                                    pool_size=5)
 
-Session = sessionmaker(bind=create_engine('postgresql+psycopg2://%(user)s:%(pw)s@/%(db)s?host=%(host)s&target_session_attrs=read-write' % bind_params,
-                                                                 max_overflow=5,
-                                                                 pool_pre_ping=True,
-                                                                 pool_recycle=600,
-                                                                 pool_size=5))
+Session = sessionmaker(bind=engine)
 Base = declarative_base()
