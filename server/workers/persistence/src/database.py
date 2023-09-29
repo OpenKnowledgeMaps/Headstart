04beb0d56043e651539d5fcd9d8681896cd35a44
chreman <web@christopherkittel.eu>
2023-09-29 12:42:41 +0200

    config updates

--- server/workers/persistence/src/database.py
+++ server/workers/persistence/src/database.py
@@ -4,15 +4,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 
+host_string = "&".join(zip(os.getenv("POSTGRES_HOSTS").split(","),
+                           os.getenv("POSTGRES_PORTS").split(",")))
+
 bind_params = {
     "user": os.getenv("POSTGRES_USER"),
     "pw": os.getenv("POSTGRES_PASSWORD"),
-    "host": os.getenv("POSTGRES_HOST"),
-    "port": os.getenv("POSTGRES_PORT"),
+    "host": host_string,
     "db": os.getenv("DEFAULT_DATABASE")
 }
 
-Session = sessionmaker(bind=create_engine('postgresql+psycopg2://%(user)s:%(pw)s@/%(db)s?host=%(host)s:7432&host=%(host)s:6432&target_session_attrs=read-write' % bind_params,
+
+Session = sessionmaker(bind=create_engine('postgresql+psycopg2://%(user)s:%(pw)s@/%(db)s?host=%(host)s&target_session_attrs=read-write' % bind_params,
                                                                  max_overflow=5,
                                                                  pool_pre_ping=True,
                                                                  pool_recycle=600,
