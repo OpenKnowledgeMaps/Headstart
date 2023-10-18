0a4bc100a3c754734d07dc316821a5590805bb4b
chreman <web@christopherkittel.eu>
2023-10-18 09:04:45 +0200

    restructure local docker env

--- server/workers/build_docker_images.sh
+++ server/workers/build_docker_images.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-services=("api" "persistence" "gsheets" "dataprocessing" "base" "pubmed" "openaire")
+services=("api" "persistence" "gsheets" "dataprocessing" "base" "pubmed" "openaire" "searchflow")
 for service in ${services[@]}; do
     docker build -f "$SCRIPT_DIR/../workers/$service/Dockerfile" -t "$service:`git rev-parse HEAD`" "$SCRIPT_DIR/../"
 done
