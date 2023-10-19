96c0c76eedb641d0cc0bc2ef98f6f7daec64b045
chreman <web@christopherkittel.eu>
2023-10-20 00:44:21 +0200

    local dev conf files added

--- local-hotreload.sh
+++ local-hotreload.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-services=("redis" "db" "api" "persistence" "base" "pubmed" "dataprocessing")
-
-
 docker compose --env-file ./local_dev/flavorconfigs/dev.env stop
-for service in ${services[@]}; do
-    docker compose --env-file ./local_dev/flavorconfigs/dev.env up -d $service
-done
-cd ./localdev/proxy
+docker compose --env-file ./local_dev/flavorconfigs/dev.env up -d
+cd ./local_dev/proxy
 docker compose down
 docker compose up -d
