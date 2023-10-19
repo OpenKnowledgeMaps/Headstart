4232887e23d026921cf371c506a45f6bfd3e25d2
chreman <web@christopherkittel.eu>
2023-10-19 22:53:48 +0200

    update for docker compose engine version

--- local-hotreload.sh
+++ local-hotreload.sh
@@ -4,10 +4,10 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 services=("redis" "db" "api" "persistence" "base" "pubmed" "dataprocessing")
 
 
-docker-compose --env-file ./local_dev/flavorconfigs/dev.env stop
+docker compose --env-file ./local_dev/flavorconfigs/dev.env stop
 for service in ${services[@]}; do
-    docker-compose --env-file ./local_dev/flavorconfigs/dev.env up -d $service
+    docker compose --env-file ./local_dev/flavorconfigs/dev.env up -d $service
 done
 cd ./localdev/proxy
-docker-compose down
-docker-compose up -d
+docker compose down
+docker compose up -d
