b1a2213d7e1ab466889d271f65feee834e6b3b97
chreman <web@christopherkittel.eu>
2023-10-30 08:39:50 +0100

    wip

--- local-hotreload.sh
+++ local-hotreload.sh
@@ -2,7 +2,7 @@
 
 npm run prod -- --mode=development
 docker compose --env-file ./local_dev/dev.env stop
-docker compose --env-file ./local_dev/dev.env up -d
+docker compose --env-file ./local_dev/dev.env up -d --build
 cd ./local_dev/proxy
 docker compose down
 docker compose up -d
