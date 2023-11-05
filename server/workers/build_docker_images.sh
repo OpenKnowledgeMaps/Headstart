4358c72160208c397e6910388fe4294eeb0b83f6
chreman <web@christopherkittel.eu>
2023-11-05 23:14:44 +0100

    build updates

--- server/workers/build_docker_images.sh
+++ server/workers/build_docker_images.sh
@@ -2,6 +2,8 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 services=("api" "persistence" "dataprocessing" "base" "pubmed" "openaire")
 for service in ${services[@]}; do
+    echo ""
+    echo "Building $service"
+    echo ""
     docker build -f "$SCRIPT_DIR/../workers/$service/Dockerfile" -t "$service:`git rev-parse HEAD`" "$SCRIPT_DIR/../"
 done
-
