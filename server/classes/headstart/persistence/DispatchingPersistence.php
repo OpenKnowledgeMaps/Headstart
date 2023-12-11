4edf4040a330b4a4e3df6b943045247a2840eddc
Alexandra Shubenko <as@belichberg.com>
2023-12-11 16:54:50 +0100

    cleanup sqlite variables

--- server/classes/headstart/persistence/DispatchingPersistence.php
+++ server/classes/headstart/persistence/DispatchingPersistence.php
@@ -2,118 +2,55 @@
 
 namespace headstart\persistence;
 
-
 /**
  * This class implements the PersistenceInterface and provides methods to interact with the database.
  */
 class DispatchingPersistence implements Persistence
 {
-    private Persistence $oldPersistence;
     private Persistence $newPersistence;
-    private int|float $shiftReadPercentage;
 
-
-    public function __construct(Persistence $oldPersistence, Persistence $newPersistence, $shiftReadPercentage)
+    public function __construct(Persistence $newPersistence)
     {
-        $this->shiftReadPercentage = $shiftReadPercentage;
-        $this->oldPersistence = $oldPersistence;
         $this->newPersistence = $newPersistence;
     }
 
     public function createVisualization($vis_id, $vis_title, $input_json, $query, $dirty_query, $params_json): void
     {
-        $this->oldPersistence->createVisualization($vis_id, $vis_title, $input_json, $query, $dirty_query, $params_json);
         $this->newPersistence->createVisualization($vis_id, $vis_title, $input_json, $query, $dirty_query, $params_json);
     }
 
     public function getRevision($vis_id, $rev_id): array|bool
     {
-        $randomFloat = getRandomFloat();
-
-        if (($randomFloat * 100) > ($this->shiftReadPercentage * 100)) {
-            $result = $this->oldPersistence->getRevision($vis_id, $rev_id);
-        } else {
-            $result = $this->newPersistence->getRevision($vis_id, $rev_id);
-        }
-
-        return $result;
+        return $this->newPersistence->getRevision($vis_id, $rev_id);
     }
 
     public function writeRevision($vis_id, $data): void
     {
-        $this->oldPersistence->writeRevision($vis_id, $data);
         $this->newPersistence->writeRevision($vis_id, $data);
     }
 
     public function existsVisualization($vis_id): array|bool
     {
-        $randomFloat = getRandomFloat();
-
-        if ($randomFloat * 100 > $this->shiftReadPercentage * 100) {
-            $result = $this->oldPersistence->existsVisualization($vis_id);
-        } else {
-            $result = $this->newPersistence->existsVisualization($vis_id);
-        }
-
-        return $result;
+        return $this->newPersistence->existsVisualization($vis_id);
     }
 
     public function getLastVersion($vis_id, $details, $context): array|bool
     {
-        $randomFloat = getRandomFloat();
-
-        if ($randomFloat * 100 > $this->shiftReadPercentage * 100) {
-            $result = $this->oldPersistence->getLastVersion($vis_id, $details, $context);
-        } else {
-            $result = $this->newPersistence->getLastVersion($vis_id, $details, $context);
-        }
-
-        return $result;
+        return $this->newPersistence->getLastVersion($vis_id, $details, $context);
     }
 
     public function getLatestRevisions(): array|bool
     {
-        $randomFloat = getRandomFloat();
-
-        if ($randomFloat * 100 > $this->shiftReadPercentage * 100) {
-            $result = $this->oldPersistence->getLatestRevisions();
-        } else {
-            $result = $this->newPersistence->getLatestRevisions();
-        }
-
-        return $result;
+        return $this->newPersistence->getLatestRevisions();
     }
 
     public function getContext($vis_id): array|bool
     {
-        $randomFloat = getRandomFloat();
-
-        if ($randomFloat * 100 > $this->shiftReadPercentage * 100) {
-            $result = $this->oldPersistence->getContext($vis_id);
-        } else {
-            $result = $this->newPersistence->getContext($vis_id);
-        }
-
-        return $result;
+        return $this->newPersistence->getContext($vis_id);
     }
 
     public function createID($string_array, $payload): string
     {
-        $randomFloat = getRandomFloat();
-
-        if ($randomFloat * 100 > $this->shiftReadPercentage * 100) {
-            $result = $this->oldPersistence->createID($string_array, $payload);
-        } else {
-            $result = $this->newPersistence->createID($string_array, $payload);
-        }
-
-        return $result;
+        return $this->newPersistence->createID($string_array, $payload);
     }
-}
-
-
-//    function for random float number generation from 0 to 1
-function getRandomFloat(): float
-{
-    return mt_rand() / mt_getrandmax();
 }
