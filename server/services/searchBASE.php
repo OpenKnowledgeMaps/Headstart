489be1783c1ccf34497e5fb5df1a1c6038fc4b0c
chreman <web@christopherkittel.eu>
2024-03-25 23:25:09 +0100

    ID creation bugfix

--- server/services/searchBASE.php
+++ server/services/searchBASE.php
@@ -11,7 +11,8 @@ $dirty_query = library\CommUtils::getParameter($_POST, "q");
 $precomputed_id = (isset($_POST["unique_id"]))?($_POST["unique_id"]):(null);
 
 $params_array = array("document_types", "sorting", "min_descsize");
-$optional_get_params = ["repo", "coll", "vis_type", "q_advanced", "lang_id", "custom_title", "exclude_date_filters", "today", "from", "to", "custom_clustering"];
+$optional_get_params = ["repo", "coll", "vis_type", "q_advanced", "lang_id", "custom_title", "exclude_date_filters", "from", "to", "custom_clustering"];
+
 
 function filterEmptyString($value)
 {
@@ -43,6 +44,23 @@ if (isset($post_params["exclude_date_filters"]) && $post_params["exclude_date_fi
     $params_array = array_merge($params_array, ["today"]);
     unset($params_array["from"], $params_array["to"]);
 }
+
+// re-establish historic order for backwards ID compatibility
+$historic_params_order = array("from", "to", "document_types", "sorting", "min_descsize", "repo");
+$reordered_params = array();
+foreach($historic_params_order as $param) {
+    if (isset($post_params[$param])) {
+        $reordered_params[] = $param;
+    }
+}
+foreach($params_array as $param) {
+    if (!in_array($param, $reordered_params)) {
+        $reordered_params[] = $param;
+    }
+}
+$params_array = $reordered_params;
+
+
 
 $result = search("base", $dirty_query
                   , $post_params, $params_array
