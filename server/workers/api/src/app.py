f4180681d36680353ea93b67333cfac94c2e3f81
chreman <web@christopherkittel.eu>
2024-04-24 14:14:55 +0200

    wip

--- server/workers/api/src/app.py
+++ server/workers/api/src/app.py
@@ -11,6 +11,7 @@ from apis.pubmed import pubmed_ns
 from apis.openaire import openaire_ns
 from apis.create_vis import vis_ns
 from apis.export import export_ns
+from apis.orcid import orcid_ns
 
 
 class ReverseProxied(object):
@@ -72,6 +73,7 @@ api.add_namespace(pubmed_ns, path='/pubmed')
 api.add_namespace(openaire_ns, path='/openaire')
 api.add_namespace(vis_ns, path='/vis')
 api.add_namespace(export_ns, path='/export')
+api.add_namespace(orcid_ns, path='/orcid')
 
 app.logger.debug(app.config)
 app.logger.debug(app.url_map)
