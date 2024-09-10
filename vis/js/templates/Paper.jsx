fd51a0482f8de99bbef2dfc92e432692625be550
Krutilin Sergey <sergey.krutilin@modsen-software.com>
2024-09-10 15:06:26 +0200

    fix: paper

--- vis/js/templates/Paper.jsx
+++ vis/js/templates/Paper.jsx
@@ -217,7 +217,7 @@ class Paper extends React.Component {
               <div
                 className="metadata"
                 style={{
-                  height: getMetadataHeight(realHeight, !!readersLabel, zoom),
+                  height: getMetadataHeight(realHeight, !!readersLabel + !!socialMediaLabel + !!citationsLabel + !!referencesLabel + !!tweetsLabel, zoom),
                   width: (1 - DOGEAR_WIDTH) * realWidth,
                 }}
                 ref={this.metadataRef}
@@ -262,7 +262,7 @@ class Paper extends React.Component {
                   <div className="readers">
                     <p id="readers" className={sizeModifierClass}>
                       <span id="num-readers">{readers} </span>
-                      <span className="readers_entity">{readersLabel}</span>
+                      <span className="readers_entity">{readersLabel || 'n/a'}</span>
                     </p>
                   </div>
                 )}
@@ -272,7 +272,7 @@ class Paper extends React.Component {
                   <div className="readers">
                     <p id="readers" className={sizeModifierClass}>
                       <span id="num-readers">{social} </span>
-                      <span className="readers_entity">{socialMediaLabel}</span>
+                      <span className="readers_entity">{socialMediaLabel || 'n/a'}</span>
                     </p>
                   </div>
                 )}
@@ -282,7 +282,7 @@ class Paper extends React.Component {
                   <div className="readers">
                     <p id="readers" className={sizeModifierClass}>
                       <span id="num-readers">{citations} </span>
-                      <span className="readers_entity">{citationsLabel}</span>
+                      <span className="readers_entity">{citationsLabel || 'n/a'}</span>
                     </p>
                   </div>
                 )}
@@ -292,7 +292,7 @@ class Paper extends React.Component {
                   <div className="readers">
                     <p id="readers" className={sizeModifierClass}>
                       <span id="num-readers">{references} </span>
-                      <span className="readers_entity">{referencesLabel}</span>
+                      <span className="readers_entity">{referencesLabel || 'n/a'}</span>
                     </p>
                   </div>
                 )}
@@ -302,7 +302,7 @@ class Paper extends React.Component {
                   <div className="readers">
                     <p id="readers" className={sizeModifierClass}>
                       <span id="num-readers">{tweets} </span>
-                      <span className="readers_entity">{tweetsLabel}</span>
+                      <span className="readers_entity">{tweetsLabel || 'n/a'}</span>
                     </p>
                   </div>
                 )}
@@ -489,14 +489,10 @@ const getEnlargeFactor = (offsetWidth, scrollHeight) => {
   return (newWidth / offsetWidth) * (1.0 / (1 - DOGEAR_WIDTH));
 };
 
-const getMetadataHeight = (realHeight, hasReaders, isZoomed) => {
-  let readersHeight = 0;
-  if (hasReaders) {
-    if (isZoomed) {
-      readersHeight = 22;
-    } else {
-      readersHeight = 12;
-    }
+const getMetadataHeight = (realHeight, numOfLabels, isZoomed) => {
+  let readersHeight = 12;
+  if (numOfLabels && isZoomed) {
+      readersHeight =+ numOfLabels * 15;
   }
 
   const height = realHeight - readersHeight;
