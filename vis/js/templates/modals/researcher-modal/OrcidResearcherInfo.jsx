ac1ee1d95acfeda8b6dd912b01e322d41ac8b519
Krutilin Sergey <sergey.krutilin@modsen-software.com>
2024-09-26 09:39:39 +0200

    feat: improve researche details modal (add show more button)

--- vis/js/templates/modals/researcher-modal/OrcidResearcherInfo.jsx
+++ vis/js/templates/modals/researcher-modal/OrcidResearcherInfo.jsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import { Modal } from "react-bootstrap";
 import { connect } from "react-redux";
 
-const DEFAULT_FALLBACK = "not available";
-
 const ResearcherInfo = ({ params }) => {
   const noDataAvailable = "No data available";
   const [showMore, setShowMore] = useState({
@@ -19,7 +17,14 @@ const ResearcherInfo = ({ params }) => {
 
   const formatEmployment = (employment) => {
     const endDate = employment.end_date ? employment.end_date : "present";
-    return `${employment.start_date} - ${endDate} / ${employment.role} / ${employment.organization} / ${employment.organization_address || DEFAULT_FALLBACK}`;
+    return [
+      `${employment.start_date} - ${endDate}`,
+      employment.role,
+      employment.organization,
+      employment.organization_address // Only displays if it exists.
+    ]
+      .filter(Boolean) // Filters out any missing data.
+      .join(" / ");
   };
 
   return (
@@ -29,22 +34,20 @@ const ResearcherInfo = ({ params }) => {
       </Modal.Header>
       <Modal.Body id="info-body">
         {/* Employment Section */}
-        <h3>EMPLOYMENT ({params.employments?.length || 0})</h3>
+        <h3>
+          EMPLOYMENT{" "}
+          {params.employments?.length ? `(${params.employments.length})` : ""}
+        </h3>
         {params.employments?.length ? (
           <>
             <p>{formatEmployment(params.employments[0])}</p>
             {params.employments.length > 1 && (
               <>
-                {showMore.employments &&
-                  params.employments
-                    .slice(1)
-                    .map((employment) => (
-                      <p key={employment.id}>{formatEmployment(employment)}</p>
-                    ))}
+                {showMore.employments && params.employments.slice(1).map((employment) => (
+                  <p key={employment.id}>{formatEmployment(employment)}</p>
+                ))}
                 <button onClick={() => handleShowMore("employments")}>
-                  <i
-                    className={`fas fa-${showMore.employments ? "chevron-up" : "chevron-down"}`}
-                  ></i>
+                  <i className={`fas fa-${showMore.employments ? "chevron-up" : "chevron-down"}`}></i>
                   {showMore.employments ? "Show Less" : "Show More"}
                 </button>
               </>
@@ -55,24 +58,32 @@ const ResearcherInfo = ({ params }) => {
         )}
 
         {/* Education Section */}
-        <h3>EDUCATION & QUALIFICATIONS ({params.educations?.length || 0})</h3>
+        <h3>
+          EDUCATION & QUALIFICATIONS{" "}
+          {params.educations?.length ? `(${params.educations.length})` : ""}
+        </h3>
         {params.educations?.length ? (
           <>
-            <p>{`${params.educations[0].start_date} - ${params.educations[0].end_date || "present"} / ${params.educations[0].role} / ${params.educations[0].organization} / ${params.educations[0].organization_address || DEFAULT_FALLBACK}`}</p>
+            <p>{[
+              `${params.educations[0].start_date} - ${params.educations[0].end_date || "present"}`,
+              params.educations[0].role,
+              params.educations[0].organization,
+              params.educations[0].organization_address
+            ]
+              .filter(Boolean)
+              .join(" / ")}</p>
             {params.educations.length > 1 && (
               <>
-                {showMore.educations &&
-                  params.educations
-                    .slice(1)
-                    .map((education) => (
-                      <p
-                        key={education.id}
-                      >{`${education.start_date} - ${education.end_date || "present"} / ${education.role} / ${education.organization} / ${education.organization_address || DEFAULT_FALLBACK}`}</p>
-                    ))}
+                {showMore.educations && params.educations.slice(1).map((education) => (
+                  <p key={education.id}>{[
+                    `${education.start_date} - ${education.end_date || "present"}`,
+                    education.role,
+                    education.organization,
+                    education.organization_address
+                  ].filter(Boolean).join(" / ")}</p>
+                ))}
                 <button onClick={() => handleShowMore("educations")}>
-                  <i
-                    className={`fas fa-${showMore.educations ? "chevron-up" : "chevron-down"}`}
-                  ></i>
+                  <i className={`fas fa-${showMore.educations ? "chevron-up" : "chevron-down"}`}></i>
                   {showMore.educations ? "Show Less" : "Show More"}
                 </button>
               </>
@@ -83,35 +94,31 @@ const ResearcherInfo = ({ params }) => {
         )}
 
         {/* Funding Section */}
-        <h3>FUNDING ({params.funds?.length || 0})</h3>
+        <h3>
+          FUNDING {params.funds?.length ? `(${params.funds.length})` : ""}
+        </h3>
         {params.funds?.length ? (
           <>
-            <p>
-              {params.funds[0].title} / {params.funds[0].start_date} -{" "}
-              {params.funds[0].end_date} / Funder:{" "}
-              {params.funds[0].organization} / Amount:{" "}
-              {params.funds[0].amount?.value} {params.funds[0].amount?.currency}
-            </p>
+            <p>{[
+              params.funds[0].title,
+              `${params.funds[0].start_date} - ${params.funds[0].end_date}`,
+              `Funder: ${params.funds[0].organization}`,
+              `Amount: ${params.funds[0].amount?.value} ${params.funds[0].amount?.currency}`
+            ].filter(Boolean).join(" / ")}</p>
           </>
         ) : (
           <p>{noDataAvailable}</p>
         )}
 
         {/* Links Section */}
-        <h3>LINKS</h3>
+        <h3>
+          LINKS {params.external_identifiers?.length ? `(${params.external_identifiers.length})` : ""}
+        </h3>
         {params.external_identifiers?.length ? (
           <>
-            <i
-              className="fas fa-solid fa-link"
-              style={{ paddingRight: "3px" }}
-            ></i>
-            <a
-              href={params.external_identifiers[0].url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {params.external_identifiers[0].type}:{" "}
-              {params.external_identifiers[0].value}
+            <i className="fas fa-solid fa-link" style={{ paddingRight: "3px" }}></i>
+            <a href={params.external_identifiers[0].url} target="_blank" rel="noopener noreferrer">
+              {params.external_identifiers[0].type}: {params.external_identifiers[0].value}
             </a>
           </>
         ) : (
@@ -119,24 +126,25 @@ const ResearcherInfo = ({ params }) => {
         )}
 
         {/* Distinctions Section */}
-        <h3>DISTINCTIONS / AWARDS ({params.distinctions?.length || 0})</h3>
+        <h3>
+          DISTINCTIONS / AWARDS {params.distinctions?.length ? `(${params.distinctions.length})` : ""}
+        </h3>
         {params.distinctions?.length ? (
           <>
-            <p>{`${params.distinctions[0].start_date || "Unknown"} / ${params.distinctions[0].title}`}</p>
+            <p>{[
+              params.distinctions[0].start_date,
+              params.distinctions[0].title
+            ].filter(Boolean).join(" / ")}</p>
             {params.distinctions.length > 1 && (
               <>
-                {showMore.distinctions &&
-                  params.distinctions
-                    .slice(1)
-                    .map((distinction) => (
-                      <p
-                        key={distinction.id}
-                      >{`${distinction.start_date || "Unknown"} / ${distinction.title}`}</p>
-                    ))}
+                {showMore.distinctions && params.distinctions.slice(1).map((distinction) => (
+                  <p key={distinction.id}>{[
+                    distinction.start_date,
+                    distinction.title
+                  ].filter(Boolean).join(" / ")}</p>
+                ))}
                 <button onClick={() => handleShowMore("distinctions")}>
-                  <i
-                    className={`fas fa-${showMore.distinctions ? "chevron-up" : "chevron-down"}`}
-                  ></i>
+                  <i className={`fas fa-${showMore.distinctions ? "chevron-up" : "chevron-down"}`}></i>
                   {showMore.distinctions ? "Show Less" : "Show More"}
                 </button>
               </>
