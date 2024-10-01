e1f54e8251c6dbf2df8c5dde5fca5000389d6673
Krutilin Sergey <sergey.krutilin@modsen-software.com>
2024-10-01 07:51:14 +0200

    minor improvement

--- vis/js/templates/listentry/Keywords.jsx
+++ vis/js/templates/listentry/Keywords.jsx
@@ -7,14 +7,12 @@ const Keywords = ({ children: text }) => {
   const localization = useLocalizationContext();
 
   return (
-    // html template starts here
     <div className="list_row">
       <span className="list_row_label">{localization.keywords}: </span>
       <span className="list_row_content">
         <Highlight queryHighlight>{text}</Highlight>
       </span>
     </div>
-    // html template ends here
   );
 };
 
