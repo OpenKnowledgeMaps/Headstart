91eb13b0c44dda68015831a981f5388ee0a91e6c
Thomas Arrow <tom@openknowledgemaps.org>
2024-12-16 16:19:01 +0000

    TomChris thoughts from 2024-12-16 working session

--- server/services/getContext.php
+++ server/services/getContext.php
@@ -18,11 +18,27 @@ $vis_id = library\CommUtils::getParameter($_GET, "vis_id");
 $revision_context =  isset($_GET["revision_context"]) ? library\CommUtils::getParameter($_GET, "revision_context") : false;
 
 $apiclient = new headstart\library\APIClient($ini_array);
-$postgresPersistence = new \headstart\persistence\PostgresPersistence($apiclient);
-$persistence = new \headstart\persistence\DispatchingPersistence($postgresPersistence);
+$persistence = new \headstart\persistence\PostgresPersistence($apiclient);
+
+// Remove the calls to this getContext on the persistence object. Replace instead with a getContextResultBuilder that returns a result object
+// This should contain the httpStatusCode to pass to the client, the result in string form (some json encoded string) and it should never throw.
 
 
+// [thing that formats the response for the user] - [thing that parses responses from the persistence api] - [thing that makes requests to persisent api and handles the network calls etc.]
+
+// Err... why is the only caller of this having to unpack the array to take the first result? Is someone wrapping this reponse in an array unecessarily?
 $data = $persistence->getContext($vis_id)[0];
+
+/*
+It it works
+{
+  "rev_timestamp": "Thu, 12 Sep 2024 10:23:47 GMT",
+  "rev_vis": "d161dba364a1e8c0468b9c74407e3575",
+  "vis_params": "{\"id\": \"d161dba364a1e8c0468b9c74407e3575\", \"query\": \"\\\\\\\"climate change\\\\\\\" and impact\", \"service\": \"base\", \"timestamp\": \"Wed, 24 Aug 2022 14:24:13 GMT\", \"params\": \"{\\\"from\\\":\\\"1665-01-01\\\",\\\"to\\\":\\\"2022-08-24\\\",\\\"document_types\\\":[\\\"121\\\"],\\\"sorting\\\":\\\"most-relevant\\\",\\\"min_descsize\\\":\\\"300\\\"}\"}",
+  "vis_query": "\\\"climate change\\\" and impact",
+  "vis_title": "base"
+}
+*/
 $return_data = array("id" => $data["rev_vis"],
                       "query" => $data["vis_query"],
                       "service" => $data["vis_title"],
@@ -32,4 +48,5 @@ if (array_key_exists("additional_context", $data)) {
   $return_data = array_merge($return_data, $data["additional_context"]);
 }
 $jsonData = json_encode($return_data);
+// To investigate: is anyone needing the callback version of this??
 library\CommUtils::echoOrCallback($jsonData, $_GET);
