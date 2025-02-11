16b4a2da12f0b8d735caac4e3b16bfa203963a33
Sergey Krutilin <sergey.krutilin@modsen-software.com>
2025-02-11 19:11:23 +0100

    feat: extract r packages base image for metrics

---
+++ server/workers/metrics/Dockerfile.r
@@ -0,0 +1,145 @@
+FROM ubuntu:20.04
+
+LABEL maintainer="Chris Kittel <christopher.kittel@openknowledgemaps.org>"
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+ARG R_VERSION
+ARG BUILD_DATE
+ARG CRAN
+## Default R version and CRAN mirror
+ENV R_VERSION=${R_VERSION:-3.6.3} \
+    CRAN=${CRAN:-https://cran.rstudio.com}
+
+# Install essential system dependencies and build tools
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    bash-completion \
+    ca-certificates \
+    file \
+    fonts-texgyre \
+    g++ \
+    gfortran \
+    gsfonts \
+    libblas-dev \
+    libbz2-1.0 \
+    libcurl4 \
+    libjpeg-turbo8-dev \
+    libopenblas-dev \
+    libpangocairo-1.0-0 \
+    libpcre3 \
+    libpng16-16 \
+    libreadline-dev \
+    libtiff5 \
+    liblzma5 \
+    locales \
+    make \
+    unzip \
+    zip \
+    zlib1g && \
+    \
+    BUILDDEPS="curl \
+    default-jdk \
+    libbz2-dev \
+    libcairo2-dev \
+    libcurl4-openssl-dev \
+    libpango1.0-dev \
+    libjpeg-dev \
+    libpcre3-dev \
+    libpng-dev \
+    libreadline-dev \
+    libtiff5-dev \
+    liblzma-dev \
+    libx11-dev \
+    libxt-dev \
+    perl \
+    tcl8.6-dev \
+    tk8.6-dev \
+    x11proto-core-dev \
+    xauth \
+    xfonts-base \
+    xvfb \
+    zlib1g-dev" && \
+    apt-get install -y --no-install-recommends $BUILDDEPS
+
+# Build and install R from source
+RUN mkdir -p /tmp/build && cd /tmp/build && \
+    curl -O https://cran.r-project.org/src/base/R-3/R-${R_VERSION}.tar.gz && \
+    tar -xf R-${R_VERSION}.tar.gz && cd R-${R_VERSION} && \
+    R_PAPERSIZE=letter \
+    R_BATCHSAVE="--no-save --no-restore" \
+    R_BROWSER=xdg-open \
+    PAGER=/usr/bin/pager \
+    PERL=/usr/bin/perl \
+    R_UNZIPCMD=/usr/bin/unzip \
+    R_ZIPCMD=/usr/bin/zip \
+    R_PRINTCMD=/usr/bin/lpr \
+    LIBnn=lib \
+    AWK=/usr/bin/awk \
+    CFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -g" \
+    CXXFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -g" \
+    ./configure --enable-R-shlib --enable-memory-profiling --with-readline --with-blas --with-tcltk --disable-nls --with-recommended-packages && \
+    make && make install && \
+    mkdir -p /usr/local/lib/R/site-library && \
+    chown root:staff /usr/local/lib/R/site-library && chmod g+ws /usr/local/lib/R/site-library && \
+    sed -i '/^R_LIBS_USER=.*$/d' /usr/local/lib/R/etc/Renviron && \
+    echo "R_LIBS_USER=\${R_LIBS_USER-'/usr/local/lib/R/site-library'}" >> /usr/local/lib/R/etc/Renviron && \
+    echo "R_LIBS=\${R_LIBS-'/usr/local/lib/R/site-library:/usr/local/lib/R/library:/usr/lib/R/library'}" >> /usr/local/lib/R/etc/Renviron && \
+    if [ -z "$BUILD_DATE" ]; then MRAN=$CRAN; else MRAN=https://mran.microsoft.com/snapshot/${BUILD_DATE}; fi && \
+    echo MRAN=$MRAN >> /etc/environment && \
+    echo "options(repos = c(CRAN='$MRAN'), download.file.method = 'libcurl')" >> /usr/local/lib/R/etc/Rprofile.site && \
+    Rscript -e "install.packages(c('littler', 'docopt'), repo='$CRAN')" && \
+    ln -s /usr/local/lib/R/site-library/littler/examples/install2.r /usr/local/bin/install2.r && \
+    ln -s /usr/local/lib/R/site-library/littler/examples/installGithub.r /usr/local/bin/installGithub.r && \
+    ln -s /usr/local/lib/R/site-library/littler/bin/r /usr/local/bin/r && \
+    cd / && rm -rf /tmp/build && \
+    apt-get remove --purge -y $BUILDDEPS && \
+    apt-get autoremove -y && \
+    apt-get autoclean -y && \
+    rm -rf /var/lib/apt/lists/*
+
+# Set up locale
+RUN locale-gen en_US.UTF-8 && \
+    update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 && \
+    dpkg-reconfigure locales
+
+# Install additional packages that R packages might need
+RUN apt update && apt full-upgrade -y && \
+    apt install -y \
+    links \
+    curl \
+    vim \
+    libcurl4-openssl-dev \
+    libxml2-dev \
+    libz-dev \
+    libpoppler-cpp-dev \
+    libopenmpi-dev \
+    libzmq3-dev \
+    build-essential \
+    python3-dev \
+    libssl1.1 \
+    libssl-dev && \
+    apt clean && \
+    rm -f /etc/localtime && \
+    ln -s /usr/share/zoneinfo/Europe/Vienna /etc/localtime && \
+    dpkg --configure -a
+
+# Install automake and extra R packages (remotes and renv)
+RUN apt-get -y install automake && \
+    R -e 'options(repos="https://cran.wu.ac.at")' && \
+    R -e 'install.packages("remotes")' && \
+    R -e 'install.packages("renv", version="0.14.0-5")'
+
+# Prepare the working directory and restore R package dependencies using renv
+WORKDIR /headstart
+COPY workers/metrics/renv.lock .
+COPY workers/metrics/activate.R .
+
+RUN R -e 'renv::consent(provided = TRUE)' && \
+    R -e 'setwd("/headstart"); renv::activate(); renv::restore(lockfile = "renv.lock")'
+
+# Copy a simple test script to verify the R installation and packages
+COPY workers/metrics/test_r_packages.R /usr/local/bin/test_r_packages.R
+RUN chmod +x /usr/local/bin/test_r_packages.R
+
+# Default command: show the R version (overrideable for testing)
+CMD ["R", "--version"]
