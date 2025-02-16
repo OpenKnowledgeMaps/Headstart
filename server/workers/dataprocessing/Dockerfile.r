ac50b01fad107b7e6b0dd9f1c51d66f26365d469
Sergey Krutilin <sergey.krutilin@modsen-software.com>
2025-02-17 00:10:42 +0100

    feat: data processing extract base image docker file R packages

--- server/workers/dataprocessing/Dockerfile.r
+++ server/workers/dataprocessing/Dockerfile.r
@@ -1,4 +1,5 @@
-FROM ubuntu:20.04
+# Dockerfile.r
+FROM ubuntu:18.04
 
 LABEL maintainer="Chris Kittel <christopher.kittel@openknowledgemaps.org>"
 
@@ -26,7 +27,7 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     libpangocairo-1.0-0 \
     libpcre3 \
     libpng16-16 \
-    libreadline-dev \
+    libreadline7 \
     libtiff5 \
     liblzma5 \
     locales \
@@ -34,7 +35,11 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     unzip \
     zip \
     zlib1g \
-    && BUILDDEPS="curl \
+    && echo "en_US.UTF-8" >> /etc/locale.gen \
+    && locale-gen en_US.UTF-8 \
+    && /usr/sbin/update-locale LANG=en_US.UTF-8
+
+ENV BUILDDEPS="curl \
     default-jdk \
     libbz2-dev \
     libcairo2-dev \
@@ -55,8 +60,9 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
     xauth \
     xfonts-base \
     xvfb \
-    zlib1g-dev" \
-    && apt-get install -y --no-install-recommends $BUILDDEPS
+    zlib1g-dev"
+
+RUN apt-get install -y --no-install-recommends $BUILDDEPS
 
 RUN mkdir -p /tmp/build && cd /tmp/build \
     && curl -O https://cran.r-project.org/src/base/R-3/R-${R_VERSION}.tar.gz \
@@ -81,7 +87,8 @@ RUN mkdir -p /tmp/build && cd /tmp/build \
     --with-tcltk \
     --disable-nls \
     --with-recommended-packages \
-    && make && make install
+    && make \
+    && make install
 
 RUN mkdir -p /usr/local/lib/R/site-library \
     && chown root:staff /usr/local/lib/R/site-library \
@@ -91,13 +98,14 @@ RUN mkdir -p /usr/local/lib/R/site-library \
     && echo "R_LIBS=\${R_LIBS-'/usr/local/lib/R/site-library:/usr/local/lib/R/library:/usr/lib/R/library'}" >> /usr/local/lib/R/etc/Renviron \
     && if [ -z "$BUILD_DATE" ]; then MRAN=$CRAN; else MRAN=https://mran.microsoft.com/snapshot/${BUILD_DATE}; fi \
     && echo MRAN=$MRAN >> /etc/environment \
-    && echo "options(repos = c(CRAN='$MRAN'), download.file.method = 'libcurl')" >> /usr/local/lib/R/etc/Rprofile.site \
-    && Rscript -e "install.packages(c('littler', 'docopt'), repo = '$CRAN')" \
+    && echo "options(repos = c(CRAN='$MRAN'), download.file.method = 'libcurl')" >> /usr/local/lib/R/etc/Rprofile.site
+
+RUN Rscript -e "install.packages(c('littler', 'docopt'), repo = '$CRAN')" \
     && ln -s /usr/local/lib/R/site-library/littler/examples/install2.r /usr/local/bin/install2.r \
     && ln -s /usr/local/lib/R/site-library/littler/examples/installGithub.r /usr/local/bin/installGithub.r \
     && ln -s /usr/local/lib/R/site-library/littler/bin/r /usr/local/bin/r
 
-RUN rm -rf /tmp/build \
+RUN cd / && rm -rf /tmp/build \
     && apt-get remove --purge -y $BUILDDEPS \
     && apt-get autoremove -y \
     && apt-get autoclean -y \
