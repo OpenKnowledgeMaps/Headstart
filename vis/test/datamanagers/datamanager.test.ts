b18fd6de2a7e017ef85e87835c497b22b8984699
andrei <andrei.shket@modsen-software.com>
2025-03-20 10:39:23 +0100

    refactor: datamanager.test as .ts file

--- vis/test/datamanagers/datamanager.test.ts
+++ vis/test/datamanagers/datamanager.test.ts
@@ -7,29 +7,30 @@ import {
   productionKMData,
   productionKMDataModified,
 } from "../data/base-raw";
+import { Config } from "../../js/@types/config";
+import { Paper } from "../../js/@types/paper";
 
-const config = {
-  show_context: true,
-  language: "en",
-  localization: { en: {} },
-};
+describe("Default data manager", () => {
+  it("Doesn't crash", () => {
+    const EXPECTED_PAPERS: Paper[] = [];
+    const MOCK_CONFIG: Partial<Config> = {
+      show_context: true,
+      language: "en",
+      localization: { en: {} },
+    };
 
-describe("default data manager", () => {
-  it("doesn't crash", () => {
-    const EXPECTED_PAPERS = [];
-
-    const dataManager = new DataManager(config);
-    dataManager.parseData({ data: [] });
+    const dataManager = new DataManager(MOCK_CONFIG as Config);
+    dataManager.parseData({ data: [] }, 1);
 
     const papers = dataManager.papers;
 
     expect(papers).toEqual(EXPECTED_PAPERS);
   });
 
-  describe("real BASE map data", () => {
+  describe("Real BASE map data", () => {
     console.warn = vitest.fn();
 
-    it("parses correct number of documents", () => {
+    it("Parses correct number of documents", () => {
       const EXPECTED_PAPERS_LENGTH = 2;
 
       const dataManager = new DataManager(productionKMConfig);
@@ -40,7 +41,7 @@ describe("default data manager", () => {
       expect(papers.length).toEqual(EXPECTED_PAPERS_LENGTH);
     });
 
-    it("creates a correct safe id", () => {
+    it("Creates a correct safe id", () => {
       const EXPECTED_SAFE_ID = "unsafe__0026dangerous-id";
 
       const dataManager = new DataManager(productionKMConfig);
@@ -51,7 +52,7 @@ describe("default data manager", () => {
       expect(papers[0].safe_id).toEqual(EXPECTED_SAFE_ID);
     });
 
-    it("moves a paper with the exact same coordinates as another paper", () => {
+    it("Moves a paper with the exact same coordinates as another paper", () => {
       const EXPECTED_X0 = 5;
       const EXPECTED_Y0 = 5;
       const EXPECTED_X1 = 5;
