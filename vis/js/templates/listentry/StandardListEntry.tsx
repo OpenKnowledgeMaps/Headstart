0632d2bdf96fd43e25c4f61d10c0776c606e8c8f
andrei <andrei.shket@modsen-software.com>
2025-03-21 14:05:17 +0100

    feat: __countMetrics test cases for all integrations

--- vis/js/templates/listentry/StandardListEntry.tsx
+++ vis/js/templates/listentry/StandardListEntry.tsx
@@ -19,7 +19,7 @@ import EntryBacklink from "./EntryBacklink";
 import Keywords from "./Keywords";
 import Link from "./Link";
 import Metrics from "./Metrics";
-import OrcidMetrics from './OrcidMetrics';
+import OrcidMetrics from "./OrcidMetrics";
 import Title from "./Title";
 
 /**
@@ -124,7 +124,9 @@ const mapStateToProps = (state) => ({
   showBacklink: state.chartType === STREAMGRAPH_MODE && !!state.selectedPaper,
   isInStreamBacklink: !!state.selectedBubble,
   showDocTags: state.service === "base" || state.service === "orcid",
-  showAllDocTypes: (state.service === "base" || state.service === "orcid") && !!state.selectedPaper,
+  showAllDocTypes:
+    (state.service === "base" || state.service === "orcid") &&
+    !!state.selectedPaper,
   service: state.service,
 });
 
