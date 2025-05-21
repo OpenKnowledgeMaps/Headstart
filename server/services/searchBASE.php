e203c434d65a6979ba4a94349c0c413557900750
andrei <andrei.shket@modsen-software.com>
2025-05-21 11:14:41 +0200

    refactor: searchBASE

--- server/services/searchBASE.php
+++ server/services/searchBASE.php
@@ -7,70 +7,106 @@ include_once dirname(__FILE__) . '/utils/normalizeAndSanitizeString.php';
 include_once dirname(__FILE__) . '/utils/getPrecomputedIdFromRequest.php';
 include_once dirname(__FILE__) . '/utils/getQueryParameterFromRequest.php';
 
+/**
+ * Re-establish historic order for backwards ID compatibility
+ * Order: [from, to, document_types, sorting, min_descsize,
+ * repo, coll, vis_type, lang_id, q_advanced, exclude_date_filters,
+ * custom_title, custom_clustering].
+ * @param array $parameters - Array with parameters.
+ * @return array - Reordered array with parameters.
+ */
+function reorderParametersForIdCompatibility(array $parameters): array {
+    $reorderedParameters = [];
+    $historicOrder = [
+        "from",
+        "to",
+        "document_types",
+        "sorting",
+        "min_descsize",
+        "repo",
+        "coll",
+        "vis_type",
+        "lang_id",
+        "q_advanced",
+        "exclude_date_filters",
+        "custom_title",
+        "custom_clustering"
+    ];
+
+    foreach($historicOrder as $param) {
+        if (isset($requestArray[$param])) {
+            $reorderedParameters[] = $param;
+        }
+    }
+
+    foreach($parameters as $param) {
+        if (!in_array($param, $reorderedParameters)) {
+            $reorderedParameters[] = $param;
+        }
+    }
+
+    return $reorderedParameters;
+}
+
+/**
+ * Preparing parameters array.
+ * @param array $requestArray - Request array ($_POST).
+ * @return array - Array with parameters.
+ */
+function getParameters(array $requestArray): array {
+    $parameters = ["document_types", "sorting", "min_descsize"];
+    $optionalParameters = ["repo", "coll", "vis_type", "q_advanced", "lang_id", "custom_title", "exclude_date_filters", "from", "to", "custom_clustering"];
+
+    // Adding optional parameters if they were passed
+    foreach ($optionalParameters as $param) {
+        if (isset($_POST[$param])) {
+            $parameters[] = $param;
+        }
+    }
+
+    // Set min_descsize if it is not
+    if (!isset($requestArray["min_descsize"])) {
+        $requestArray["min_descsize"] = 300;
+    }
+
+    // Set lang_id even if it is empty
+    if (isset($requestArray["lang_id"])) {
+        $requestArray["lang_id"] = array_filter(
+            $requestArray["lang_id"],
+            fn(string $value): bool => $value !== ''
+        );
+
+        if (count($requestArray["lang_id"]) === 0) {
+            $requestArray["lang_id"] = ["all-lang"];
+        }
+    }
+
+    // Ignore exclude date filters parameter if visualization type is timeline
+    if (isset($requestArray["vis_type"]) && $requestArray["vis_type"] == "timeline") {
+        unset($parameters[array_search("exclude_date_filters", $parameters)]);
+        unset($requestArray["exclude_date_filters"]);
+    }
+
+    // Remove date filters and add "today" parameter if exclude date filters parameter is set
+    if (isset($requestArray["exclude_date_filters"]) && $requestArray["exclude_date_filters"] === true) {
+        $parameters = array_merge($parameters, ["today"]);
+        unset($parameters["from"], $parameters["to"]);
+    }
+
+    return reorderParametersForIdCompatibility($parameters);
+}
+
 $query = getQueryParameterFromRequest($_POST, "q");
 $normalizedAndSanitizedQuery = normalizeAndSanitizeString($query);
 $precomputedId = getPrecomputedIdFromRequest($_POST, "unique_id");
-
-$params_array = array("document_types", "sorting", "min_descsize");
-$optional_get_params = ["repo", "coll", "vis_type", "q_advanced", "lang_id", "custom_title", "exclude_date_filters", "from", "to", "custom_clustering"];
-
-function filterEmptyString($value)
-{
-    // Exclude empty strings
-    return $value !== '';
-}
-
-foreach ($optional_get_params as $param) {
-    if (isset($_POST[$param])) {
-        $params_array[] = $param;
-    }
-}
-
-$post_params = $_POST;
-
-if (!isset($post_params["min_descsize"])) {
-    $post_params["min_descsize"] = 300;
-}
-if (isset($post_params["lang_id"])) {
-    $post_params["lang_id"] = array_filter($post_params["lang_id"], 'filterEmptyString');
-    if (count($post_params["lang_id"]) == 0) {
-        $post_params["lang_id"] = ["all-lang"];
-    }
-}
-// ignore date filters if vis_type is set to timeline
-if (isset($post_params["vis_type"]) && $post_params["vis_type"] == "timeline") {
-    unset($params_array[array_search("exclude_date_filters", $params_array)]);
-    unset($post_params["exclude_date_filters"]);
-}
-// check if exclude_date_filters is set and true
-if (isset($post_params["exclude_date_filters"]) && $post_params["exclude_date_filters"] === true) {
-    // Add "today" and exclude "from" and "to" from the $params_array
-    $params_array = array_merge($params_array, ["today"]);
-    unset($params_array["from"], $params_array["to"]);
-}
-
-// re-establish historic order for backwards ID compatibility
-// from, to, document_types, sorting, min_descsize, repo, coll, vis_type, lang_id, q_advanced, exclude_date_filters, custom_title, custom_clustering
-$historic_params_order = array("from", "to", "document_types", "sorting", "min_descsize", "repo",
-                               "coll", "vis_type", "lang_id", "q_advanced", "exclude_date_filters", "custom_title", "custom_clustering");
-$reordered_params = array();
-foreach($historic_params_order as $param) {
-    if (isset($post_params[$param])) {
-        $reordered_params[] = $param;
-    }
-}
-foreach($params_array as $param) {
-    if (!in_array($param, $reordered_params)) {
-        $reordered_params[] = $param;
-    }
-}
-$params_array = $reordered_params;
+$requestArray = $_POST;
+$parameters = getParameters($requestArray);
 
 $result = search(
     "base",
     $normalizedAndSanitizedQuery,
-    $post_params,
-    $params_array,
+    $requestArray,
+    $parameters,
     true,
     true,
     null,
