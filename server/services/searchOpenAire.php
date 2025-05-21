d17b4a941112053395f2a8e658e838d76404b300
andrei <andrei.shket@modsen-software.com>
2025-05-21 10:15:33 +0200

    refactor: remove parameters duplication in searchOpenAire

--- server/services/searchOpenAire.php
+++ server/services/searchOpenAire.php
@@ -19,50 +19,6 @@ function getQuery(array $requestArray, bool $isWithAcronymTitle): string|null {
 }
 
 /**
- * Returning the array with parameters that depends on the request type.
- * Known code "bad moment": common parameters should be stored in a separate array and reused to
- * remove code duplication, but it is impossible because order of elements results in visualization
- * id creation.
- * @param bool $isWithAcronymTitle - Is request with acronym title.
- * @return string[] - Array with parameters.
- */
-function getParameters(bool $isWithAcronymTitle): array {
-    $ACRONYM_TITLE = [
-        "project_id",
-        "funder",
-        "title",
-        "funding_tree",
-        "call_id",
-        "start_date",
-        "end_date",
-        "oa_mandate",
-        "special_clause",
-        "organisations",
-        "openaire_link",
-        "obj_id",
-        "acronym",
-    ];
-
-    $NON_ACRONYM_TITLE = [
-        "project_id",
-        "funder",
-        "acronym",
-        "title",
-        "start_date",
-        "end_date",
-        "special_clause",
-        "oa_mandate",
-        "organisations",
-        "openaire_link",
-        "obj_id",
-        "call_id",
-        "funding_tree",
-    ];
-
-    return $isWithAcronymTitle ? $ACRONYM_TITLE : $NON_ACRONYM_TITLE;
-}
-
-/**
  * Returning the array with ids depends on the request type.
  * @param bool $isWithAcronymTitle - Is request with acronym title.
  * @return string[] - Array with ids.
@@ -72,21 +28,35 @@ function getArrayWithIds(bool $isWithAcronymTitle): array {
     return $isWithAcronymTitle ? $DEFAULT : [... $DEFAULT, "today"];
 }
 
+$PARAMETERS = [
+    "project_id",
+    "funder",
+    "acronym",
+    "title",
+    "start_date",
+    "end_date",
+    "special_clause",
+    "oa_mandate",
+    "organisations",
+    "openaire_link",
+    "obj_id",
+    "call_id",
+    "funding_tree",
+];
+
 $requestArray = $_POST;
 $isWithAcronymTitle = array_key_exists("acronymtitle", $requestArray);
 
 $query = getQuery($_POST, $isWithAcronymTitle);
 $normalizedAndSanitizedQuery = normalizeAndSanitizeString($query);
 $precomputedId = getPrecomputedIdFromRequest($requestArray, "unique_id");
-$parameters = getParameters($isWithAcronymTitle);
 $arrayWithIds = getArrayWithIds($isWithAcronymTitle);
-
 
 $result = search(
     "openaire",
     $normalizedAndSanitizedQuery,
     $requestArray,
-    $parameters,
+    $PARAMETERS,
     false,
     true,
     $arrayWithIds,
