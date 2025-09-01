cd7f9546ea7714be3fdc2fcf2132a28be043c4b8
andrei <andrei.shket@modsen-software.com>
2025-09-01 17:54:32 +0200

    refactor: types for the DocumentTypes component

--- vis/js/templates/contextfeatures/DocumentTypes.tsx
+++ vis/js/templates/contextfeatures/DocumentTypes.tsx
@@ -1,12 +1,22 @@
-// @ts-nocheck
-
-import React from "react";
-
+import React, { FC, ReactNode } from "react";
 import HoverPopover from "../HoverPopover";
 import { useLocalizationContext } from "../../components/LocalizationProvider";
 import useMatomo from "../../utils/useMatomo";
 
-const DocumentTypes = ({ documentTypes, popoverContainer }) => {
+interface DocumentTypesProps {
+  documentTypes: string[];
+  popoverContainer: ReactNode;
+}
+
+const DocumentTypes: FC<DocumentTypesProps> = ({
+  documentTypes,
+  popoverContainer,
+}) => {
+  console.log("DocumentTypes props:", {
+    documentTypes,
+    popoverContainer,
+  });
+
   const loc = useLocalizationContext();
   const { trackEvent } = useMatomo();
 
@@ -31,19 +41,19 @@ const DocumentTypes = ({ documentTypes, popoverContainer }) => {
           size="wide"
           container={popoverContainer}
           content={
-              <>
-                  {loc.documenttypes_tooltip}
-                  <br/>
-                  <br/>
-                  {text}
-              </>
+            <>
+              {loc.documenttypes_tooltip}
+              <br />
+              <br />
+              {text}
+            </>
           }
         >
           <span className="context_moreinfo">{loc.documenttypes_label}</span>
         </HoverPopover>
       </span>
-        {/* this empty space was on web interface in context line */}
-        {/*{" "}*/}
+      {/* this empty space was on web interface in context line */}
+      {/*{" "}*/}
     </>
   );
 };
