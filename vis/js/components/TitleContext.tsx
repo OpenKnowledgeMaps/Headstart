4d375fd0b70c8d9ad6fa5910557b6fd65be4906f
andrei <andrei.shket@modsen-software.com>
2025-09-08 15:10:30 +0200

    refactor: types for the TitleContext component

--- vis/js/components/TitleContext.tsx
+++ vis/js/components/TitleContext.tsx
@@ -1,32 +1,38 @@
-// @ts-nocheck
-
 import React from "react";
 import { connect } from "react-redux";
 
 import SubdisciplineTitle from "../templates/SubdisciplineTitle";
 import AuthorImage from "../templates/AuthorImage";
+import { ServiceType } from "../@types/service";
 
 interface TitleContextProps {
   showAuthor: boolean;
   authorImage: string;
   orcidId: string;
-  service: string;
+  service: ServiceType;
 }
 
-const TitleContext = ({ showAuthor, authorImage, orcidId, service }: TitleContextProps) => {
+const TitleContext = ({
+  showAuthor,
+  authorImage,
+  orcidId,
+  service,
+}: TitleContextProps) => {
   return (
     <div id="title_context" style={{ minHeight: 54 }}>
-      {showAuthor && <AuthorImage service={service} orcidId={orcidId} url={authorImage} />}
+      {showAuthor && (
+        <AuthorImage service={service} orcidId={orcidId} url={authorImage} />
+      )}
       <SubdisciplineTitle />
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: any) => ({
   showAuthor: state.contextLine.showAuthor,
   authorImage: state.contextLine.author.imageLink,
   orcidId: state.author.orcid_id,
-  service: state.contextLine.service
+  service: state.contextLine.service,
 });
 
 export default connect(mapStateToProps)(TitleContext);
