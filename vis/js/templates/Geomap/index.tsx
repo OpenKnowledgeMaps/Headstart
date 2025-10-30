7ed01c5da2c410957a48fbb0dd5bce2e70280c2c
andrei <andrei.shket@modsen-software.com>
2025-10-30 10:12:18 +0100

    refactor: data filtering and searching in the separate hook

--- vis/js/templates/Geomap/index.tsx
+++ vis/js/templates/Geomap/index.tsx
@@ -4,28 +4,20 @@ import { FC, useCallback } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import { useDispatch, useSelector } from "react-redux";
 
+import { useData } from "@/hooks/useData";
 import { selectPaper } from "@/js/actions";
 import { AllPossiblePapersType } from "@/js/types";
-import { filterData } from "@/js/utils/data";
 
 import { OPTIONS } from "./options";
 import { Pin } from "./Pin";
-import * as selectors from "./selectors";
+import { getSelectedItemId } from "./selectors";
 
 const { MAP, LAYER } = OPTIONS;
 
 export const Geomap: FC = () => {
-  const data = useSelector(selectors.getData);
-  const selectedItemId = useSelector(selectors.getSelectedItemId);
-  const searchSettings = useSelector(selectors.getSearchSettings);
-  const filterSettings = useSelector(selectors.getSearchFilterSettings);
+  const { filteredData } = useData(true);
+  const selectedItemId = useSelector(getSelectedItemId);
   const handleItemSelect = useDispatch();
-
-  const filteredData: AllPossiblePapersType[] = filterData(
-    data,
-    searchSettings,
-    filterSettings,
-  );
 
   const handlePinClick = useCallback(
     (data: AllPossiblePapersType) => {
@@ -37,19 +29,20 @@ export const Geomap: FC = () => {
   return (
     <MapContainer {...MAP} className="geomap_container">
       <TileLayer {...LAYER} />
-      {filteredData.map((item) => {
-        const { safe_id: id } = item;
-        const isActive = selectedItemId === id;
+      {filteredData &&
+        filteredData.map((item) => {
+          const { safe_id: id } = item;
+          const isActive = selectedItemId === id;
 
-        return (
-          <Pin
-            key={id + isActive}
-            data={item}
-            isActive={isActive}
-            onClick={handlePinClick}
-          />
-        );
-      })}
+          return (
+            <Pin
+              key={id + isActive}
+              data={item}
+              isActive={isActive}
+              onClick={handlePinClick}
+            />
+          );
+        })}
     </MapContainer>
   );
 };
--- vis/js/templates/Geomap/selectors.ts
+++ vis/js/templates/Geomap/selectors.ts
@@ -1,21 +1,3 @@
-import { STREAMGRAPH_MODE } from "@/js/reducers/chartType";
 import { State } from "@/js/types";
 
-export const getData = (state: State) => state.data.list;
-
 export const getSelectedItemId = (state: State) => state.selectedPaper?.safeId;
-
-export const getSearchSettings = (state: State) => ({
-  value: state.list.searchValue,
-});
-
-export const getSearchFilterSettings = (state: State) => ({
-  value: state.list.filterValue,
-  field: state.list.filterField,
-  zoomed: state.zoom,
-  area: state.selectedBubble ? state.selectedBubble.uri : null,
-  paper: null,
-  isStreamgraph: state.chartType === STREAMGRAPH_MODE,
-  title: state.selectedBubble ? state.selectedBubble.title : null,
-  docIds: state.selectedBubble ? state.selectedBubble.docIds : null,
-});
