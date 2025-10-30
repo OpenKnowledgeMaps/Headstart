71752746707615d666d79141251a3d7cd5fc35d2
andrei <andrei.shket@modsen-software.com>
2025-10-30 09:07:24 +0100

    feat: search for pins on the map

--- vis/js/templates/Geomap/index.tsx
+++ vis/js/templates/Geomap/index.tsx
@@ -5,20 +5,27 @@ import { MapContainer, TileLayer } from "react-leaflet";
 import { useDispatch, useSelector } from "react-redux";
 
 import { selectPaper } from "@/js/actions";
-import { AllPossiblePapersType, State } from "@/js/types";
+import { AllPossiblePapersType } from "@/js/types";
+import { filterData } from "@/js/utils/data";
 
 import { OPTIONS } from "./options";
 import { Pin } from "./Pin";
+import * as selectors from "./selectors";
 
 const { MAP, LAYER } = OPTIONS;
 
-const getItemsData = (state: State) => state.data.list;
-const getSelectedItemId = (state: State) => state.selectedPaper?.safeId;
-
 export const Geomap: FC = () => {
-  const items = useSelector(getItemsData);
-  const selectedItemId = useSelector(getSelectedItemId);
+  const data = useSelector(selectors.getData);
+  const selectedItemId = useSelector(selectors.getSelectedItemId);
+  const searchSettings = useSelector(selectors.getSearchSettings);
+  const filterSettings = useSelector(selectors.getSearchFilterSettings);
   const handleItemSelect = useDispatch();
+
+  const filteredData: AllPossiblePapersType[] = filterData(
+    data,
+    searchSettings,
+    filterSettings,
+  );
 
   const handlePinClick = useCallback(
     (data: AllPossiblePapersType) => {
@@ -30,14 +37,14 @@ export const Geomap: FC = () => {
   return (
     <MapContainer {...MAP} className="geomap_container">
       <TileLayer {...LAYER} />
-      {items.map((data) => {
-        const { safe_id: id } = data;
+      {filteredData.map((item) => {
+        const { safe_id: id } = item;
         const isActive = selectedItemId === id;
 
         return (
           <Pin
             key={id + isActive}
-            data={data}
+            data={item}
             isActive={isActive}
             onClick={handlePinClick}
           />
