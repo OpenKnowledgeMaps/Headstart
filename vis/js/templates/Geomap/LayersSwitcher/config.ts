17566c17f7419e157df4e73c7be65a1d1f5d0ba9
andrei <andrei.shket@modsen-software.com>
2025-11-05 15:40:54 +0100

    feat: refactor attribution for layers

--- vis/js/templates/Geomap/LayersSwitcher/config.ts
+++ vis/js/templates/Geomap/LayersSwitcher/config.ts
@@ -2,24 +2,19 @@ import { Config } from "./types";
 
 export const CONFIG: Config = {
   POSITION: "topright",
-  CHECKED_LAYER_NAME: "OpenStreetMap",
+  CHECKED_LAYER_NAME: "Standard",
   LAYERS: [
     {
-      name: "OpenStreetMap",
-      attribution: "&copy; OpenStreetMap contributors",
+      name: "Standard",
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap contributors</a> ♥ <a href="https://wiki.osmfoundation.org/wiki/Terms_of_Use" target="_blank" rel="noopener noreferrer">Make a Donation</a>. <a href="https://www.openstreetmap.org/terms" target="_blank" rel="noopener noreferrer">Website and API terms</a>',
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
     },
     {
-      name: "OpenStreetMap.HOT",
+      name: "Humanitarian",
       attribution:
-        "&copy; OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France",
+        '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noopener noreferrer">OpenStreetMap contributors</a>. Tiles style by <a href="https://www.hotosm.org/" target="_blank" rel="noopener noreferrer">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank" rel="noopener noreferrer">OpenStreetMap France</a>. <a href="https://www.openstreetmap.org/terms" target="_blank" rel="noopener noreferrer">Website and API terms</a>',
       url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
-    },
-    {
-      name: "OpenTopoMap",
-      attribution:
-        "Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap (CC-BY-SA)",
-      url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
     },
   ],
 };
