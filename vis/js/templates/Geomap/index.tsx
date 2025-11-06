3eb2689df2835d732deed63a4694dcdcf722f61b
andrei <andrei.shket@modsen-software.com>
2025-11-06 14:22:16 +0100

    refactor: height of the geomap visualization container

--- vis/js/templates/Geomap/index.tsx
+++ vis/js/templates/Geomap/index.tsx
@@ -4,15 +4,18 @@ import { FC } from "react";
 import { MapContainer, ZoomControl } from "react-leaflet";
 
 import { CONFIG } from "./config";
+import { HeightContainer } from "./HeightContainer";
 import { LayersSwitcher } from "./LayersSwitcher";
 import { Pins } from "./Pins";
 
 const { MAP, ZOOM_CONTROL } = CONFIG;
 
 export const Geomap: FC = () => (
-  <MapContainer {...MAP} className="geomap_container">
-    <ZoomControl {...ZOOM_CONTROL} />
-    <LayersSwitcher />
-    <Pins />
-  </MapContainer>
+  <HeightContainer>
+    <MapContainer {...MAP} className="geomap_container">
+      <ZoomControl {...ZOOM_CONTROL} />
+      <LayersSwitcher />
+      <Pins />
+    </MapContainer>
+  </HeightContainer>
 );
