8a26978129fe6113ad24b62a7af8fd6e0ccfe3f2
andrei <andrei.shket@modsen-software.com>
2025-11-12 10:51:35 +0100

    bugfix: list scroll reset on the map click

--- vis/js/templates/Geomap/HeightContainer/index.tsx
+++ vis/js/templates/Geomap/HeightContainer/index.tsx
@@ -1,15 +1,19 @@
 import { FC, PropsWithChildren } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
+import { useActiveDataItem } from "@/hooks";
 import { deselectPaper } from "@/js/actions";
 
 import { getMapHeight } from "./selectors";
 
 export const HeightContainer: FC<PropsWithChildren> = ({ children }) => {
+  const { selectedItemId } = useActiveDataItem();
   const height = useSelector(getMapHeight);
   const dispatch = useDispatch();
 
   const handleMapClick = () => {
+    if (!selectedItemId) return;
+
     dispatch(deselectPaper());
   };
 
