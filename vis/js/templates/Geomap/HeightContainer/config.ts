964e5cd0f5e57e55e60a7589704464cbf87e4960
andrei <andrei.shket@modsen-software.com>
2025-12-16 14:16:45 +0100

    feat: accessibility attributes for map container

---
+++ vis/js/templates/Geomap/HeightContainer/config.ts
@@ -0,0 +1,4 @@
+export const ACCESSABILITY_ATTRIBUTES = {
+  "role": "region",
+  "aria-label": "Geo Map of aquatic mesocosm facilities",
+};
--- vis/js/templates/Geomap/HeightContainer/index.tsx
+++ vis/js/templates/Geomap/HeightContainer/index.tsx
@@ -2,9 +2,14 @@ import { FC, PropsWithChildren } from "react";
 import { useSelector } from "react-redux";
 
 import { getMapHeight } from "./selectors";
+import { ACCESSABILITY_ATTRIBUTES } from "./config";
 
 export const HeightContainer: FC<PropsWithChildren> = ({ children }) => {
   const height = useSelector(getMapHeight);
 
-  return <div style={{ height }}>{children}</div>;
+  return (
+    <div style={{ height }} {...ACCESSABILITY_ATTRIBUTES}>
+      {children}
+    </div>
+  );
 };
