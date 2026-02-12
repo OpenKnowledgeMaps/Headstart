814be7648b01b44bbf101ee4f9ed03061521c454
andrei <andrei.shket@modsen-software.com>
2026-02-12 10:53:42 +0100

    feat: native merging strategy for subject and subject_orig

--- server/workers/common/common/enrichment.py
+++ server/workers/common/common/enrichment.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
-def enrich_anchor_using_duplicates(df, dupind):
+STRATEGY_REPLACE = 'replace'
+STRATEGY_MERGE = 'merge'
+
+def enrich_anchor_using_duplicates(df, dupind, subject_strategy=STRATEGY_MERGE):
     """
     Enriches anchor elements using data from duplicates in their groups.
 
@@ -9,13 +12,16 @@ def enrich_anchor_using_duplicates(df, dupind):
     All improvements are done in a single pass through the group for efficiency.
 
     List of improvements:
-        - subject_orig: replaced with value having the highest number of keywords
-        - subject: replaced with value having the highest number of keywords
+        - subject_orig: processed according to subject_strategy
+        - subject: processed according to subject_strategy
         - paper_abstract: replaced with the longest description
 
     Args:
         df: DataFrame with metadata, containing the column is_anchor
         dupind: Series with indices of duplicates for each id
+        subject_strategy: Strategy for processing keywords ('replace' or 'merge')
+            - 'replace': select the value with the highest number of keywords
+            - 'merge': combine all keywords from the group, removing duplicates
 
     Returns:
         DataFrame with improved anchor properties
@@ -27,6 +33,8 @@ def enrich_anchor_using_duplicates(df, dupind):
     is_all_columns_are_missing = not has_subject_orig and not has_subject and not has_paper_abstract
     if is_all_columns_are_missing:
         return df
+
+    is_use_merge_strategy = subject_strategy == STRATEGY_MERGE
 
     for _, idx in dupind.items():
         idx = df.index.intersection(idx)
@@ -47,54 +55,76 @@ def enrich_anchor_using_duplicates(df, dupind):
         anchor = anchors.iloc[0]
         anchor_idx = anchor.name
 
+        all_subject_orig_keywords = set()
+        all_subject_keywords = set()
+
         best_subject_orig = None
         best_subject_orig_count = 0
         best_subject = None
         best_subject_count = 0
+
         best_paper_abstract = None
         best_paper_abstract_length = 0
 
         for element_idx in idx:
             if has_subject_orig:
                 subject_orig_value = group_data.loc[element_idx, 'subject_orig']
-                is_subject_orig_value_is_missing = pd.isna(subject_orig_value) or subject_orig_value == ''
-                if not is_subject_orig_value_is_missing:
+                is_not_empty = not (pd.isna(subject_orig_value) or subject_orig_value == '')
+                if is_not_empty:
                     keywords = [kw.strip() for kw in str(subject_orig_value).split(';') if kw.strip()]
-                    keyword_count = len(keywords)
-                    if keyword_count > best_subject_orig_count:
-                        best_subject_orig_count = keyword_count
-                        best_subject_orig = subject_orig_value
+                    if is_use_merge_strategy:
+                        all_subject_orig_keywords.update(keywords)
+                    else:
+                        keyword_count = len(keywords)
+                        if keyword_count > best_subject_orig_count:
+                            best_subject_orig_count = keyword_count
+                            best_subject_orig = subject_orig_value
 
             if has_subject:
                 subject_value = group_data.loc[element_idx, 'subject']
-                is_subject_value_is_missing = pd.isna(subject_value) or subject_value == ''
-                if not is_subject_value_is_missing:
+                is_not_empty = not (pd.isna(subject_value) or subject_value == '')
+                if is_not_empty:
                     keywords = [kw.strip() for kw in str(subject_value).split(';') if kw.strip()]
-                    keyword_count = len(keywords)
-                    if keyword_count > best_subject_count:
-                        best_subject_count = keyword_count
-                        best_subject = subject_value
+                    if is_use_merge_strategy:
+                        all_subject_keywords.update(keywords)
+                    else:
+                        keyword_count = len(keywords)
+                        if keyword_count > best_subject_count:
+                            best_subject_count = keyword_count
+                            best_subject = subject_value
 
             if has_paper_abstract:
                 paper_abstract_value = group_data.loc[element_idx, 'paper_abstract']
-                is_paper_abstract_value_is_missing = pd.isna(paper_abstract_value) or paper_abstract_value == ''
-                if not is_paper_abstract_value_is_missing:
+                is_not_empty = not (pd.isna(paper_abstract_value) or paper_abstract_value == '')
+                if is_not_empty:
                     abstract_length = len(str(paper_abstract_value))
                     if abstract_length > best_paper_abstract_length:
                         best_paper_abstract_length = abstract_length
                         best_paper_abstract = paper_abstract_value
 
-        is_better_subject_orig_presented = best_subject_orig is not None
-        if is_better_subject_orig_presented:
-            current = df.loc[anchor_idx, 'subject_orig']
-            if pd.isna(current) or str(current) != str(best_subject_orig):
-                df.loc[anchor_idx, 'subject_orig'] = best_subject_orig
+        if has_subject_orig:
+            if is_use_merge_strategy:
+                if all_subject_orig_keywords:
+                    merged_value = '; '.join(sorted(all_subject_orig_keywords))
+                    df.loc[anchor_idx, 'subject_orig'] = merged_value
+            else:
+                is_better_subject_orig_presented = best_subject_orig is not None
+                if is_better_subject_orig_presented:
+                    current = df.loc[anchor_idx, 'subject_orig']
+                    if pd.isna(current) or str(current) != str(best_subject_orig):
+                        df.loc[anchor_idx, 'subject_orig'] = best_subject_orig
 
-        is_better_subject_presented = best_subject is not None
-        if is_better_subject_presented:
-            current = df.loc[anchor_idx, 'subject']
-            if pd.isna(current) or str(current) != str(best_subject):
-                df.loc[anchor_idx, 'subject'] = best_subject
+        if has_subject:
+            if is_use_merge_strategy:
+                if all_subject_keywords:
+                    merged_value = '; '.join(sorted(all_subject_keywords))
+                    df.loc[anchor_idx, 'subject'] = merged_value
+            else:
+                is_better_subject_presented = best_subject is not None
+                if is_better_subject_presented:
+                    current = df.loc[anchor_idx, 'subject']
+                    if pd.isna(current) or str(current) != str(best_subject):
+                        df.loc[anchor_idx, 'subject'] = best_subject
 
         is_better_paper_abstract_presented = best_paper_abstract is not None
         if is_better_paper_abstract_presented:
