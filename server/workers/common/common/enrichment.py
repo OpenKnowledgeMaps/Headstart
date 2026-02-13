4556aff0094db35a283663169ea8f9775bfa729f
andrei <andrei.shket@modsen-software.com>
2026-02-13 13:29:26 +0100

    refactor: code clean up

--- server/workers/common/common/enrichment.py
+++ server/workers/common/common/enrichment.py
@@ -69,121 +69,208 @@ def enrich_anchor_using_duplicates(df, dupind, subject_strategy=STRATEGY_MERGE):
         anchor = anchors.iloc[0]
         anchor_idx = anchor.name
 
-        all_subject_orig_keywords = set()
-        all_subject_keywords = set()
-
-        best_subject_orig = None
-        best_subject_orig_count = 0
-        best_subject = None
-        best_subject_count = 0
-
-        best_paper_abstract = None
-        best_paper_abstract_length = 0
-
-        best_oa_state = None
-        best_oa_state_priority = float('inf')
-
+        subject_orig_acc = {'all_keywords': set(), 'best_value': None, 'best_count': 0}
+        subject_acc = {'all_keywords': set(), 'best_value': None, 'best_count': 0}
+        paper_abstract_acc = {'best_value': None, 'best_length': 0}
+        oa_state_acc = {'best_value': None, 'best_priority': float('inf')}
         all_links = set()
 
         for element_idx in idx:
             if has_subject_orig:
                 subject_orig_value = group_data.loc[element_idx, 'subject_orig']
-                is_not_empty = not (pd.isna(subject_orig_value) or subject_orig_value == '')
-                if is_not_empty:
-                    keywords = [kw.strip() for kw in str(subject_orig_value).split(';') if kw.strip()]
-                    if is_use_merge_strategy:
-                        all_subject_orig_keywords.update(keywords)
-                    else:
-                        keyword_count = len(keywords)
-                        if keyword_count > best_subject_orig_count:
-                            best_subject_orig_count = keyword_count
-                            best_subject_orig = subject_orig_value
+                process_subject_orig_element(subject_orig_value, subject_orig_acc, is_use_merge_strategy)
 
             if has_subject:
                 subject_value = group_data.loc[element_idx, 'subject']
-                is_not_empty = not (pd.isna(subject_value) or subject_value == '')
-                if is_not_empty:
-                    keywords = [kw.strip() for kw in str(subject_value).split(';') if kw.strip()]
-                    if is_use_merge_strategy:
-                        all_subject_keywords.update(keywords)
-                    else:
-                        keyword_count = len(keywords)
-                        if keyword_count > best_subject_count:
-                            best_subject_count = keyword_count
-                            best_subject = subject_value
+                process_subject_element(subject_value, subject_acc, is_use_merge_strategy)
 
             if has_paper_abstract:
                 paper_abstract_value = group_data.loc[element_idx, 'paper_abstract']
-                is_not_empty = not (pd.isna(paper_abstract_value) or paper_abstract_value == '')
-                if is_not_empty:
-                    abstract_length = len(str(paper_abstract_value))
-                    if abstract_length > best_paper_abstract_length:
-                        best_paper_abstract_length = abstract_length
-                        best_paper_abstract = paper_abstract_value
+                process_paper_abstract_element(paper_abstract_value, paper_abstract_acc)
 
             if has_oa_state:
                 oa_state_value = group_data.loc[element_idx, 'oa_state']
-                is_not_empty = not pd.isna(oa_state_value)
-                if is_not_empty:
-                    oa_state_str = str(oa_state_value)
-                    priority = OA_STATE_PRIORITY.get(oa_state_str, float('inf'))
-                    if priority < best_oa_state_priority:
-                        best_oa_state_priority = priority
-                        best_oa_state = oa_state_value
+                process_oa_state_element(oa_state_value, oa_state_acc)
 
             if has_link:
                 link_value = group_data.loc[element_idx, 'link']
-                is_not_empty = not (pd.isna(link_value) or link_value == '')
-                if is_not_empty:
-                    links = [link.strip() for link in str(link_value).split(';') if link.strip()]
-                    all_links.update(links)
+                process_link_element(link_value, all_links)
 
         if has_subject_orig:
-            if is_use_merge_strategy:
-                if all_subject_orig_keywords:
-                    unique_keywords = deduplicate_keywords(all_subject_orig_keywords)
-                    merged_value = '; '.join(sorted(unique_keywords))
-                    df.loc[anchor_idx, 'subject_orig'] = merged_value
-            else:
-                is_better_subject_orig_presented = best_subject_orig is not None
-                if is_better_subject_orig_presented:
-                    current = df.loc[anchor_idx, 'subject_orig']
-                    if pd.isna(current) or str(current) != str(best_subject_orig):
-                        df.loc[anchor_idx, 'subject_orig'] = best_subject_orig
+            apply_subject_improvements(df, anchor_idx, subject_orig_acc, is_use_merge_strategy, 'subject_orig')
 
         if has_subject:
-            if is_use_merge_strategy:
-                if all_subject_keywords:
-                    unique_keywords = deduplicate_keywords(all_subject_keywords)
-                    merged_value = '; '.join(sorted(unique_keywords))
-                    df.loc[anchor_idx, 'subject'] = merged_value
-            else:
-                is_better_subject_presented = best_subject is not None
-                if is_better_subject_presented:
-                    current = df.loc[anchor_idx, 'subject']
-                    if pd.isna(current) or str(current) != str(best_subject):
-                        df.loc[anchor_idx, 'subject'] = best_subject
+            apply_subject_improvements(df, anchor_idx, subject_acc, is_use_merge_strategy, 'subject')
 
-        is_better_paper_abstract_presented = best_paper_abstract is not None
-        if is_better_paper_abstract_presented:
-            current = df.loc[anchor_idx, 'paper_abstract']
-            if pd.isna(current) or str(current) != str(best_paper_abstract):
-                df.loc[anchor_idx, 'paper_abstract'] = best_paper_abstract
+        if has_paper_abstract:
+            apply_paper_abstract_improvements(df, anchor_idx, paper_abstract_acc)
 
-        is_better_oa_state_presented = best_oa_state is not None
-        if is_better_oa_state_presented:
-            current = df.loc[anchor_idx, 'oa_state']
-            if pd.isna(current) or str(current) != str(best_oa_state):
-                df.loc[anchor_idx, 'oa_state'] = best_oa_state
+        if has_oa_state:
+            apply_oa_state_improvements(df, anchor_idx, oa_state_acc)
 
         if has_link:
-            if all_links:
-                unique_links = deduplicate_links(all_links)
-                if unique_links:
-                    merged_links = '; '.join(sorted(unique_links))
-                    df.loc[anchor_idx, 'link'] = merged_links
+            apply_link_improvements(df, anchor_idx, all_links)
 
     return df
+
+def process_subject_orig_element(value, accumulator, use_merge_strategy):
+    """
+    Processes the subject_orig value for one element of the group.
+
+    Args:
+        value: The subject_orig value from the element
+        accumulator: Dictionary with accumulative data
+        use_merge_strategy: If True, uses the merge strategy, otherwise replace
+    """
+    is_not_empty = not (pd.isna(value) or value == '')
+    if not is_not_empty:
+        return
+
+    keywords = [kw.strip() for kw in str(value).split(';') if kw.strip()]
+    if use_merge_strategy:
+        accumulator['all_keywords'].update(keywords)
+    else:
+        keyword_count = len(keywords)
+        if keyword_count > accumulator['best_count']:
+            accumulator['best_count'] = keyword_count
+            accumulator['best_value'] = value
+
+def process_subject_element(value, accumulator, use_merge_strategy):
+    """
+    Processes the subject value for one element of the group.
+
+    Args:
+        value: The subject value from the element
+        accumulator: Dictionary with accumulative data
+        use_merge_strategy: If True, uses the merge strategy, otherwise replace
+    """
+    is_not_empty = not (pd.isna(value) or value == '')
+    if not is_not_empty:
+        return
+
+    keywords = [kw.strip() for kw in str(value).split(';') if kw.strip()]
+    if use_merge_strategy:
+        accumulator['all_keywords'].update(keywords)
+    else:
+        keyword_count = len(keywords)
+        if keyword_count > accumulator['best_count']:
+            accumulator['best_count'] = keyword_count
+            accumulator['best_value'] = value
+
+def process_paper_abstract_element(value, accumulator):
+    """
+    Processes the paper_abstract value for one element of the group.
+
+    Args:
+        value: The paper_abstract value from the element
+        accumulator: Dictionary with accumulative data
+    """
+    is_not_empty = not (pd.isna(value) or value == '')
+    if not is_not_empty:
+        return
+
+    abstract_length = len(str(value))
+    if abstract_length > accumulator['best_length']:
+        accumulator['best_length'] = abstract_length
+        accumulator['best_value'] = value
+
+def process_oa_state_element(value, accumulator):
+    """
+    Processes the oa_state value for one element of the group.
+
+    Args:
+        value: The oa_state value from the element
+        accumulator: Dictionary with accumulative data
+    """
+    is_not_empty = not pd.isna(value)
+    if not is_not_empty:
+        return
+
+    oa_state_str = str(value)
+    priority = OA_STATE_PRIORITY.get(oa_state_str, float('inf'))
+    if priority < accumulator['best_priority']:
+        accumulator['best_priority'] = priority
+        accumulator['best_value'] = value
+
+def process_link_element(value, accumulator):
+    """
+    Processes the link value for one element of the group.
+
+    Args:
+        value: The link value from the element
+        accumulator: Set to collect all links
+    """
+    is_not_empty = not (pd.isna(value) or value == '')
+    if not is_not_empty:
+        return
+
+    links = [link.strip() for link in str(value).split(';') if link.strip()]
+    accumulator.update(links)
+
+def apply_subject_improvements(df, anchor_idx, accumulator, use_merge_strategy, column_name):
+    """
+    Applies improvements for subject or subject_orig to the anchor element.
+
+    Args:
+        df: DataFrame with data
+        anchor_idx: Index of the anchor element
+        accumulator: Dictionary with accumulative data
+        use_merge_strategy: If True, uses the merge strategy, otherwise replace
+        column_name: Column name ('subject' or 'subject_orig')
+    """
+    if use_merge_strategy:
+        if accumulator['all_keywords']:
+            unique_keywords = deduplicate_keywords(accumulator['all_keywords'])
+            merged_value = '; '.join(sorted(unique_keywords))
+            df.loc[anchor_idx, column_name] = merged_value
+    else:
+        if accumulator['best_value'] is not None:
+            current = df.loc[anchor_idx, column_name]
+            if pd.isna(current) or str(current) != str(accumulator['best_value']):
+                df.loc[anchor_idx, column_name] = accumulator['best_value']
+
+def apply_paper_abstract_improvements(df, anchor_idx, accumulator):
+    """
+    Applies improvements for paper_abstract to the anchor element.
+
+    Args:
+        df: DataFrame with data
+        anchor_idx: Index of the anchor element
+        accumulator: Dictionary with accumulative data
+    """
+    if accumulator['best_value'] is not None:
+        current = df.loc[anchor_idx, 'paper_abstract']
+        if pd.isna(current) or str(current) != str(accumulator['best_value']):
+            df.loc[anchor_idx, 'paper_abstract'] = accumulator['best_value']
+
+def apply_oa_state_improvements(df, anchor_idx, accumulator):
+    """
+    Applies improvements for oa_state to the anchor element.
+
+    Args:
+        df: DataFrame with data
+        anchor_idx: Index of the anchor element
+        accumulator: Dictionary with accumulative data
+    """
+    if accumulator['best_value'] is not None:
+        current = df.loc[anchor_idx, 'oa_state']
+        if pd.isna(current) or str(current) != str(accumulator['best_value']):
+            df.loc[anchor_idx, 'oa_state'] = accumulator['best_value']
+
+def apply_link_improvements(df, anchor_idx, all_links):
+    """
+    Applies improvements for link to the anchor element.
+
+    Args:
+        df: DataFrame with data
+        anchor_idx: Index of the anchor element
+        all_links: Set of all collected links
+    """
+    if all_links:
+        unique_links = deduplicate_links(all_links)
+        if unique_links:
+            merged_links = '; '.join(sorted(unique_links))
+            df.loc[anchor_idx, 'link'] = merged_links
 
 def deduplicate_keywords(keywords, similarity_threshold=KEYWORD_SIMILARITY_THRESHOLD):
     """
