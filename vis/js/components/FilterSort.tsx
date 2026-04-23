4db3614a6da0460ca44840e2b5d8656cd7145851
andrei <andrei.shket@modsen-software.com>
2026-04-23 12:39:05 +0200

    bugfix: the sort by filter is not displayed when list is hidden

--- vis/js/components/FilterSort.tsx
+++ vis/js/components/FilterSort.tsx
@@ -8,18 +8,29 @@ import { State } from "../types";
 export interface FilterSortProps {
   showSort: boolean;
   showFilter: boolean;
+  isListVisible: boolean;
   color: string | null;
 }
 
-const FilterSort: FC<FilterSortProps> = ({ showSort, showFilter, color }) => {
+const FilterSort: FC<FilterSortProps> = ({
+  showSort,
+  showFilter,
+  color,
+  isListVisible,
+}) => {
+  const isSortFilterDisplayed = showSort && isListVisible;
+
   if (showFilter) {
-    return <StandardFilterSort displaySort={showSort} color={color} />;
+    return (
+      <StandardFilterSort displaySort={isSortFilterDisplayed} color={color} />
+    );
   }
 
-  return <BasicFilterSort displaySort={showSort} color={color} />;
+  return <BasicFilterSort displaySort={isSortFilterDisplayed} color={color} />;
 };
 
 const mapStateToProps = (state: State) => ({
+  isListVisible: state.list.show,
   showSort: state.list.showSort,
   showFilter: state.list.showFilter,
   color: state.selectedBubble ? state.selectedBubble.color : null,
