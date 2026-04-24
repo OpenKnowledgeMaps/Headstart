31f7d7f34a21d1ad7ab6447a403e9ec96b8fc3c2
andrei <andrei.shket@modsen-software.com>
2026-04-24 13:30:53 +0200

    feat: e2e tests for AQUANAVI integration

---
+++ e2e/basicTests/AQUANAVI.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+import { prepareVisualisation } from "../../vis/js/utils/e2eVisualisationLoader";
+
+test.describe("Basic tests for AQUANAVI integration (Geomap visualisation type)", () => {
+  test("Loads default Knowledge Map visualisation", async ({ page }) => {
+    const VISUALISATION_DYNAMIC_URL =
+      "/search?type=get&vis_type=geomap&q=mesocosms&service=aquanavi&custom_title=aquatic+mesocosm+facilities&min_descsize=0";
+
+    await prepareVisualisation(page, VISUALISATION_DYNAMIC_URL);
+
+    await expect(page.locator("#search-term-unique")).toContainText(
+      "aquatic mesocosm facilities",
+    );
+    await expect(page.locator("#source")).toContainText(
+      "Data source: AQUANAVI",
+    );
+    await page.getByTestId("context").getByText("More information").click();
+    await expect(page.locator("#info-body")).toContainText(
+      "This geo map presents you with an overview of aquatic mesocosm facilities.",
+    );
+    await expect(page.locator("#info-body")).toContainText(
+      "This visualization has a custom title and was created using the following query: mesocosms",
+    );
+  });
+});
